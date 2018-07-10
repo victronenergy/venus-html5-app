878db15ba5f623508b8a07e8a5742a7af5a80d22
Martin Carltoft <carltoft@technocreatives.com>
2018-07-10 11:27:59 +0200

    Implement one-way data binding

---
+++ src/DataBinding.js
@@ -0,0 +1,17 @@
+import Metric from './Metric.js'
+
+export class DataBinding {
+	constructor(element, elementProperty, metric, metricProperty) {
+		this.element = element
+		this.elementProperty = elementProperty
+		this.metric = metric
+		this.metricProperty = metricProperty
+		this.update()
+		let ref = this
+		this.metric.addOnChangeCallback((metric) => { ref.update() })
+	}
+
+	update() {
+		this.element[this.elementProperty] = this.metric[this.metricProperty]
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Metric from './Metric.js'
+import DataBinding from './DataBinding.js'
 import MqttInterface from './MqttInterface.js'
 
 export * from './Metric'
+export * from './DataBinding'
 export * from './MqttInterface'
 
