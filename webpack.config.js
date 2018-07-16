0b91de7c48dc641795c129c9cee46473dc7d8479
Martin Carltoft <carltoft@technocreatives.com>
2018-07-16 09:15:28 +0200

    Move the library files to a new folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './library/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'venus-metrics.min.js',
