889bb9c454ea4729725955707d6f625e8430eaf0
Christoph Lange <christoph@clange.de>
2018-07-27 09:29:32 +0200

    Refactor: enter IP and portalID in beginning only

--- mqtt-relay.js
+++ mqtt-relay.js
@@ -1,15 +1,16 @@
 var mqtt = require('mqtt')
 
-var venus = mqtt.connect('mqtt://192.168.3.70')
-// var venus = mqtt.connect('mqtt://em.duckdns.org:11883') // gustav
+portalID = "38d2692be8fc";
+venusIP = "192.168.3.70";
+
+var venus = mqtt.connect('mqtt://' + venusIP)
 var relay = mqtt.connect('mqtt://localhost')
 
 venus.on('connect', function() {
 	console.log('venus connected')
 	venus.subscribe('N/#')
 	setInterval(() => {
-		relay.publish('R/38d2692be8fc/system/0/Serial', '');
-		// relay.publish('R/e8eb11e15100/system/0/Serial', ''); // gustav
+		relay.publish('R/' + portalID + '/system/0/Serial', '');
 	}, 5000)
 })
 
@@ -22,8 +23,7 @@ relay.on('connect', function() {
 	relay.subscribe('#')
 	setInterval(() => {
 		// work-around for greeting sent by venus when connecting
-		venus.publish('N/38d2692be8fc/system/0/Serial', '{"value": "38d2692be8fc"}')
-		// venus.publish('N/e8eb11e15100/system/0/Serial', '{"value": "e8eb11e15100"}') // gustav
+		venus.publish('N/' + portalID + '/system/0/Serial', '{"value": "' + portalID + '"}')
 	}, 10000)
 })
 
@@ -36,5 +36,4 @@ relay.on('message', function(topic, message) {
 		console.log(`relay recv: ${topic}: ${message.toString()}`)
 		venus.publish(topic, message)
 	}
-	// console.log(`relay recv: ${topic}: ${message.toString()}`)
 })
