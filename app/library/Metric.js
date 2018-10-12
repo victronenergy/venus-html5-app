e5e3cfb72fe16e181caf76405552fef617218f20
Heikki Honkanen <heikki.honkanen@reaktor.com>
2018-10-12 07:16:18 +0200

    Format files with prettier.

--- app/library/Metric.js
+++ app/library/Metric.js
@@ -7,70 +7,78 @@
  *   Use the provided defaultFormatter, numericFormatter functions or write your own to format a custom value.
  */
 class Metric {
-	/** 
-	 * Create a Metric instance.
-	 * @param {string} key - A unique key identifying the metric.
-	 * @param {string} description - A text description of the metric.
-	 * @param {string} unit - The unit of the value the metric represents.
-	 * @param {} formatter - The formatter function used to convert the raw value of the metric to a user-friendy display.
+  /**
+   * Create a Metric instance.
+   * @param {string} key - A unique key identifying the metric.
+   * @param {string} description - A text description of the metric.
+   * @param {string} unit - The unit of the value the metric represents.
+   * @param {} formatter - The formatter function used to convert the raw value of the metric to a user-friendy display.
    * @param {number} timeout - The timeout in seconds until the value is
    * considered stale.
-	 */
-	constructor(key, description, unit, formatter, timeout) {
-		this.key = key
-		this.description = description
-		this.unit = unit
-		this.formatter = formatter === undefined ? defaultFormatter() : formatter
-		this.timeout = timeout === undefined ? 0 : timeout
-		this._rawValue = undefined
-		this.callbacks = []
-		this.timerReference = undefined
-	}
+   */
+  constructor(key, description, unit, formatter, timeout) {
+    this.key = key
+    this.description = description
+    this.unit = unit
+    this.formatter = formatter === undefined ? defaultFormatter() : formatter
+    this.timeout = timeout === undefined ? 0 : timeout
+    this._rawValue = undefined
+    this.callbacks = []
+    this.timerReference = undefined
+  }
 
-	/**
-	 * Get the formatted value of the metric by calling its formatter.
-	 * @return {string} The formatted value.
-	 */
-	get value() {
-		return '<span id=' + this.key + '>' + this.formatter(this) + '</a>';
-	}
+  /**
+   * Get the formatted value of the metric by calling its formatter.
+   * @return {string} The formatted value.
+   */
+  get value() {
+    return "<span id=" + this.key + ">" + this.formatter(this) + "</a>"
+  }
 
-	/**
-	 * Get the raw value of the metric.
-	 * @return {} The raw value.
-	 */
-	get rawValue() { return this._rawValue }
+  /**
+   * Get the raw value of the metric.
+   * @return {} The raw value.
+   */
+  get rawValue() {
+    return this._rawValue
+  }
 
-	toStale(key) {
-		var element = document.getElementById(key);
-		if (element != undefined && element != null) {
-			element.innerHTML = '<span class="staleValues">' + element.innerHTML + '</span>';
-		}
-	}
+  toStale(key) {
+    var element = document.getElementById(key)
+    if (element != undefined && element != null) {
+      element.innerHTML =
+        '<span class="staleValues">' + element.innerHTML + "</span>"
+    }
+  }
 
-	/** 
-	 * Set the raw value of the metric.
-	 * This will also fire any callbacks registered using the addOnChangeCallback method.
-	 * @param {} rawValue - The new raw value.
-	 */
-	set rawValue(rawValue) {
-		this._rawValue = rawValue
+  /**
+   * Set the raw value of the metric.
+   * This will also fire any callbacks registered using the addOnChangeCallback method.
+   * @param {} rawValue - The new raw value.
+   */
+  set rawValue(rawValue) {
+    this._rawValue = rawValue
 
-		if (this.timeout > 0) {
-			clearTimeout(this.timerReference);
-			this.timerReference = setTimeout(this.toStale.bind(this, this.key), this.timeout);
-		}
+    if (this.timeout > 0) {
+      clearTimeout(this.timerReference)
+      this.timerReference = setTimeout(
+        this.toStale.bind(this, this.key),
+        this.timeout
+      )
+    }
 
-		this.callbacks.forEach((callback) => { callback(this) })
-	}
+    this.callbacks.forEach(callback => {
+      callback(this)
+    })
+  }
 
-	/**
-	 * Add a callback that is fired when the raw value of the metric changes.
-	 * @param {(metric) => {}} callback - The callback function.
-	 */
-	addOnChangeCallback(callback) {
-		this.callbacks.push(callback)
-	}
+  /**
+   * Add a callback that is fired when the raw value of the metric changes.
+   * @param {(metric) => {}} callback - The callback function.
+   */
+  addOnChangeCallback(callback) {
+    this.callbacks.push(callback)
+  }
 }
 
 /**
@@ -78,13 +86,13 @@ class Metric {
  * @param {string} defaultValue - The value that is displayed if the metric raw value is null or undefined.
  * @return A formatting function.
  */
-function defaultFormatter(defaultValue = '--') {
-	return (metric) => {
-		if (metric.rawValue === undefined || metric.rawValue === null) {
-			return defaultValue + metric.unit
-		}
-		return metric.rawValue.toString() + metric.unit
-	}
+function defaultFormatter(defaultValue = "--") {
+  return metric => {
+    if (metric.rawValue === undefined || metric.rawValue === null) {
+      return defaultValue + metric.unit
+    }
+    return metric.rawValue.toString() + metric.unit
+  }
 }
 
 /**
@@ -94,14 +102,14 @@ function defaultFormatter(defaultValue = '--') {
  * @param {string} defaultValue - The value that is displayed if the metric raw value is null or undefined.
  * @return A formatting function.
  */
-function numericFormatter(precision = 0, factor = 1.0, defaultValue = '--') {
-	return (metric) => {
-		if (metric.rawValue === undefined || metric.rawValue === null) {
-			return defaultValue + metric.unit
-		}
-		let value = Number(metric.rawValue) * factor
-		return precision === undefined
-			? value.toString() + metric.unit
-			: value.toFixed(precision) + metric.unit
-	}
+function numericFormatter(precision = 0, factor = 1.0, defaultValue = "--") {
+  return metric => {
+    if (metric.rawValue === undefined || metric.rawValue === null) {
+      return defaultValue + metric.unit
+    }
+    let value = Number(metric.rawValue) * factor
+    return precision === undefined
+      ? value.toString() + metric.unit
+      : value.toFixed(precision) + metric.unit
+  }
 }
