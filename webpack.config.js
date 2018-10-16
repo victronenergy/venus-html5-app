f0030436f67ff2dc8cf93a5ca8a690dea92a8e5f
Heikki Honkanen <heikki.honkanen@reaktor.com>
2018-10-16 15:55:02 +0200

    New webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 const path = require("path")
 
 module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, "app/js/index.js"),
   output: {
-    path: path.resolve(__dirname, "app/js"), // string
+    path: path.resolve(__dirname, "dist/js"), // string
     filename: "bundle.js"
   },
+  plugins: [
+    new CopyWebpackPlugin(
+      [
+        { from: path.resolve(__dirname, "app/css/"), to: path.resolve(__dirname, "dist/css") },
+        { from: path.resolve(__dirname, "app/images/"), to: path.resolve(__dirname, "dist/images") },
+        { from: path.resolve(__dirname, "app/vendor/"), to: path.resolve(__dirname, "dist/vendor") },
+        { from: path.resolve(__dirname, "app/index.html"), to: path.resolve(__dirname, "dist/") }
+      ],
+      {}
+    )
+  ],
   module: {
     rules: [
       {
