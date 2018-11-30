cb2bebe12b0a13fdfe9fc743ed0ae6ca229e6417
Corina Udrescu <corina.udrescu@gmail.com>
2018-11-30 15:38:28 +0100

    Save last received mqtt values in the Context

--- src/app/mqtt/MqttClientProvider.js
+++ src/app/mqtt/MqttClientProvider.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { MqttClientContext } from "../index.js"
 
 import * as mqtt from "mqtt"
+import { getMessageJson, parseTopic } from "../../service/util"
 export const STATUS = {
   CONNECTING: "connecting",
   CONNECTED: "connected",
@@ -14,7 +15,8 @@ class MqttClientProvider extends Component {
     client: null,
     error: null,
     status: STATUS.CONNECTING,
-    hasFailedToConnect: false
+    hasFailedToConnect: false,
+    messages: {}
   }
 
   componentDidMount() {
@@ -49,12 +51,61 @@ class MqttClientProvider extends Component {
 
     client.on("message", (topic, message) => {
       console.log(`Message received: ${topic} - ${message.toString()}`)
+
+      this.setState({
+        messages: {
+          ...this.state.messages,
+          [topic]: getMessageJson(message)
+        }
+      })
     })
+  }
+
+  getMessagesByTopics = topicList => {
+    let initialValues = topicList.reduce((obj, key) => {
+      obj[key] = { value: null }
+      return obj
+    }, {})
+
+    return Object.keys(this.state.messages)
+      .filter(topic => topicList.includes(topic))
+      .reduce((obj, key) => {
+        obj[key] = this.state.messages[key]
+        return obj
+      }, initialValues)
+  }
+
+  getMessagesByWildcard = wildcard => {
+    const wildcardParts = parseTopic(wildcard)
+    let values = Object.keys(this.state.messages)
+      .filter(topic => {
+        const topicParts = parseTopic(topic)
+        const matchesWildcard =
+          (topicParts.dbusPath === wildcardParts.dbusPath || wildcardParts.dbusPath === "+") &&
+          (topicParts.portalId === wildcardParts.portalId || wildcardParts.portalId === "+") &&
+          (topicParts.serviceType === wildcardParts.serviceType || wildcardParts.serviceType === "+") &&
+          (topicParts.deviceInstance === wildcardParts.deviceInstance || wildcardParts.deviceInstance === "+")
+
+        return matchesWildcard
+      })
+      .reduce((obj, key) => {
+        obj[key] = this.state.messages[key]
+        return obj
+      }, {})
+
+    return values
   }
 
   render() {
     return (
-      <MqttClientContext.Provider value={this.state.client}>
+      <MqttClientContext.Provider
+        value={{
+          client: this.state.client,
+          messages: this.state.messages,
+          getMessagesByTopics: this.getMessagesByTopics,
+          getMessagesByWildcard: this.getMessagesByWildcard
+        }}
+      >
         {this.props.children(this.state.status, this.state.error)}
       </MqttClientContext.Provider>
     )
--- src/app/mqtt/MqttTopicWildcard.js
+++ src/app/mqtt/MqttTopicWildcard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import { getMessageValue, parseTopic } from "../../service/util"
 import { MqttClientContext } from "../index.js"
 
 class MqttTopicWildcard extends Component {
@@ -12,7 +11,6 @@ class MqttTopicWildcard extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (!this.state.initialized && this.props.client) {
-      this.listenFoIncomingMessages()
       this.subscribeToTopic(this.props.wildcard)
       this.setState({ initialized: true })
       return
@@ -55,53 +53,16 @@ class MqttTopicWildcard extends Component {
     this.props.client.publish(wildcard.replace("N/", "R/"))
   }
 
-  listenFoIncomingMessages() {
-    this.props.client.on("message", (topic, message) => {
-      // console.log("<MqttTopicWildcard />  Message received: ", topic, message.toString())
-      // If the subscribe failed, do nothing
-      if (this.error) {
-        return
-      }
-
-      // Only listen to messages for the given topic
-      const topicParts = parseTopic(topic)
-      const wildcardParts = parseTopic(this.props.wildcard)
-
-      // TODO Fix check for dbuspath
-      const matchesWildcard =
-        (topicParts.dbusPath === wildcardParts.dbusPath || wildcardParts.dbusPath === "+") &&
-        (topicParts.portalId === wildcardParts.portalId || wildcardParts.portalId === "+") &&
-        (topicParts.serviceType === wildcardParts.serviceType || wildcardParts.serviceType === "+") &&
-        (topicParts.deviceInstance === wildcardParts.deviceInstance || wildcardParts.deviceInstance === "+")
-
-      if (!matchesWildcard) {
-        return
-      }
-
-      const value = getMessageValue(message)
-      console.log(`<MqttTopicWildcard /> Received ${topic} >> ${value}`)
-
-      this.setState({
-        values: {
-          ...this.state.values,
-          [topic]: {
-            ...topicParts,
-            value
-          }
-        }
-      })
-    })
-  }
-
   render() {
-    return <React.Fragment>{this.props.children(this.state.values)}</React.Fragment>
+    return <>{this.props.children(this.props.messages)}</>
   }
 }
 
 export default props => (
   <MqttClientContext.Consumer>
-    {client => {
-      return <MqttTopicWildcard {...props} client={client} />
+    {({ client, getMessagesByWildcard }) => {
+      const filteredMessages = getMessagesByWildcard(props.wildcard)
+      return <MqttTopicWildcard {...props} client={client} messages={filteredMessages} />
     }}
   </MqttClientContext.Consumer>
 )
