a994a4cfa7439041d4a1b0f91eb006bc4f63596e
Corina Udrescu <corina.udrescu@gmail.com>
2018-12-04 14:56:11 +0100

    Update test to wait for data to come in

--- cypress/integration/values.spec.js
+++ cypress/integration/values.spec.js
@@ -10,6 +10,8 @@ describe("Testing basic values rendering in the ui", () => {
   })
 
   it("Has values in all value elements", () => {
+    // Wait for data to start coming in
+    cy.wait(1000)
     cy.get("p.value").each(p => {
       assert.isNotNaN(parseInt(p.text()))
     })
