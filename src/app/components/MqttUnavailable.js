c4dfac887a2429028d1e26cbdce6d26d1fd8aa86
Corina Udrescu <corina.udrescu@gmail.com>
2018-12-04 10:48:41 +0100

    Fix mqtt unavailable page text color

--- src/app/components/MqttUnavailable.js
+++ src/app/components/MqttUnavailable.js
@@ -1,14 +1,16 @@
 import React from "react"
 
 export default props => (
-  <div className="error-page">
-    <span className="text text--large">
-      Could not connect to the MQTT server. <br />
-      Please check that MQTT is enabled in your settings: <br />
-      Remote Console > Settings > Services > MQTT.
-    </span>
-    <div className="image-container">
-      <img src={require("../../images/mqtt-settings.png")} />
+  <div className="main__container">
+    <div className="error-page">
+      <span className="text text--large">
+        Could not connect to the MQTT server. <br />
+        Please check that MQTT is enabled in your settings: <br />
+        Remote Console > Settings > Services > MQTT.
+      </span>
+      <div className="image-container">
+        <img src={require("../../images/mqtt-settings.png")} />
+      </div>
     </div>
   </div>
 )
