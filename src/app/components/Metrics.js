2a46986d33da40478950da7bf85c6ab1b375e507
Heikki Honkanen <heikki.honkanen@reaktor.com>
2019-01-02 09:26:41 +0100

    WIP: metrics-container size

--- src/app/components/Metrics.js
+++ src/app/components/Metrics.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import Battery from "./Battery"
 import ActiveSource from "./ActiveSource"
 
@@ -9,25 +9,39 @@ import Solar from "./Solar"
 
 const solarFeatureEnabled = false
 
-export default props => {
-  const { portalId, vebusInstanceId, isConnected } = props
-  return (
-    <div id="metrics-container">
-      <Battery portalId={portalId} />
-      <ActiveSource
-        portalId={portalId}
-        vebusInstanceId={vebusInstanceId}
-        onChangeShoreInputLimitClicked={props.onChangeShoreInputLimitClicked}
-      />
-      <InverterCharger
-        portalId={portalId}
-        vebusInstanceId={vebusInstanceId}
-        connected={isConnected}
-        onModeSelected={props.onModeSelected}
-      />
-      <AcLoads portalId={portalId} vebusInstanceId={vebusInstanceId} />
-      <DcLoads portalId={portalId} />
-      {solarFeatureEnabled && <Solar portalId={portalId} />}
-    </div>
-  )
+export default class Metrics extends Component {
+  ref = React.createRef()
+  state = { height: 50 } // height as CSS vh-attribute
+
+  render() {
+    const { portalId, vebusInstanceId, isConnected, onChangeShoreInputLimitClicked, onModeSelected } = this.props
+
+    if (
+      this.state.height < 100 &&
+      this.ref.current &&
+      this.ref.current.children[this.ref.current.childElementCount - 1].style.visibility === "hidden"
+    ) {
+      this.setState({ height: this.state.height + 5 })
+    }
+
+    return (
+      <div id="metrics-container" ref={this.ref} style={{ height: `${this.state.height}vh` }}>
+        <Battery portalId={portalId} />
+        <ActiveSource
+          portalId={portalId}
+          vebusInstanceId={vebusInstanceId}
+          onChangeShoreInputLimitClicked={onChangeShoreInputLimitClicked}
+        />
+        <InverterCharger
+          portalId={portalId}
+          vebusInstanceId={vebusInstanceId}
+          connected={isConnected}
+          onModeSelected={onModeSelected}
+        />
+        <AcLoads portalId={portalId} vebusInstanceId={vebusInstanceId} />
+        <DcLoads portalId={portalId} />
+        {solarFeatureEnabled && <Solar portalId={portalId} />}
+      </div>
+    )
+  }
 }
