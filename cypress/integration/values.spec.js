67377f26eb71c472f3f11e956c4f826fc18c5fdc
Heikki Honkanen <heikki.honkanen@reaktor.com>
2019-01-02 13:42:03 +0100

    Fix tests

--- cypress/integration/values.spec.js
+++ cypress/integration/values.spec.js
@@ -12,8 +12,8 @@ describe("Testing basic values rendering in the ui", () => {
   it("Has values in all value elements", () => {
     // Wait for data to start coming in
     cy.wait(1000)
-    cy.get("p.value").each(p => {
-      assert.isNotNaN(parseInt(p.text()))
+    cy.get("span.value").each(span => {
+      assert.isNotNaN(parseInt(span.text()))
     })
   })
 })
