f44a5dd7920e1b60311016d28b54f5333a4df6b6
Corina Udrescu <corina.udrescu@gmail.com>
2019-01-16 13:11:35 +0100

    Add 'power supply mode' to system modes

--- src/app/components/SystemState.js
+++ src/app/components/SystemState.js
@@ -1,20 +1,39 @@
 import React from "react"
+import { VEBUS_SYSTEM_STATE } from "../utils/constants"
 
 const systemStateFormatter = value => {
-  if (value == 0) return "Off"
-  if (value == 1) return "Low power"
-  if (value == 2) return "VE.Bus Fault condition"
-  if (value == 3) return "Bulk charging"
-  if (value == 4) return "Absorption charging"
-  if (value == 5) return "Float charging"
-  if (value == 6) return "Storage mode"
-  if (value == 7) return "Equalisation charging"
-  if (value == 8) return "Passthru"
-  if (value == 9) return "Inverting"
-  if (value == 10) return "Assisting"
-  if (value == 256) return "Discharging"
-  if (value == 257) return "Sustain"
-  return "--"
+  switch (value) {
+    case VEBUS_SYSTEM_STATE.OFF:
+      return "Off"
+    case VEBUS_SYSTEM_STATE.LOW_POWER:
+      return "Low power"
+    case VEBUS_SYSTEM_STATE.FAULT_CONDITION:
+      return "VE.Bus Fault condition"
+    case VEBUS_SYSTEM_STATE.BULK_CHARGING:
+      return "Bulk charging"
+    case VEBUS_SYSTEM_STATE.ABSORPTION_CHARGINNG:
+      return "Absorption charging"
+    case VEBUS_SYSTEM_STATE.FLOAT_CHARGING:
+      return "Float charging"
+    case VEBUS_SYSTEM_STATE.STORAGE_MODE:
+      return "Storage mode"
+    case VEBUS_SYSTEM_STATE.EQUALISATION_CHARGING:
+      return "Equalisation charging"
+    case VEBUS_SYSTEM_STATE.PASSTHRU:
+      return "Passthru"
+    case VEBUS_SYSTEM_STATE.INVERTING:
+      return "Inverting"
+    case VEBUS_SYSTEM_STATE.ASSISTING:
+      return "Assisting"
+    case VEBUS_SYSTEM_STATE.POWER_SUPPLY_MODE:
+      return "Power supply mode"
+    case VEBUS_SYSTEM_STATE.DISCHARGING:
+      return "Discharging"
+    case VEBUS_SYSTEM_STATE.SUSTAIN:
+      return "Sustain"
+    default:
+      return "--"
+  }
 }
 
 export default ({ value }) => <>{systemStateFormatter(value)}</>
