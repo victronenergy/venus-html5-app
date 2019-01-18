4dd91ccef5725e946b382980464e7403aa0f84bb
Corina Udrescu <corina.udrescu@gmail.com>
2019-01-18 11:22:39 +0100

    Configure jest mocks for assets

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testPathIgnorePatterns: ["./cypress"],
   setupTestFrameworkScriptFile: "<rootDir>test/setupTests.js",
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "identity-obj-proxy"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/test/styleMock.js"
   }
 }
