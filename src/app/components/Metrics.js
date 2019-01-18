a8c825602ee15e3059a4112d1dc98449235fa3ee
Heikki Honkanen <heikki.honkanen@reaktor.com>
2019-01-18 15:25:22 +0100

    Support column modes better in metrics-container

--- src/app/components/Metrics.js
+++ src/app/components/Metrics.js
@@ -9,15 +9,32 @@ import InverterCharger from "./InverterCharger"
 import Solar from "./Solar"
 import Chargers from "./Chargers"
 
+const hasTwoColumns = metricsContainer => {
+  if (!metricsContainer || metricsContainer.childElementCount < 2) return false
+  const [first] = metricsContainer.children
+  return [...metricsContainer.children].some(c => c.offsetTop === first.offsetTop)
+}
+
+const lastMetricIsHidden = metricsContainer => {
+  return metricsContainer.children[metricsContainer.childElementCount - 1].style.visibility === "hidden"
+}
+
 export default class Metrics extends Component {
   metricsRef = React.createRef()
-  state = { height: 10 } // height as CSS vh-attribute
+  state = { height: 10, layoutCols: 1 } // height as CSS vh-attribute
+
+  componentDidMount() {
+    if (this.state.layoutCols === 1 && hasTwoColumns(this.metricsRef.current)) {
+      this.setState({ layoutCols: 2 })
+    }
+  }
 
   componentDidUpdate() {
     if (
+      this.state.layoutCols === 2 &&
       this.state.height < 80 &&
       this.metricsRef.current &&
-      this.metricsRef.current.children[this.metricsRef.current.childElementCount - 1].style.visibility === "hidden"
+      lastMetricIsHidden(this.metricsRef.current)
     ) {
       this.setState({ height: this.state.height + 5 })
     }
@@ -26,14 +43,15 @@ export default class Metrics extends Component {
   render() {
     const { portalId, inverterChargerDeviceId, isConnected, onChangeShoreInputLimitClicked } = this.props
     const commonProps = { portalId, inverterChargerDeviceId, metricsRef: this.metricsRef }
+    const style = this.state.layoutCols === 2 ? { height: `${this.state.height}vh` } : {}
     return (
-      <div id="metrics-container" ref={this.metricsRef} style={{ height: `${this.state.height}vh` }}>
+      <div id="metrics-container" ref={this.metricsRef} style={style}>
         <Battery {...commonProps} />
-        {inverterChargerDeviceId && (
+        {!!inverterChargerDeviceId && (
           <ActiveSource {...commonProps} onChangeShoreInputLimitClicked={onChangeShoreInputLimitClicked} />
         )}
-        {inverterChargerDeviceId && <InverterCharger {...commonProps} connected={isConnected} />}
-        {inverterChargerDeviceId && <AcLoads {...commonProps} />}
+        {!!inverterChargerDeviceId && <InverterCharger {...commonProps} connected={isConnected} />}
+        {!!inverterChargerDeviceId && <AcLoads {...commonProps} />}
         <DcLoads {...commonProps} />
         <Solar {...commonProps} />
         <Chargers {...commonProps} />
