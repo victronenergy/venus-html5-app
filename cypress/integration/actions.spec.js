6577699701726b6bf0f460565849bfba53b1d8dc
Alex Imbrea <alex@elnino.tech>
2020-01-07 21:50:50 +0100

    Fix actions e2e test

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -6,10 +6,10 @@ describe("Testing basic actions in the ui", () => {
   })
 
   it("Selects inverter charger mode", () => {
-    cy.get(".charger__mode-selector > button.selector-button--active")
+    cy.get(".charger__mode-selector > .selector-button.selector-button--active")
       .as("active")
       .should("have.length", 1)
-    cy.get(".charger__mode-selector > button:not(.selector-button--active)")
+    cy.get(".charger__mode-selector > .selector-button:not(.selector-button--active)")
       .as("buttons")
       .should("have.length", 2)
     cy.get("@buttons")
@@ -22,13 +22,13 @@ describe("Testing basic actions in the ui", () => {
   })
 
   it("Selects shore input limit", () => {
-    cy.get(".metric__current-input-limit button").click()
+    cy.get(".metric__current-input-limit > .selector-button").click()
     cy.get(".amperage-selector__container").should("be.visible")
     cy.get(".metrics-container").should("not.be.visible")
-    cy.get(".amperage-selector > button.selector-button--active")
+    cy.get(".amperage-selector > .selector-button.selector-button--active")
       .as("active")
       .should("have.length", 1)
-    cy.get(".amperage-selector > button:not(.selector-button--active)").as("buttons")
+    cy.get(".amperage-selector > .selector-button:not(.selector-button--active)").as("buttons")
     cy.get("@buttons")
       .eq(0)
       .click()
