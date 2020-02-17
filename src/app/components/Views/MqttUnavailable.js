5833da467e3b1e6b0d1af8fb9e42f39365b0d9d5
Alex Imbrea <alex@elnino.tech>
2020-02-17 12:17:25 +0100

    Improve responsiveness of mqtt-instructions
    Fixes #128

--- src/app/components/Views/MqttUnavailable.js
+++ src/app/components/Views/MqttUnavailable.js
@@ -5,11 +5,11 @@ export default ({ viewUnmounting }) => (
   <main>
     <Fade unmount={viewUnmounting}>
       <div className="error-page">
-        <span className="text text--large">
+        <div className="instructions-container text--bold">
           Could not connect to the MQTT server. <br />
           Please check that MQTT is enabled in your settings: <br />
           Remote Control > Settings > Services > MQTT on LAN (Plaintext)
-        </span>
+        </div>
         <div className="image-container">
           <img src={require("../../../images/mqtt-settings-v2.42.png")} />
         </div>
