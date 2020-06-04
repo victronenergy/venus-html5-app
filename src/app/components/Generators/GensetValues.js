b18ba668ea67b2098f6e914e6a90d44abed91083
Alex Imbrea <alex@elnino.tech>
2020-06-04 14:47:10 +0200

    Add temperature to the GensetValue

--- src/app/components/Generators/GensetValues.js
+++ src/app/components/Generators/GensetValues.js
@@ -25,27 +25,43 @@ const getTopics = portalId => {
       `N/${portalId}/genset/0/Ac/L1/Frequency`,
       `N/${portalId}/genset/0/Ac/L2/Frequency`,
       `N/${portalId}/genset/0/Ac/L3/Frequency`
-    ]
+    ],
+    coolant: `N/${portalId}/genset/0/Engine/CoolantTemperature`,
+    winding: `N/${portalId}/genset/0/Engine/WindingTemperature`,
+    exhaust: `N/${portalId}/genset/0/Engine/ExaustTemperature`
   }
 }
 
-export const ActiveInTotalValues = ({ voltage, current, power, frequency, threePhase }) => {
+export const ActiveInTotalValues = ({ voltage, current, power, frequency, coolant, winding, exhaust, threePhase }) => {
+  const temperatures = (
+    <ListRow key="temperatures">
+      <span className="value value__temperature">Coolant</span>
+      <NumericValue value={coolant} unit="°C" />
+      <span className="value value__temperature">Winding</span>
+      <NumericValue value={winding} unit="°C" />
+      <span className="value value__temperature">Exhaust</span>
+      <NumericValue value={exhaust} unit="°C" />
+    </ListRow>
+  )
   return threePhase ? (
-    voltage.map((v, i) => (
-      <ListRow key={i}>
-        <span className="value value__phase">L {i + 1}</span>
-        <NumericValue value={v} unit="V" />
-        <NumericValue value={current[i]} unit="A" precision={1} />
-        <NumericValue value={power[i]} unit="W" />
-        <NumericValue value={frequency[i]} unit="Hz" />
-      </ListRow>
-    ))
+    voltage
+      .map((v, i) => (
+        <ListRow key={i}>
+          <span className="value value__phase">L {i + 1}</span>
+          <NumericValue value={v} unit="V" />
+          <NumericValue value={current[i]} unit="A" precision={1} />
+          <NumericValue value={power[i]} unit="W" />
+          <NumericValue value={frequency[i]} unit="Hz" />
+        </ListRow>
+      ))
+      .concat(temperatures)
   ) : (
     <>
       <NumericValue value={voltage[0]} unit={"V"} />
       <NumericValue value={current[0]} unit="A" precision={1} />
       <NumericValue value={power[0]} unit="W" />
       <NumericValue value={frequency[0]} unit="Hz" />
+      <div className="metric__values__temperatures">{temperatures}</div>
     </>
   )
 }
