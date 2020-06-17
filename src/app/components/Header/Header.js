d16a24a4da23002faad21190f85a1703d0babdfb
Alex Imbrea <alex@elnino.tech>
2020-06-17 12:30:55 +0200

    Add remote console button without MQTT

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,24 +1,17 @@
 import React, { Component } from "react"
-import classnames from "classnames"
-
 import MqttSubscriptions from "../../mqtt/MqttSubscriptions"
-
 import { VIEWS } from "../../utils/constants"
 
 import "./Header.scss"
 
 const Header = props => {
-  const { isConnected, showRemoteConsoleSetting, currentView, handleRemoteConsoleButtonClicked } = props
+  const { showRemoteConsoleSetting, currentView, handleRemoteConsoleButtonClicked } = props
   return (
     <header>
       <img src={require("../../../images/icons/logo.png")} className="logo" />
       <div className="connection">
         {showRemoteConsoleSetting && (
-          <button
-            className="remote-console-button text text--small"
-            onClick={handleRemoteConsoleButtonClicked}
-            disabled={!isConnected}
-          >
+          <button className="remote-console-button text text--small" onClick={handleRemoteConsoleButtonClicked}>
             {currentView !== VIEWS.REMOTE_CONSOLE ? "Remote Console" : "Close"}
           </button>
         )}
@@ -29,14 +22,13 @@ const Header = props => {
 
 class HeaderWithData extends Component {
   render() {
-    const { portalId, isConnected, currentView, handleRemoteConsoleButtonClicked } = this.props
+    const { portalId, currentView, handleRemoteConsoleButtonClicked } = this.props
     return (
       <MqttSubscriptions topics={{ showRemoteConsoleSetting: `N/${portalId}/settings/0/Settings/System/VncLocal` }}>
         {topics => {
           return (
             <Header
               showRemoteConsoleSetting={!!topics.showRemoteConsoleSetting}
-              isConnected={isConnected}
               handleRemoteConsoleButtonClicked={handleRemoteConsoleButtonClicked}
               currentView={currentView}
             />
@@ -47,4 +39,19 @@ class HeaderWithData extends Component {
   }
 }
 
+class HeaderWithoutMQTTData extends Component {
+  render() {
+    const { currentView, handleRemoteConsoleButtonClicked } = this.props
+    return (
+      <Header
+        showRemoteConsoleSetting={true}
+        handleRemoteConsoleButtonClicked={handleRemoteConsoleButtonClicked}
+        currentView={currentView}
+      />
+    )
+  }
+}
+
 export default HeaderWithData
+
+export { HeaderWithData, HeaderWithoutMQTTData }
