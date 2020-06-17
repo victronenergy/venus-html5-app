25940fc697ddbca1dbbc21fba1873f15174ce2a6
Alex Imbrea <alex@elnino.tech>
2020-06-17 14:54:32 +0200

    Change MQTT/Remote Console instructions

--- src/app/components/Views/MqttUnavailable.js
+++ src/app/components/Views/MqttUnavailable.js
@@ -8,9 +8,8 @@ export default ({ viewUnmounting }) => (
         <div className="instructions-container text--bold">
           Could not connect to the MQTT server. <br />
           Please check that MQTT is enabled in your settings: <br />
-          Remote Control > Settings > Services > MQTT on LAN (Plaintext) <br />
-          You can try to use the Remote Console button located at the top of this screen to change your settings
-          remotely.
+          <i>Remote Control > Settings > Services > MQTT on LAN (Plaintext)</i> <br />
+          Try using the Remote Console button at the top of this screen to change your settings remotely.
         </div>
         <div className="image-container">
           <img src={require("../../../images/mqtt-settings-v2.42.png")} />
