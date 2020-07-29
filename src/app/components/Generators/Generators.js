9c60a8fa0bc314468e5d83c3e7b9c3be2b4f81d5
Alex Imbrea <alex@elnino.tech>
2020-07-29 14:51:00 +0200

    Add combined ActiveSource and Generator metric

--- src/app/components/Generators/Generators.js
+++ src/app/components/Generators/Generators.js
@@ -2,40 +2,23 @@ import React from "react"
 
 import GeneratorFp from "./GeneratorFp"
 import GeneratorRelay from "./GeneratorRelay"
-import MqttSubscriptions from "../../mqtt/MqttSubscriptions"
-import { RELAY_FUNCTION } from "../../utils/constants"
 
-const getTopics = portalId => {
-  return {
-    relayFunction: `N/${portalId}/settings/0/Settings/Relay/Function`,
-    gensetCode: `N/${portalId}/genset/0/StatusCode`,
-    generatorState: `N/${portalId}/generator/0/Generator0/State`
-  }
-}
-
-const Generators = ({ portalId, metricsRef }) => (
-  <MqttSubscriptions topics={getTopics(portalId)}>
-    {topics => (
-      <>
-        {topics.gensetCode !== undefined && (
-          <GeneratorFp
-            portalId={portalId}
-            metricsRef={metricsRef}
-            autoStartStopTopic={`W/${portalId}/settings/0/Settings/Services/FischerPandaAutoStartStop`}
-            manualStartStopTopic={`W/${portalId}/genset/0/Start`}
-          />
-        )}
-        {topics.relayFunction === RELAY_FUNCTION.GENERATOR_START_STOP && topics.generatorState !== undefined && (
-          <GeneratorRelay
-            portalId={portalId}
-            metricsRef={metricsRef}
-            autoStartStopTopic={`W/${portalId}/settings/0/Settings/Generator0/AutoStartEnabled`}
-            manualStartStopTopic={`W/${portalId}/generator/0/Generator0/ManualStart`}
-          />
-        )}
-      </>
-    )}
-  </MqttSubscriptions>
+const Generators = ({ portalId, inverterChargerDeviceId, metricsRef }) => (
+  <>
+    <GeneratorFp
+      portalId={portalId}
+      metricsRef={metricsRef}
+      autoStartStopTopic={`W/${portalId}/settings/0/Settings/Services/FischerPandaAutoStartStop`}
+      manualStartStopTopic={`W/${portalId}/genset/0/Start`}
+    />
+    <GeneratorRelay
+      portalId={portalId}
+      metricsRef={metricsRef}
+      autoStartStopTopic={`W/${portalId}/settings/0/Settings/Generator0/AutoStartEnabled`}
+      manualStartStopTopic={`W/${portalId}/generator/0/Generator0/ManualStart`}
+      inverterChargerDeviceId={inverterChargerDeviceId}
+    />
+  </>
 )
 
 export default Generators
