673b0fcf18d2240048f5ce81abb6aa32b2b6eee6
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-04-21 15:13:12 +0200

    WIP: Add new directory with views for KVNRV

---
+++ src/app/KVNRV/components/Views/Connecting/Connecting.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Fade from "../../../../components/Fade"
+
+import "./Connecting.scss"
+
+const Connecting = ({ viewUnmounting }) => (
+  <main>
+    <Fade unmount={viewUnmounting}>
+      <div className="connecting">
+        <p className="text text--very-large">Connecting</p>
+        <div className="connecting__dots">
+          <p className="dot">.</p>
+          <p className="dot two">.</p>
+          <p className="dot three">.</p>
+        </div>
+      </div>
+    </Fade>
+  </main>
+)
+
+export default Connecting;
---
+++ src/app/KVNRV/components/Views/MqttUnavailable.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Fade from "../../../components/Fade"
+
+const MqttUnavailable = ({ viewUnmounting }) => (
+  <main>
+    <Fade unmount={viewUnmounting}>
+      <div className="error-page">
+        <div className="instructions-container text--bold">
+          Could not connect to the MQTT server. <br />
+          Please check that MQTT is enabled in your settings: <br />
+          <i>Remote Console &gt; Settings &gt; Services &gt; MQTT on LAN (Plaintext)</i> <br />
+          Try using the Remote Console button at the top of this screen to change your settings remotely.
+        </div>
+        <div className="image-container">
+          <img src={require("../../../../images/mqtt-settings-v2.42.png")} alt={"MQTT Settings Guide"}/>
+        </div>
+      </div>
+    </Fade>
+  </main>
+)
+
+export default MqttUnavailable;
+
