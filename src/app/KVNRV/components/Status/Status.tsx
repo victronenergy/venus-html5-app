6b625833950cb9cfe9a7570d8498f956ec820d2c
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-04-23 17:56:03 +0200

    Rework and implement some of the Status card (KVNRV)

--- src/app/KVNRV/components/Status/Status.tsx
+++ src/app/KVNRV/components/Status/Status.tsx
@@ -1,19 +1,46 @@
 import React  from "react"
 
-import { Card, SIZE_BIG } from "../Card"
-import { NotAvailable } from "../NotAvailable"
+import { Card, SIZE_BIG, SIZE_LONG } from "../Card"
+import { Footer } from "../Card/Card"
+import IconWarning from "../../images/IconWarning.svg"
+import IconAlarm from "../../images/IconWarning.svg"
+import IconClose from "../../images/IconClose.svg"
+import { STATUS_LEVELS, StatusUpdate } from "../Views/Metrics"
 import "./Status.scss"
 
-export const Status = () => {
+type StatusProps = {
+  status_updates: Array<StatusUpdate>
+}
+
+export const Status = (props: StatusProps) => {
+  const footer: Footer = {status: STATUS_LEVELS.SUCCESS, property: "Connection"}
+
   return (
     <div className="metrics__status">
-      <Card title={'Status'} icon={undefined} size={SIZE_BIG}>
-        <div className="gauge">
-          <NotAvailable />
+      <Card title={'Status'} size={[SIZE_BIG, SIZE_LONG]} footer={footer}>
+        <div className={"title"}>Penny's House</div>
+        <div className={"subheading row"}>
+          <div className={"subheading__model"}>Class A Motor Home</div>
+          <div className={"subheading__year"}>Model: 2016</div>
+        </div>
+
+        <div className={"status-updates"}>
+          {props.status_updates && props.status_updates.map(update => (
+            <div className={"status-update row " + update.level} key={'status-update-' + update.part}>
+              <span className={"row align-items-center"}>
+                <div className={"status-update__icon"}>
+                  <img src={update.level === STATUS_LEVELS.WARNING ? IconWarning : IconAlarm}  alt={"Status update icon"}/>
+                </div>
+
+                <span>{update.part}: </span>
+                <span>{update.message}</span>
+              </span>
+
+              <img src={IconClose} alt={"Close icon"}/>
+            </div>
+          ))}
         </div>
       </Card>
     </div>
   )
 }
-
-export default Status
--- src/app/KVNRV/components/Status/index.js
+++ src/app/KVNRV/components/Status/index.js
@@ -1,1 +1,1 @@
-export { default } from "./Status"
+export { Status } from "./Status"
