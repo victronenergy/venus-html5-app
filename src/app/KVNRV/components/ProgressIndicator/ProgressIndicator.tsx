fbf54727d230e566d90262fec700df719250fe07
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-04-28 11:53:41 +0200

    Add a progress indicator component and vars (KVNRV)

---
+++ src/app/KVNRV/components/ProgressIndicator/ProgressIndicator.tsx
@@ -0,0 +1,19 @@
+import React  from "react"
+import './ProgressIndicator.scss'
+
+type ProgressIndicatorProps = {
+  percent: number,
+  level: string
+}
+
+const ProgressIndicator = (props: ProgressIndicatorProps) => {
+  const styles = {width: (props.percent * 100) + "%" }
+
+  return (
+    <div className="progress-indicator">
+      <div className={"progress-indicator__bar " + props.level} style={styles} />
+    </div>
+  )
+}
+
+export default ProgressIndicator
