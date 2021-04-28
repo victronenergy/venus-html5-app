b81991308e06cca99410f111b0e34f44f88a4db5
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-04-28 11:56:44 +0200

    Implement mock waste water card to show progress ind. + general water provider  (KVNRV)

--- src/app/KVNRV/components/WasteWater/WasteWater.tsx
+++ src/app/KVNRV/components/WasteWater/WasteWater.tsx
@@ -4,15 +4,42 @@ import "./WasteWater.scss"
 import { NotAvailable } from "../NotAvailable"
 import { CommonProps, STATUS_LEVELS } from "../Views/Metrics"
 import { Footer } from "../Card/Card"
+import NumericValue from "../../../components/NumericValue"
+import { useWater } from "../../../modules/Water/Water.provider"
+import ProgressIndicator from "../ProgressIndicator"
+import { WASTE_WATER_CONF } from "../../constants/constants"
 
 export const WasteWater = (props: CommonProps) => {
+  const {waste_water} = useWater()
+
   const footer: Footer = {status: STATUS_LEVELS.SUCCESS, property: "Status"}
+
+  let level = '';
+  if (waste_water) {
+    level = waste_water.level < WASTE_WATER_CONF.THRESHOLDS[0] ? STATUS_LEVELS.SUCCESS : (
+      waste_water.level < WASTE_WATER_CONF.THRESHOLDS[0] + WASTE_WATER_CONF.THRESHOLDS[1] ? STATUS_LEVELS.WARNING : STATUS_LEVELS.ALARM
+    )
+  }
+
 
   return (
     <div className="">
       <Card title={'Waste Water'} size={SIZE_SMALL} footer={footer}>
         <div className="gauge">
-          <NotAvailable />
+          {waste_water ? (
+            <div className={'waste-water'}>
+              <div className="indicator-main">
+              <span>
+                <NumericValue value={waste_water.level * 100} unit="%" defaultValue={'--'} precision={0} />
+                <span className="name">
+                  {waste_water.size + ' gal.'}
+                </span>
+              </span>
+              </div>
+
+              <ProgressIndicator percent={waste_water.level} level={level} />
+            </div>
+          ) : (<NotAvailable />)}
         </div>
       </Card>
     </div>
--- src/app/modules/Water/Water.provider.ts
+++ src/app/modules/Water/Water.provider.ts
@@ -1,34 +1,35 @@
 import {useObservableState, useSubscription} from 'observable-hooks'
 import {map} from 'rxjs/operators'
-import {mqttQuery} from '../Mqtt'
-import {useMqtt} from '../Mqtt/Mqtt.provider'
+import { mqttQuery, PortalId } from "../Mqtt"
+import { useMqtt, useTopicSubscriptions, useTopicsWithPortalId } from "../Mqtt/Mqtt.provider"
+import { DcLoadsTopics } from "../DcLoads"
 
-export interface FreshWaterType {
+export interface WaterType {
   volume: number,
   size: number,
   level: number
 }
 
 export interface WaterState {
-  fresh_water: FreshWaterType | undefined
+  fresh_water: WaterType | undefined
+  waste_water: WaterType | undefined
+  black_water: WaterType | undefined
 }
 
 export function useWater (): WaterState {
-  const portalId = useObservableState(mqttQuery.portalId$)
-  const mqttService = useMqtt()
-
-  const topics = {
+  const getTopics = (portalId: PortalId) => ({
     fresh_water: `N/${portalId}/system/0/Water`,
-  }
-
-  useSubscription(mqttQuery.portalId$, () => {
-    mqttService.subscribeToTopics(topics)
-
-    return () => mqttService.unsubscribeFromTopics(topics)
+    waste_water: `N/${portalId}/system/0/Water`,
+    black_water: `N/${portalId}/system/0/Water`,
   })
+  const topics$ = useTopicsWithPortalId<DcLoadsTopics>(getTopics, mqttQuery.portalId$)
+  const topics = useObservableState(topics$, {})
 
+  // useTopicSubscriptions(topics$)
 
-  const fresh_water = {volume: 2, size: 9, level: 0} as FreshWaterType
+  const fresh_water = {volume: 2, size: 9, level: 0} as WaterType
+  const waste_water = {volume: 2, size: 9, level: 0.4} as WaterType
+  const black_water = {volume: 2, size: 9, level: 0.4} as WaterType
 
-  return {fresh_water}
+  return {fresh_water, waste_water, black_water}
 }
