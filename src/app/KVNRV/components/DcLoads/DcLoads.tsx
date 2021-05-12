70b4b892335790f7b8d6268790ca04268353ede0
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-05-12 14:45:20 +0200

    Refactor the DcLoads component

--- src/app/KVNRV/components/DcLoads/DcLoads.tsx
+++ src/app/KVNRV/components/DcLoads/DcLoads.tsx
@@ -1,10 +1,9 @@
 import React from "react"
-import { Card, SIZE_SMALL } from "../Card"
-import DonutIndicator from "../DonutIndicator"
-import { useDcLoads } from "../../../modules/DcLoads"
-import { DC_CONF } from "../../constants/constants"
+import { Card, SIZE_SMALL } from "../../../components/Card"
+import DonutIndicator from "../../../components/DonutIndicator"
+import { useDcLoads } from "../../../modules"
+import { DC_CONF } from "../../utils/constants"
 
-import { NotAvailable } from "../NotAvailable"
 import { CommonProps } from "../Views/Metrics"
 import "./DcLoads.scss"
 import NumericValue from "../../../components/NumericValue"
@@ -12,29 +11,17 @@ import NumericValue from "../../../components/NumericValue"
 export const DcLoads = (props: CommonProps) => {
   const { voltage, power } = useDcLoads()
 
-  let v = 0,
-    p = 0
-  if (voltage && power) {
-    v = voltage[0] || 0
-    p = power[0] || 0
-  }
-
-  let normalized_power = (p || 0) / DC_CONF.MAX
+  let normalized_power = (power || 0) / DC_CONF.MAX
   normalized_power = Math.max(Math.min(normalized_power, 1), 0)
 
   return (
     <div className="">
       <Card title={"DC Loads"} size={SIZE_SMALL}>
         <div className="gauge">
-          {power ? (
-            <DonutIndicator value={p} percent={normalized_power} parts={DC_CONF.THRESHOLDS} unit={"W"} />
-          ) : (
-            <NotAvailable />
-          )}
-
+          <DonutIndicator value={power} percent={normalized_power} parts={DC_CONF.THRESHOLDS} unit={"W"} />
           <div className={"info-bar"}>
             <div className={"info-bar__cell"}>
-              <NumericValue value={v} unit={"A"} precision={0} />
+              <NumericValue value={voltage} unit={"A"} precision={0} />
             </div>
           </div>
         </div>
