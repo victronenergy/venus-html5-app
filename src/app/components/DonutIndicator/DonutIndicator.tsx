25358f78b268dd51fc67979072f40ff384302a08
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-05-13 15:43:10 +0200

    Ensure that donut indicator doesn't run into issues

--- src/app/components/DonutIndicator/DonutIndicator.tsx
+++ src/app/components/DonutIndicator/DonutIndicator.tsx
@@ -125,15 +125,20 @@ export class DonutIndicator extends PureComponent<DonutIndicatorProps> {
   }
 
   getColors() {
-    const container = getComputedStyle(document.querySelector("#root > .container") as Element)
+    const selected = document.querySelector("#root > .container")
+    if (selected) {
+      const container = getComputedStyle(selected as Element)
 
-    const colorGray = container.getPropertyValue("--color-lightgray")
-    const colorRed = container.getPropertyValue("--color-red")
-    const colorYellow = container.getPropertyValue("--color-yellow")
-    const colorGreen = container.getPropertyValue("--color-green")
-    const textColor = container.getPropertyValue("--text-color-main")
+      const colorGray = container.getPropertyValue("--color-lightgray")
+      const colorRed = container.getPropertyValue("--color-red")
+      const colorYellow = container.getPropertyValue("--color-yellow")
+      const colorGreen = container.getPropertyValue("--color-green")
+      const textColor = container.getPropertyValue("--text-color-main")
 
-    return { colorGray, colorRed, colorYellow, colorGreen, textColor }
+      return { colorGray, colorRed, colorYellow, colorGreen, textColor }
+    }
+
+    return { colorGray: "#000", colorRed: "#000", colorYellow: "#000", colorGreen: "#000", textColor: "#000" }
   }
 
   updateInner() {
