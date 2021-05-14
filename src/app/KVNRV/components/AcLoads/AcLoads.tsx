2c2522f6909e87c957d5ca4855274aa08191b9c8
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-05-14 16:38:29 +0200

    Render whole card if mqtt dat unavailable

--- src/app/KVNRV/components/AcLoads/AcLoads.tsx
+++ src/app/KVNRV/components/AcLoads/AcLoads.tsx
@@ -13,25 +13,25 @@ export const AcLoads = () => {
   const normalizedPower = normalizePower(power && power[0] ? power[0] : 0, AC_CONF.MAX)
   useSendUpdate(normalizedPower, AC_CONF, "AC Loads")
 
-  if (!(current && voltage && power && frequency)) {
-    return <NotAvailable />
-  }
-
   return (
     <div className="">
       <Card title={"AC Loads"} size={SIZE_SMALL}>
         <div className="gauge">
-          <DonutIndicator value={power[0] ?? 0} percent={normalizedPower} parts={AC_CONF.THRESHOLDS} unit={"W"} />
+          {power ? (
+            <DonutIndicator value={power[0] ?? 0} percent={normalizedPower} parts={AC_CONF.THRESHOLDS} unit={"W"} />
+          ) : (
+            <NotAvailable />
+          )}
 
           <div className={"info-bar"}>
             <div className={"info-bar__cell"}>
-              <NumericValue value={voltage[0] ?? 0} unit={"V"} precision={0} />
+              <NumericValue value={voltage ? voltage[0] : undefined} unit={"V"} precision={0} />
             </div>
             <div className={"info-bar__cell"}>
-              <NumericValue value={current[0] ?? 0} unit={"A"} precision={0} />
+              <NumericValue value={current ? current[0] : undefined} unit={"A"} precision={0} />
             </div>
             <div className={"info-bar__cell"}>
-              <NumericValue value={frequency[0] ?? 0} unit={"Hz"} precision={0} />
+              <NumericValue value={frequency ? frequency[0] : undefined} unit={"Hz"} precision={0} />
             </div>
           </div>
         </div>
