cdeb37f3366f9097ca924b97806c4fb939019249
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-05-20 17:09:47 +0200

    Add a helper function

--- src/app/KVNRV/utils/helpers.ts
+++ src/app/KVNRV/utils/helpers.ts
@@ -1,3 +1,7 @@
 export const normalizePower = (power: number, max: number) => {
   return Math.max(Math.min(power / max, 1), 0)
 }
+
+export const sum = (arr: number[]) => {
+  return arr.reduce((a, b) => a + b)
+}
