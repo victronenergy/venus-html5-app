f87784382add7424f3a306f5cb29f4d5221480d0
JakubOrlinskiElnino <81819014+JakubOrlinskiElnino@users.noreply.github.com>
2021-05-26 18:33:51 +0200

    Add providers for MQTT alarms from different places

---
+++ src/app/modules/Status/InverterAlarms.provider.ts
@@ -0,0 +1,43 @@
+import { mqttQuery, PortalId, Topics } from "../Mqtt"
+import { useTopicsState, useTopicSubscriptions, useTopicsWithPortalId } from "../Mqtt/Mqtt.provider"
+
+export interface InverterAlarmsState {
+  lowVoltage: number
+  highVoltage: number
+  lowTemperature: number
+  highTemperature: number
+  overload: number
+  ripple: number
+  lowVoltageAcOut: number
+  highVoltageAcOut: number
+}
+
+export interface InverterAlarmsTopics extends Topics {
+  lowVoltage?: string
+  highVoltage?: string
+  lowTemperature?: string
+  highTemperature?: string
+  overload?: string
+  ripple?: string
+  lowVoltageAcOut?: string
+  highVoltageAcOut?: string
+}
+
+export function useInverterAlarms(): InverterAlarmsState {
+  const getTopics = (portalId: PortalId) => ({
+    lowVoltage: `N/${portalId}/inverter/Alarms/LowVoltage`,
+    highVoltage: `N/${portalId}/inverter/Alarms/HighVoltage`,
+    lowTemperature: `N/${portalId}/inverter/Alarms/LowTemperature`,
+    highTemperature: `N/${portalId}/inverter/Alarms/HighTemperature`,
+    overload: `N/${portalId}/inverter/Alarms/Overload`,
+    ripple: `N/${portalId}/inverter/Alarms/Ripple`,
+    lowVoltageAcOut: `N/${portalId}/inverter/Alarms/LowVoltageAcOut`,
+    highVoltageAcOut: `N/${portalId}/inverter/Alarms/HighVoltageAcOut`,
+  })
+
+  const topics$ = useTopicsWithPortalId<InverterAlarmsTopics>(getTopics, mqttQuery.portalId$)
+
+  useTopicSubscriptions(topics$)
+
+  return useTopicsState<InverterAlarmsState>(topics$)
+}
---
+++ src/app/modules/Status/VebusAlarms.provider.ts
@@ -0,0 +1,51 @@
+import { mqttQuery, PortalId, Topics } from "../Mqtt"
+import { useTopicsState, useTopicSubscriptions, useTopicsWithParameters } from "../Mqtt/Mqtt.provider"
+import { InstanceId, vebusQuery } from "../Vebus"
+
+export interface VebusAlarmsState {
+  voltage: number
+  power: number
+  current: number
+}
+
+export interface AlarmsTopics extends Topics {
+  power?: string
+  voltage?: string
+  current?: string
+}
+
+export function useVebusAlarms(): VebusAlarmsState {
+  const getTopics = (portalId: PortalId, instanceId: InstanceId) => ({
+    lowBattery: `N/${portalId}/vebus/${instanceId}/Alarms/LowBattery`,
+    phaseRotation: `N/${portalId}/vebus/${instanceId}/Alarms/PhaseRotation`,
+    ripple: `N/${portalId}/vebus/${instanceId}/Alarms/Ripple`,
+    batteryTemperatureSensor: `N/${portalId}/vebus/${instanceId}/Alarms/TemperatureSensor`,
+
+    phaseHighTemperature: [
+      `N/${portalId}/vebus/${instanceId}/Alarms/L1/HighTemperature`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L2/HighTemperature`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L3/HighTemperature`,
+    ],
+    phaseLowBattery: [
+      `N/${portalId}/vebus/${instanceId}/Alarms/L1/LowBattery`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L2/LowBattery`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L3/LowBattery`,
+    ],
+    phaseOverload: [
+      `N/${portalId}/vebus/${instanceId}/Alarms/L1/Overload`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L2/Overload`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L3/Overload`,
+    ],
+    phaseRipple: [
+      `N/${portalId}/vebus/${instanceId}/Alarms/L1/Ripple`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L2/Ripple`,
+      `N/${portalId}/vebus/${instanceId}/Alarms/L3/Ripple`,
+    ],
+  })
+
+  const topics$ = useTopicsWithParameters<AlarmsTopics>(getTopics, mqttQuery.portalId$, vebusQuery.instanceId$)
+
+  useTopicSubscriptions(topics$)
+
+  return useTopicsState<VebusAlarmsState>(topics$)
+}
