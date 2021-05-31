0a05d4ff768475680ae1eeb66879bee679568304
Alex Imbrea <alex@elnino.tech>
2021-05-31 15:13:54 +0200

    Set the precision for kW to 1 to fit inside the gauge

--- src/app/components/NumericValue/NumericValue.js
+++ src/app/components/NumericValue/NumericValue.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 
-export const formatNumber = ({ value, unit = "", precision = 0, factor = 1.0, defaultValue = "--" }) => {
+export const formatNumber = ({ value, unit = "", precision = 0, factor = 1.0, defaultValue = " - " }) => {
   if (value === null || value === undefined || isNaN(value)) {
     return defaultValue + unit
   }
@@ -8,14 +8,14 @@ export const formatNumber = ({ value, unit = "", precision = 0, factor = 1.0, de
   if (Math.abs(numericValue) > 1000) {
     numericValue = numericValue / 1000
     unit = "k" + unit
-    precision = 2
+    precision = 1
   }
   return precision === undefined ? numericValue.toString() + unit : numericValue.toFixed(precision) + unit
 }
 
 export default class NumericValue extends Component {
   render() {
-    const { value, unit = "", precision = 0, factor = 1.0, defaultValue = "--" } = this.props
+    const { value, unit = "", precision = 0, factor = 1.0, defaultValue = " - " } = this.props
     return <span className="value">{formatNumber({ value, unit, precision, factor, defaultValue })}</span>
   }
 }
