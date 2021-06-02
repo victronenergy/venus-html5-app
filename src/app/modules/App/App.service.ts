1298e09e8632755e5998abcc2eec3b0cf2bdafd8
Kristo Lippur <kristo@elnino.tech>
2021-06-02 16:21:20 +0200

    Persist locked state in MQTT.

--- src/app/modules/App/App.service.ts
+++ src/app/modules/App/App.service.ts
@@ -1,9 +1,20 @@
+import { MqttService, mqttStore } from "../Mqtt"
+import { LOCK_STATE_TOPIC } from "./App.provider"
 import { appStore, AppStore } from "./App.store"
 
 export class AppService {
   constructor(protected store: AppStore) {}
 
-  toggleLocked = () => this.store.update(({ locked }) => ({ locked: !locked }))
+  toggleLocked = () => {
+    this.store.update(({ locked }) => {
+      const mqttService = new MqttService(mqttStore)
+      mqttService.publish(LOCK_STATE_TOPIC, +!locked)
+      return { locked: !locked }
+    })
+  }
+  setLockedWithoutPersistence = (locked: boolean) => {
+    this.store.update({ locked })
+  }
   setPage = (page: string) => this.store.update({ page })
   setRemote = (remote: boolean) => this.store.update({ remote })
   toggleRemote = () => this.store.update(({ remote }) => ({ remote: !remote }))
