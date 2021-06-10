363e79d374d31a7bf068b6a731e20b8d49727d0e
Alex Imbrea <alex@elnino.tech>
2021-06-10 11:16:31 +0200

    Add SW skipWaiting and update logic

--- src/index.tsx
+++ src/index.tsx
@@ -32,7 +32,18 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    if (registration && registration.waiting) {
+      await registration.unregister()
+      // Makes Workbox call skipWaiting()
+      registration.waiting.postMessage({ type: "SKIP_WAITING" })
+      // Once the service worker is unregistered, we can reload the page to let
+      // the browser download a fresh copy of our app (invalidating the cache)
+      window.location.reload()
+    }
+  },
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
