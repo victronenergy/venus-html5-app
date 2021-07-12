c3cbdbece6f0e64cf20de32f89da6ed40c6f9236
Kristo Lippur <kristo@elnino.tech>
2021-07-12 16:55:31 +0200

    Fix CI no-console error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["react-app", "react-app/jest", "plugin:prettier/recommended"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": [
       "error",
