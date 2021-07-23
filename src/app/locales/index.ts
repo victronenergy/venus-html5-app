9006a002e88457b552e51c1f0393213f03bbaa80
Nicolae Mihălache <nicolaemihalache@Nicolaes-MacBook-Pro--El-Nino.local>
2021-07-23 13:11:21 +0300

    fallback to default language when key not found; console.warn when the key is not found in the default obj

--- src/app/locales/index.ts
+++ src/app/locales/index.ts
@@ -1,13 +1,26 @@
-import { setTranslations, setLocale } from "react-i18nify"
+import { setTranslations, setLocale, setHandleMissingTranslation, translate } from "react-i18nify"
 import { __LOCAL_STORAGE_LANG_KEY__, LANGAUGES, DEFAULT_LANGUAGE } from "./constants"
+import { get } from "lodash-es"
 
-const languagesJSONS = LANGAUGES.reduce(
+type LangObj = {
+  [key: string]: Record<string, any>
+}
+
+const languagesJSONS: LangObj = LANGAUGES.reduce(
   (rx, l) => ({
     ...rx,
     [l]: require(`./languages/${l}.json`),
   }),
   {}
 )
+
+setHandleMissingTranslation((key, replacements) => {
+  if (!get(languagesJSONS[DEFAULT_LANGUAGE], key)) {
+    console.warn(`Missing translation for ${key}`)
+    return `Missing translation for ${key}`
+  }
+  return translate(key, replacements, { locale: DEFAULT_LANGUAGE })
+})
 
 setTranslations(languagesJSONS)
 
