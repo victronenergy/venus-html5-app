496793daa45dc111d70981305a1ecf37f3f07202
Alex Imbrea <alex@elnino.tech>
2021-08-20 14:22:52 +0200

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -29,6 +29,6 @@ HOST="$1"
 echo "Building app.."
 npm run build
 
-echo "Uploading build/* to ${USERNAME}@${HOST}:/var/www/venus/app"
+echo "Uploading build/* to ${USERNAME}@${HOST}:/data/www/app/"
 
-rsync --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -r dist/* ${USERNAME}@${HOST}:/var/www/venus/app
+rsync --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -r dist/* ${USERNAME}@${HOST}:/data/www/app/
