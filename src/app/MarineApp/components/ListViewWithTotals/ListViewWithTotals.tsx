80230188a5321b9be0c5c1b6b8e24e38d594b4ac
JoshuaDodds <jdodds@local>
2022-07-25 12:45:48 +0200

    adjust position of header totals and remove css artifacts from previous layout

--- src/app/MarineApp/components/ListViewWithTotals/ListViewWithTotals.tsx
+++ src/app/MarineApp/components/ListViewWithTotals/ListViewWithTotals.tsx
@@ -23,15 +23,9 @@ export const ListViewWithTotals: React.FunctionComponent<ListViewWithTotalsProps
     <div className="list-view__header">
       <img src={icon} className="metric__icon" alt={"List View Icon"} />
       <div className="metric__value-container">
-        <div className="widget--header">
-          <div className="text text--title text--bold">
-            <p className="text--title--left">{title}</p>
-          </div>
-          <div className="text text--title text--bold">
-            <p className="text--power--totals">{totals.toFixed()} W</p>
-          </div>
-        </div>
-        {subTitle && <span className="text text--smaller text--opaque">{subTitle}</span>}
+        <div className="text--title text--title--left">{title}</div>
+        {subTitle && <span className="text--very-small text--opaque text--subtitle-upper">{subTitle}</span>}
+        <div className="text text--bold">{totals.toFixed()} W</div>
       </div>
     </div>
     {children && <div className="list-view__rows">{children}</div>}
