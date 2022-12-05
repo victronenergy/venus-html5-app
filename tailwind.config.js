cf1415433d7d3c3b884ea982b7d6cf31dd169b0e
Sergey Nechaev <sergey@elnino.tech>
2022-12-05 17:45:41 +0100

    Uninstall unused tailwind typography and form plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
   ],
