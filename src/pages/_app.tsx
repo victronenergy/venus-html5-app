6aef803cc02a8671ee0b96fe640f798f2740ae34
Sergey Nechaev <sergey@elnino.tech>
2022-12-05 23:31:51 +0100

    Configure common persistable settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "~/styles/global.css"
 import type { AppProps } from "next/app"
 import { MuseoSans } from "~/fonts"
+import { configurePersistable } from 'mobx-persist-store'
+import * as localForage from 'localforage'
+
+// Common configuration for MobX persistable stores, you can override them in each store configuration
+configurePersistable({
+  storage: typeof window !== 'undefined' ? localForage : undefined,
+  expireIn: 1000 * 60 * 60 * 24 * 180, // 180 days (ms),
+  removeOnExpiration: true,
+  stringify: false,
+  // debugMode: true, // switch it on to have console debug messages
+})
 
 const MfdApp = ({ Component, pageProps }: AppProps) => {
   return (
