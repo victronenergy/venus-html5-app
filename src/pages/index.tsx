028d75e21532f43741939fd86e2493c2ac66faa9
Sergey Nechaev <sergey@elnino.tech>
2022-12-07 20:52:45 +0100

    Add example grid for System overview screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import { observer } from 'mobx-react-lite'
-import ThemeMode from '@components/theme/ThemeMode'
 import { NextPageWithLayout } from '@pages/_app'
 import CommonPageLayout from '@components/layout/page/CommonPageLayout'
+import Box from '@components/layout/Box'
+import Grid from '@components/layout/Grid'
 
 const Home: NextPageWithLayout = () => {
   return (
-    <>
-      <div className={'text-center dark:text-white'}>
-        <h1 className={'text-2xl mb-2'}>Victron Venus MFD</h1>
-      </div>
-      <div className={'absolute top-2 right-8'}>
-        <ThemeMode />
-      </div>
-    </>
+    <div className={'p-4 h-full'}>
+      <Grid>
+        <Box className={'bg-amber-300 dark:bg-amber-700'}>Box 1</Box>
+        <Box className={'bg-blue-300 dark:bg-blue-700'}>Box 2</Box>
+        <Box className={'bg-green-300 dark:bg-green-700'}>Box 3</Box>
+        <Box className={'bg-fuchsia-300 dark:bg-fuchsia-700'}>Box 4</Box>
+      </Grid>
+    </div>
   )
 }
 
