a1206b32ce215161033df0492b8ac633bb5301eb
Sergey Nechaev <sergey@elnino.tech>
2022-12-07 20:06:04 +0100

    Use custom page layout for app pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '~/styles/global.css'
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode, useMemo } from 'react'
 import classnames from 'classnames'
 import { observer } from 'mobx-react-lite'
 import { configurePersistable } from 'mobx-persist-store'
 import { MuseoSans } from '~/fonts'
 import { Storage, useTheme } from '@elninotech/mfd-modules'
+import { NextPage } from 'next'
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
 // Common configuration for MobX persistable stores, you can override them in each store configuration
 configurePersistable({
@@ -15,12 +25,17 @@ configurePersistable({
   // debugMode: true, // switch it on to have console debug messages
 })
 
-const MfdApp = ({ Component, pageProps }: AppProps) => {
+const MfdApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const { darkMode } = useTheme()
+
+  // try to use layout defined at the page level, if available
+  const getLayout = useMemo(() => {
+    return Component.getLayout ?? ((page: ReactNode) => page)
+  }, [Component.getLayout])
 
   return (
     <main className={classnames(`${MuseoSans.variable} font-sans`, { dark: darkMode })}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </main>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,24 @@
 import { observer } from 'mobx-react-lite'
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import ThemeMode from '@components/theme/ThemeMode'
+import { NextPageWithLayout } from '@pages/_app'
+import CommonPageLayout from '@components/layout/page/CommonPageLayout'
 
-const Home: NextPage = () => {
+const Home: NextPageWithLayout = () => {
   return (
-    <div>
-      <Head>
-        <title>Victron Venus MFD</title>
-      </Head>
-
-      <main className={'safe-h-screen dark:bg-black'}>
-        <div className={'text-center dark:text-white'}>
-          <h1 className={'text-2xl mb-2'}>Victron Venus MFD</h1>
-        </div>
-        <div className={'absolute top-2 right-8'}>
-          <ThemeMode />
-        </div>
-      </main>
-    </div>
+    <>
+      <div className={'text-center dark:text-white'}>
+        <h1 className={'text-2xl mb-2'}>Victron Venus MFD</h1>
+      </div>
+      <div className={'absolute top-2 right-8'}>
+        <ThemeMode />
+      </div>
+    </>
   )
+}
+
+Home.getLayout = (page: JSX.Element) => {
+  // TODO: add translations
+  return <CommonPageLayout title='System Overview'>{page}</CommonPageLayout>
 }
 
 export default observer(Home)
