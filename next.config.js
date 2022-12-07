ae640341f14c77392482422903a656d9936ca56c
Sergey Nechaev <sergey@elnino.tech>
2022-12-07 14:27:51 +0100

    Add empty publicRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ const withTM = require('next-transpile-modules')(['@elninotech/mfd-modules'])
 module.exports = withTM({
   reactStrictMode: true,
   poweredByHeader: false,
+  publicRuntimeConfig: {},
 })
