4017ee5deb5db348312c715729f98f0e63cd74b7
Sergey Nechaev <sergey@elnino.tech>
2022-12-09 00:39:51 +0100

    Use Energy component in Overview page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import { NextPageWithLayout } from '@pages/_app'
 import CommonPageLayout from '@components/layout/CommonPageLayout'
 import Box from '@components/ui/Box'
 import Grid from '@components/ui/Grid'
+import Energy from '@components/boxes/Energy'
 
 const Home: NextPageWithLayout = () => {
   return (
     <div className={'p-4 h-full'}>
-      <Grid>
-        <Box className={'bg-amber-300 dark:bg-amber-700'}>Box 1</Box>
+      <Grid className={'gap-2'}>
+        <Energy />
         <Box className={'bg-blue-300 dark:bg-blue-700'}>Box 2</Box>
         <Box className={'bg-green-300 dark:bg-green-700'}>Box 3</Box>
         <Box className={'bg-fuchsia-300 dark:bg-fuchsia-700'}>Box 4</Box>
