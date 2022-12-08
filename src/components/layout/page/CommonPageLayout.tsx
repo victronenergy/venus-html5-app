4f8cb33da7de9424fbc9ca2d6feb0b06e687af3f
Sergey Nechaev <sergey@elnino.tech>
2022-12-08 18:36:31 +0100

    Add meta viewport params for page layout

--- src/components/layout/page/CommonPageLayout.tsx
+++ src/components/layout/page/CommonPageLayout.tsx
@@ -7,6 +7,7 @@ const CommonPageLayout = ({ title, children }: Props) => {
   return (
     <>
       <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
         <title>{`${title ? `${title} — ` : ''} Victron Venus MFD`}</title>
       </Head>
       <div className='dark:bg-black dark:text-white safe-h-screen min-safe-h-screen flex flex-col'>
