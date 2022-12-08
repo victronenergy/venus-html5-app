1da30629372072c64be2b96a94860e8484fe3ab5
Sergey Nechaev <sergey@elnino.tech>
2022-12-08 18:35:16 +0100

    Add support for injected svg icons

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ const withTM = require('next-transpile-modules')(['@elninotech/mfd-modules'])
 module.exports = withTM({
   reactStrictMode: true,
   poweredByHeader: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
   publicRuntimeConfig: {},
 })
