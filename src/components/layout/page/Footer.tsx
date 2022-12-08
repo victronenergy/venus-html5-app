1cad99cd64347dc50699baabc93a0b52bf051f1c
Sergey Nechaev <sergey@elnino.tech>
2022-12-08 18:35:56 +0100

    Add Victron svg logo

--- src/components/layout/page/Footer.tsx
+++ src/components/layout/page/Footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
 import ThemeMode from '@components/theme/ThemeMode'
+import LogoIcon from '@public/logo/logo.svg'
 
 const Footer = () => {
   return (
     <div className='flex w-full flex-row justify-between p-4'>
-      <div>Victron Logo</div>
+      <div>
+        <LogoIcon width={125} className={'text-black dark:text-white'} />
+      </div>
       <ThemeMode />
     </div>
   )
