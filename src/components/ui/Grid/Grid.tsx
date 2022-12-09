dc31d08f3a24efb50bf6d59d0d4a6de0def03acf
Sergey Nechaev <sergey@elnino.tech>
2022-12-09 16:05:54 +0100

    Add gap width for Grid component

--- src/components/ui/Grid/Grid.tsx
+++ src/components/ui/Grid/Grid.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 
-const Grid = ({ children, className, flow = 'row' }: Props) => {
+const Grid = ({ children, className, flow = 'row', gapWidth = 'small' }: Props) => {
   const childrenCount = Array.isArray(children) ? children.length : 1
 
   return (
@@ -9,12 +9,15 @@ const Grid = ({ children, className, flow = 'row' }: Props) => {
       className={classnames(
         'h-full w-full grid auto-rows-fr auto-cols-fr',
         {
+          'grid-flow-col': flow === 'col',
+          'grid-flow-row': flow === 'row',
+          'gap-2': gapWidth === 'small',
+          'gap-4': gapWidth === 'medium',
+          'gap-8': gapWidth === 'large',
           'grid-cols-2': flow === 'row' && childrenCount > 1,
           'grid-cols-1': flow === 'row' && childrenCount <= 1,
           'grid-rows-2': flow === 'col' && childrenCount > 1,
           'grid-rows-1': flow === 'col' && childrenCount <= 1,
-          'grid-flow-col': flow === 'col',
-          'grid-flow-row': flow === 'row',
         },
         className
       )}
@@ -44,6 +47,7 @@ interface Props {
   onClick?: () => void
   className?: string
   flow?: 'row' | 'col'
+  gapWidth?: 'none' | 'small' | 'medium' | 'large'
 }
 
 export default Grid
