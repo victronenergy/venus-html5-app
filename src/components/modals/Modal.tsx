b11622566704e3fa9d863afca418bfa9a1aef5e0
andreielnino <andrei@elnino.tech>
2022-12-13 15:00:38 +0100

    Add modal for theme settings preferences

---
+++ src/components/modals/Modal.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames'
+import React from 'react'
+
+interface Props {
+  open?: boolean
+  onClose: () => void
+  children: React.ReactNode
+}
+
+const Frame: React.FC<Props> = ({ children, open = true, onClose}) => {
+  return (
+    <div
+      className={classNames(
+        'fixed inset-0 z-10 p-8 text-white bg-gray-1/70',
+        `${open ? 'block' : 'hidden'}` // control visibility via `open` attribute (or render conditionally)
+      )}
+    >
+      <div className='absolute w-full max-w-sm mb-20 mr-6 bottom-0 right-0'>
+        <div className='overflow-hidden p-4 bg-gray-1 rounded-md shadow-[0_8px_24px_-15px_rgba(0,0,0,0.75)]'>
+          {children}
+          <button onClick={() => onClose()} className="w-full border-2 border-v-blue bg-v-blue/30 rounded-md pt-2 pb-2">Remote Console</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Head: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div className='block p-4'>
+    {children}
+  </div>
+)
+
+const Body: React.FC<{ children: React.ReactNode }> = ({ children }) => <div>{children}</div>
+
+export const Modal = { Frame, Head, Body }
--- src/components/theme/ThemeMode/ThemeMode.tsx
+++ src/components/theme/ThemeMode/ThemeMode.tsx
@@ -1,17 +1,70 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { observer } from 'mobx-react-lite'
 import { useTheme } from '@elninotech/mfd-modules'
+import { Modal } from '~/components/modals/Modal'
+import PreferencesIcon from '~/public/icons/preferences.svg'
+import { ToggleSwitch, RadioButton } from '~/components/buttons/base'
 
 const ThemeMode = () => {
   const { themeStore } = useTheme()
+  const [modal, setModal] = useState(false)
+  const [lock, setLock] = useState(false)
 
   const toggleMode = () => {
     themeStore.setDarkMode(!themeStore.darkMode)
   }
 
+  const toggleLock = () => {
+    setLock(!lock)
+  }
+
+  const setAutoMode = () => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+    if (mediaQuery.matches) {
+      themeStore.setDarkMode(true)
+    } else {
+      themeStore.setDarkMode(false)
+    }
+  }
+
   return (
     <div className='dark:text-white cursor-pointer'>
-      <div onClick={toggleMode}>{themeStore.darkMode ? 'Light' : 'Dark'}</div>
+    <div className="flex justify-center items-center w-full">
+      <button onClick={() => setModal(!modal)}> <PreferencesIcon /> </button>
+      <Modal.Frame
+        open={modal}
+        onClose={() => {
+            setModal(false);
+        }}
+      >
+        <Modal.Body>
+          <div className="flex flex-col">
+            <div className="text-xl mb-4 dark:text-white">
+              <label className="flex justify-between items-center pb-4">
+                <span className="mr-2">Lock to prevent changes</span>
+                  <ToggleSwitch onChange={toggleLock} selected={lock}/>
+              </label>
+              <div className="border border-gray-300"></div>
+              <label className="text-base text-gray-5 pb-4">Mode</label>
+              <label className="flex justify-between items-center pb-8">
+                <span className="mr-2">Auto</span>
+                <ToggleSwitch onChange={setAutoMode} disabled={lock}/>
+              </label>
+              <label className="flex justify-between items-center pb-8">
+                <span className="mr-2">Light</span>
+                <RadioButton onChange={toggleMode} selected={!themeStore.darkMode} disabled={lock}/>
+              </label>
+              <label className="flex justify-between items-center pb-8">
+                <span className="mr-2">Dark</span>  
+                <RadioButton onChange={toggleMode} selected={themeStore.darkMode} disabled={lock}/>
+              </label>
+              <div className="border border-gray-300"></div>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal.Frame>
+    </div>
     </div>
   )
 }
