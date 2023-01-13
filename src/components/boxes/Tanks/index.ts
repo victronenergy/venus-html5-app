a66f9706b9ff993a7efddb5d2252cd0bbdf8a785
Simon <info@simonvreman.nl>
2023-01-13 14:54:40 +0100

    Add Tanks component and child Tank component including basic state logic.

---
+++ src/components/boxes/Tanks/index.ts
@@ -0,0 +1,2 @@
+export { default } from './Tanks'
+export * from './Tanks'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { useRouter } from 'next/router'
 import { DEFAULT_LANGUAGE, LANGUAGE_KEY_LOCAL_STORAGE } from '~/utils/constants'
 import { useEffect } from 'react'
+import { useStore } from '~/stores'
+import EnergyAC from '~/components/boxes/EnergyAC'
+import EnergyShore from '~/components/boxes/EnergyShore'
+import Tanks from '~/components/boxes/Tanks'
 
 const Redirect = () => {
   const router = useRouter()
@@ -11,6 +15,21 @@ const Redirect = () => {
       router.replace('/' + language + router.asPath)
     }
   }, [router])
+    // TODO: add translations
+    navigationStore.setTitle('System Overview')
+  }, [])
+
+  return (
+    <div className={'p-4 h-full'}>
+      <Grid className={'gap-2'}>
+        <EnergyOverview />
+        <EnergyAC mode={'full'} />
+        <EnergyShore />
+        <Tanks />
+      </Grid>
+    </div>
+  )
+}
 
   return <></>
 }
