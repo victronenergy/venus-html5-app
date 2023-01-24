60f894a670ed2852d2d2883a8aeef9ebe527ef36
Sergey Nechaev <sergey@elnino.tech>
2023-01-24 14:47:17 +0100

    Add basic victron palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,52 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
+  // we try to use the default theme as much as possible,
+  // see https://tailwindcss.com/docs/theme
   theme: {
     extend: {
+      colors: {
+        victron: {
+          white: '#FFFFFF',
+          black: '#000000',
+          darkGray: {
+            DEFAULT: '#141414',
+            dark: '#141414',
+          },
+          gray: {
+            DEFAULT: '#969591',
+            dark: '#969591',
+          },
+          blue: {
+            DEFAULT: '#387DC5',
+            dark: '#387DC5',
+          },
+          red: {
+            DEFAULT: '#F35C58',
+            dark: '#F35C58',
+          },
+          cyan: {
+            DEFAULT: '#84CBF8',
+            dark: '#84CBF8',
+          },
+          purple: {
+            DEFAULT: '#6B4AB3',
+            dark: '#6B4AB3',
+          },
+          lime: {
+            DEFAULT: '#76832C',
+            dark: '#76832C',
+          },
+          green: {
+            DEFAULT: '#72B84C',
+            dark: '#72B84C',
+          },
+          slate: {
+            DEFAULT: '#7F7F9C',
+            dark: '#7F7F9C',
+          },
+        },
+      },
       fontFamily: {
         sans: ['var(--font-museo-sans)', ...defaultTheme.fontFamily.sans],
       },
