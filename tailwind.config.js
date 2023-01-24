f297d4e941b292f1af1d322428215370941a8e53
Sergey Nechaev <sergey@elnino.tech>
2023-01-24 14:46:54 +0100

    Set basic border radius tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-museo-sans)', ...defaultTheme.fontFamily.sans],
       },
+      borderRadius: {
+        none: '0',
+        sm: '0.25rem',
+        md: '0.5rem',
+        lg: '0.75rem',
+        full: '9999px',
+        DEFAULT: '0.25rem',
+      },
     },
   },
   plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp')],
