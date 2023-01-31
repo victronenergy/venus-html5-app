9faf37efbb621361edbfd4982f61d74eda156daa
Sergey Nechaev <sergey@elnino.tech>
2023-01-31 09:44:17 +0100

    Fix doubling min-h-0

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classNames from 'classnames'
 
 const Container = ({ children, className }: Props) => {
-  return <div className={classNames('w-full min-h-0 h-full min-h-0', className)}>{children}</div>
+  return <div className={classNames('w-full h-full min-h-0', className)}>{children}</div>
 }
 
 interface Props {
