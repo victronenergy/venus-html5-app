b42a550661e3f40063bfbdb76443fbdc745b721d
andreielnino <andrei@elnino.tech>
2023-01-31 17:23:02 +0100

    Add custom buttons as in the design

--- src/components/buttons/base.tsx
+++ src/components/buttons/base.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
+import classNames from 'classnames'
 
 interface Props {
   selected?: boolean
   disabled?: boolean
   color?: string
   onChange: () => void
-
 }
 
-export const RadioButton:React.FC<Props> = ({selected, onChange, disabled}) => {
+export const RadioButton: React.FC<Props> = ({ selected, onChange, disabled }) => {
   return (
-    <input type="radio" value="auto" checked={selected} onChange={onChange} disabled={disabled}/>
+    <div>
+      <div onClick={() => !disabled && onChange()} className={classNames('w-6 h-6 bg-inherit rounded-full border-2', {
+        'border-victron-gray': !selected,
+        'border-victron-blue': selected,
+      })}>
+        {selected && <div className='w-4 h-4 mt-[2px] ml-[2px] rounded-full bg-victron-blue'></div>}
+      </div>
+    </div>
   )
 }
 
-export const ToggleSwitch:React.FC<Props> = ({onChange, selected, disabled}) => {
+export const ToggleSwitch: React.FC<Props> = ({ onChange, selected, disabled }) => {
   return (
-    <span className="relative">
-      <input id="Toggle2" type="checkbox" className="hidden peer" onChange={onChange} checked={selected} disabled={disabled}/>
-      <div className="w-10 h-4 rounded-full shadow bg-gray-600 peer-checked:bg-blue-400"></div>
-      <div className="absolute left-0 w-6 h-6 rounded-full shadow -inset-y-1 peer-checked:right-0 peer-checked:left-auto bg-blue-400"></div>
+    <span className='relative'>
+      <input
+        id='Toggle2'
+        type='checkbox'
+        className='hidden peer'
+        onChange={onChange}
+        checked={selected}
+        disabled={disabled}
+      />
+      <div className='w-11 h-5 rounded-full shadow border-victron-gray border-2 peer-checked:bg-victron-blue peer-checked:border-victron-blue'></div>
+      <div className='absolute left-0 w-6 h-6 rounded-full shadow -inset-y-[2px] bg-victron-blue peer-checked:right-0 peer-checked:left-auto peer-checked:bg-white'></div>
     </span>
   )
 }
--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -11,12 +11,12 @@ const Frame: React.FC<Props> = ({ children, open = true, onClose }) => {
   return (
     <div
       className={classNames(
-        'fixed inset-0 z-10 p-8 text-neutral-600 bg-neutral-100 dark:text-white dark:bg-gray-1/70 ',
+        'fixed inset-0 z-10 p-8 text-neutral-600 bg-neutral-100 dark:text-white dark:bg-victron-darkGray/70 ',
         `${open ? 'block' : 'hidden'}` // control visibility via `open` attribute (or render conditionally)
       )}
     >
       <div className='absolute w-full max-w-sm mb-20 mr-6 bottom-0 right-0'>
-        <div className='overflow-hidden p-4 dark:bg-gray-1 rounded-md shadow-[0_8px_24px_-15px_rgba(0,0,0,0.75)]'>
+        <div className='overflow-hidden p-4 dark:bg-victron-darkGray rounded-md shadow-[0_8px_24px_-15px_rgba(0,0,0,0.75)]'>
           {children}
         </div>
       </div>
