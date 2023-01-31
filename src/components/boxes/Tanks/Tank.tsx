8ea41dcda5fa095481ff574d0cfc513a3b812a84
andreielnino <andrei@elnino.tech>
2023-01-31 14:53:06 +0100

    Add tank component orientation (horizontal, vertical) and modify used component

--- src/components/boxes/Tanks/Tank.tsx
+++ src/components/boxes/Tanks/Tank.tsx
@@ -3,12 +3,12 @@ import { observer } from 'mobx-react-lite'
 import ProgressBar from '~/components/ui/ProgressBar'
 import classnames from 'classnames'
 
-import FuelIcon from '~/public/icons/tanks/fuel.svg'
-import WaterIcon from '~/public/icons/tanks/water.svg'
-import BlackWaterIcon from '~/public/icons/tanks/black-water.svg'
-import GrayWaterIcon from '~/public/icons/tanks/gray-water.svg'
+import FuelIcon from '~/public/icons/fuel.svg'
+import WaterIcon from '~/public/icons/fresh-water.svg'
+import BlackWaterIcon from '~/public/icons/black-water.svg'
+import GrayWaterIcon from '~/public/icons/waste-water.svg'
 
-const Tank = ({ tankInstanceId, mode = 'compact' }: Props) => {
+const Tank = ({ tankInstanceId, mode = 'compact', orientation = 'vertical' }: Props) => {
   let { capacity, fluidType, level, remaining, unit } = useTank(tankInstanceId)
 
   // check if tank values are not undefined
@@ -27,7 +27,7 @@ const Tank = ({ tankInstanceId, mode = 'compact' }: Props) => {
       <div>
         <div className='grid grid-cols-8 gap-2'>
           <div className='col-span-6 flex items-center sm:col-span-3 lg:col-span-2'>
-            <div>{fluidIcon(+fluidType)}</div>
+            <div>{fluidIcon(+fluidType, orientation)}</div>
             <div className='flex flex-col p-2'>
               <div className='text-lg lg:text-xl'>{fluidTypeFormatter(+fluidType)}</div>
             </div>
@@ -57,39 +57,77 @@ const Tank = ({ tankInstanceId, mode = 'compact' }: Props) => {
     )
   }
 
-  return (
-    <div>
-      <div className='grid grid-cols-8 gap-2'>
-        <div className='col-span-3 flex items-center md:col-span-2'>
-          <div>{fluidIcon(+fluidType)}</div>
-          <div className='flex flex-col p-2'>
-            <div className='text-xl'>{fluidTypeFormatter(+fluidType)}</div>
-            <div className='text-gray-5'>{formatCapacity(remaining) + '/' + formatCapacity(capacity) + ' l'}</div>
+  if (orientation === 'vertical') {
+    return (
+      <div>
+        <div className='grid grid-cols-8 gap-2'>
+          <div className='col-span-3 flex items-center md:col-span-2'>
+            <div>{fluidIcon(+fluidType, orientation)}</div>
+            <div className='flex flex-col p-2'>
+              <div className='text-2xl'>{fluidTypeFormatter(+fluidType)}</div>
+              <div className='text-victron-gray'>
+                {formatCapacity(remaining) + '/' + formatCapacity(capacity) + ' l'}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className='flex items-center justify-center col-span-4 md:col-span-5'>
-          <ProgressBar percentage={levelFormatter(level)} type={+fluidType} />
-        </div>
-        <div className='col-span-1 flex items-center justify-center'>
-          <div
-            className={classnames('text-3xl flex flex-row', {
-              'text-victron-red': level > 75,
-            })}
-          >
-            <div>{levelFormatter(level)}</div>
+          <div className='flex items-center justify-center col-span-4 md:col-span-5'>
+            <ProgressBar percentage={levelFormatter(level)} type={+fluidType} />
+          </div>
+          <div className='col-span-1 flex items-center justify-center'>
             <div
-              className={classnames({
+              className={classnames('text-3xl flex flex-row', {
                 'text-victron-red': level > 75,
-                'text-victron-gray': level <= 75,
               })}
             >
-              {' %'}
+              <div>{levelFormatter(level)}</div>
+              <div
+                className={classnames({
+                  'text-victron-red': level > 75,
+                  'text-victron-gray': level <= 75,
+                })}
+              >
+                {' %'}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className='h-full w-full grid grid-cols-8'>
+        <div className='w-full col-span-1'>
+          <ProgressBar percentage={levelFormatter(level)} type={+fluidType} orientation='vertical' />
+        </div>
+        <div className='col-span-7 flex flex-col justify-between'>
+          <div className='pt-2'>
+            <div>{fluidIcon(+fluidType, orientation)}</div>
+            <div className=''>
+              <div className='text-2xl'>{fluidTypeFormatter(+fluidType)}</div>
+            </div>
+          </div>
+          <div className='flex flex-col'>
+            <div
+              className={classnames('text-3xl flex flex-row', {
+                'text-victron-red': level > 75,
+              })}
+            >
+              <div>{levelFormatter(level)}</div>
+              <div
+                className={classnames({
+                  'text-victron-red': level > 75,
+                  'text-victron-gray': level <= 75,
+                })}
+              >
+                {' %'}
+              </div>
+            </div>
+            <div className='text-victron-gray'>{formatCapacity(remaining) + '/' + formatCapacity(capacity) + ' l'}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 // Convert remaining and total capacity to liters from m3
@@ -101,32 +139,32 @@ const levelFormatter = function (level: number) {
   return Math.round(level)
 }
 
-const fluidIcon = function (type: number) {
+const fluidIcon = function (type: number, mode: string = 'compact') {
   switch (type) {
     case 0:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 1:
-      return <WaterIcon />
+      return mode === 'compact' ? <WaterIcon className='w-7'/> : <WaterIcon className='w-9'/>
     case 2:
-      return <GrayWaterIcon />
+      return mode === 'compact' ? <GrayWaterIcon className='w-7'/> : <GrayWaterIcon className='w-9'/>
     case 3:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 4:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 5:
-      return <BlackWaterIcon />
+      return mode === 'compact' ? <BlackWaterIcon className='w-7'/> : <BlackWaterIcon className='w-9'/>
     case 6:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 7:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 8:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 9:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 10:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     case 11:
-      return <FuelIcon />
+      return mode === 'compact' ? <FuelIcon className='w-7'/> : <FuelIcon className='w-9'/>
     default:
       return null
   }
@@ -166,6 +204,7 @@ const fluidTypeFormatter = function (type: number) {
 interface Props {
   tankInstanceId: number
   mode?: 'compact' | 'full'
+  orientation?: 'vertical' | 'horizontal'
 }
 
 export default observer(Tank)
--- src/components/boxes/TanksOverview/TanksOverview.tsx
+++ src/components/boxes/TanksOverview/TanksOverview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Box from '~/components/ui/Box'
 import TanksIcon from '~/public/icons/tanks.svg'
 import { RouterPath } from '~/types/routes'
@@ -9,9 +9,25 @@ import { BoxProps } from '~/types/boxes'
 import { useTanks } from '@elninotech/mfd-modules'
 import Tank from '~/components/boxes/Tanks/Tank'
 import { observer } from 'mobx-react-lite'
+import Grid from '~/components/ui/Grid'
+import { useComponentSize, useWindowSize } from '~/utils/hooks'
 
 const TanksOverview = ({ mode = 'compact' }: BoxProps) => {
   const { tanks } = useTanks()
+  const gridRef = useRef<HTMLDivElement>(null)
+  const [orientation, setOrientation] = React.useState<'horizontal' | 'vertical'>('vertical')
+
+  const componentSize = useComponentSize(gridRef)
+  const windowSize = useWindowSize()
+
+  useEffect(() => {
+    if (!windowSize || !componentSize) return
+    if (windowSize.height !== undefined && 2 * componentSize.height > windowSize.height) {
+      setOrientation('horizontal')
+    } else {
+      setOrientation('vertical')
+    }
+  }, [windowSize, componentSize])
 
   if (mode === 'compact') {
     return (
@@ -22,27 +38,41 @@ const TanksOverview = ({ mode = 'compact' }: BoxProps) => {
       >
         <>
           <div>Tanks</div>
-          <div>
-            {tanks &&
-              tanks.map((tank, index) => {
-                return tank ? <Tank key={index} tankInstanceId={tank} /> : null
-              })}
-          </div>
+            <div>
+              {tanks &&
+                tanks.map((tank, index) => {
+                  return tank ? <Tank key={index} tankInstanceId={tank} /> : null
+                })}
+            </div>
         </>
       </Box>
     )
   }
 
+  if (orientation == "vertical") {
   return (
-    <Box title={'Tanks'} icon={<TanksIcon className={'w-6 text-black dark:text-white'} />}>
-      <>
-        {tanks &&
-        tanks.map((tank, index) => {
-          return tank ? <Tank key={index} tankInstanceId={tank} mode='full' /> : null
-        })}
-      </>
-    </Box>
-  )
+      <Box title={'Tanks'} icon={<TanksIcon className={'w-6 text-black dark:text-white'} />}>
+              <div ref={gridRef}>
+                {tanks &&
+                tanks.map((tank, index) => {
+                  return tank ? <Tank key={index} tankInstanceId={tank} mode='full' orientation={orientation}/> : <></>
+                })}
+              </div>
+      </Box>
+    )
+  } else {
+    return (
+      <Box title={'Tanks'} icon={<TanksIcon className={'w-6 text-black dark:text-white'} />}>
+        <div className='grid grid-cols-4 h-full'>
+              {tanks &&
+                tanks.map((tank, index) => {
+                  return tank ? <Tank key={index} tankInstanceId={tank} mode='full' orientation={orientation}/> : <></>
+                })}
+        </div>
+      </Box>
+    )
+  }
+  
 }
 
 const ComponentWithErrorBoundary = withErrorBoundary(TanksOverview, {
