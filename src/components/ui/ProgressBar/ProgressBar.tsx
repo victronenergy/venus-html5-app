08424fad1f6ace51da131cf9a1359e7f9ddbb0de
andreielnino <andrei@elnino.tech>
2023-01-31 14:53:06 +0100

    Add tank component orientation (horizontal, vertical) and modify used component

--- src/components/ui/ProgressBar/ProgressBar.tsx
+++ src/components/ui/ProgressBar/ProgressBar.tsx
@@ -1,31 +1,70 @@
-import React from "react";
-import classnames from "classnames";
+import React from 'react'
+import classnames from 'classnames'
 
-const ProgressBar = ({percentage, type}: Props) => {
-  const color = colorFortmatter(type);
-  const bgColor = colorFortmatter( type, true);
+const ProgressBar = ({ percentage, type, orientation = 'horizontal' }: Props) => {
+  const color = colorFortmatter(type)
+  const bgColor = colorFortmatter(type, true)
 
-  return (
-    <div className="w-full flex  p-5">
-      <div className={classnames(`w-1/4 h-1 rounded-2xl mr-2 ${bgColor}`)}>
-        <div className={classnames(`h-full rounded-2xl ${color}`)}
-          style={{width: percentage >= 25 ? "100%" : `${percentage * 4}%`}} />
+  if (orientation === 'horizontal') {
+    return (
+      <div className='w-full flex p-5'>
+        <div className={classnames(`w-1/4 h-2 rounded-l-2xl mr-2 ${bgColor}`)}>
+          <div
+            className={classnames(`h-full rounded-l-2xl ${color}`)}
+            style={{ width: percentage >= 25 ? '100%' : `${percentage * 4}%` }}
+          />
+        </div>
+        <div className={classnames(`w-1/4 h-2 mr-2 ${bgColor}`)}>
+          <div
+            className={classnames(`h-full ${color}`)}
+            style={{ width: percentage >= 50 ? '100%' : percentage <= 25 ? '0%' : `${(percentage - 25) * 4}%` }}
+          />
+        </div>
+        <div className={classnames(`w-1/4 color/30 h-2 mr-2 ${bgColor}`)}>
+          <div
+            className={classnames(`h-full ${color}`)}
+            style={{ width: percentage >= 75 ? '100%' : percentage <= 50 ? '0%' : `${(percentage - 50) * 4}%` }}
+          />
+        </div>
+        <div className={classnames(`w-1/4 color/30 h-2 rounded-r-2xl mr-2 ${bgColor}`)}>
+          <div
+            className={classnames(`h-full rounded-r-2xl ${color}`)}
+            style={{ width: percentage <= 75 ? '0%' : `${(percentage - 75) * 4}%` }}
+          />
+        </div>
       </div>
-      <div className={classnames(`w-1/4 h-1 rounded-2xl mr-2 ${bgColor}`)}>
-        <div className={classnames(`h-full rounded-2xl ${color}`)}
-          style={{width: percentage >= 50 ? "100%" : percentage <= 25 ? '0%' : `${(percentage - 25) * 4}%`}} />
+    )
+  } else {
+    return (
+      <div className='h-full w-full flex flex-col-reverse items-center'>
+        <div className={classnames(`h-1/4 w-2 m-1 ${bgColor} rotate-180`)}>
+          <div
+            className={classnames(`w-full rounded-t-2xl ${color}`)}
+            style={{ height: percentage >= 25 ? '100%' : `${percentage * 4}%` }}
+          />
+        </div>
+        <div className={classnames(`h-1/4 w-2 m-1 ${bgColor} rotate-180`)}>
+          <div
+            className={classnames(`w-full ${color}`)}
+            style={{ height: percentage >= 50 ? '100%' : percentage <= 25 ? '0%' : `${(percentage - 25) * 4}%` }}
+          />
+        </div>
+        <div className={classnames(`h-1/4 w-2 m-1 ${bgColor} rotate-180`)}>
+          <div
+            className={classnames(`w-full ${color}`)}
+            style={{ height: percentage >= 75 ? '100%' : percentage <= 50 ? '0%' : `${(percentage - 50) * 4}%` }}
+          />
+        </div>
+        <div className={classnames(`h-1/4 w-2 m-1 rounded-b-2xl ${bgColor} rotate-180`)}>
+          <div
+            className={classnames(`w-full ${color}`)}
+            style={{ height: percentage <= 75 ? '0%' : `${(percentage - 75) * 4}%` }}
+          />
+        </div>
       </div>
-      <div className={classnames(`w-1/4 color/30 h-1 rounded-2xl mr-2 ${bgColor}`)}>
-        <div className={classnames(`h-full rounded-2xl ${color}`)}
-          style={{width: percentage >= 75 ? "100%" : percentage <= 50 ? '0%' : `${(percentage - 50) * 4}%`}} />
-      </div>
-      <div className={classnames(`w-1/4 color/30 h-1 rounded-2xl mr-2 ${bgColor}`)}>
-        <div className={classnames(`h-full rounded-2xl ${color}`)}
-          style={{width: percentage <= 75 ? "0%" : `${(percentage - 75) * 4}%`}} />
-      </div>
-    </div>
-  );
-};
+    )
+  }
+}
 
 const colorFortmatter = function (type: number, opacity: boolean = false) {
   switch (type) {
@@ -57,8 +96,9 @@ const colorFortmatter = function (type: number, opacity: boolean = false) {
 }
 
 interface Props {
-  percentage: number;
-  type: number;
+  percentage: number
+  type: number
+  orientation?: 'vertical' | 'horizontal'
 }
 
-export default ProgressBar;
+export default ProgressBar
