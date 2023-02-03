a511fe24db2c94bfa54aa8c70b864a04bb623525
andreielnino <andrei@elnino.tech>
2023-02-03 16:13:15 +0100

    Add styling for DC loads compact component

--- src/components/boxes/EnergyDC/EnergyDC.tsx
+++ src/components/boxes/EnergyDC/EnergyDC.tsx
@@ -4,13 +4,22 @@ import { BoxProps } from '~/types/boxes'
 import EnergyIcon from '~/public/icons/energy.svg'
 import { DcLoadsState } from '@elninotech/mfd-modules'
 import { useTranslation } from 'next-i18next'
+import DCIcon from '~/public/icons/dc.svg'
 
 const EnergyDC = ({ mode = 'compact', dcLoads }: Props) => {
   const { power, voltage } = dcLoads
   const { t } = useTranslation()
 
   if (mode === 'compact') {
-    return <p>{t('boxes.dcLoads')}: {Math.round(power / voltage) + 'A'}</p>
+    return (
+      <div className='flex flex-row justify-between items-center'>
+        <div className='flex'>
+          <DCIcon className={'w-7 text-black dark:text-white'} />
+          <p className='text-2xl pl-3'>{t('boxes.dcLoads')}</p>
+        </div>
+        <p className='text-2xl'>{Math.round(power / voltage)}<span className='text-victron-gray dark:text-victron-gray-dark'>A</span></p>
+      </div>
+    ) 
   }
 
   return (
