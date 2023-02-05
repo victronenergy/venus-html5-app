06dd1343dfe6dc8ddbe5227c2b54d5123f9d9321
andreielnino <andrei@elnino.tech>
2023-02-05 23:53:03 +0100

    Remove useless else condition

--- src/components/boxes/TanksOverview/TanksOverview.tsx
+++ src/components/boxes/TanksOverview/TanksOverview.tsx
@@ -2,14 +2,10 @@ import React, { useEffect, useRef, useState } from 'react'
 import Box from '~/components/ui/Box'
 import TanksIcon from '~/public/icons/tanks.svg'
 import { RouterPath } from '~/types/routes'
-import { withErrorBoundary } from 'react-error-boundary'
-import ErrorFallback from '~/components/ui/ErrorBoundary/ErrorFallback'
-import { useRouter } from 'next/router'
 import { BoxProps } from '~/types/boxes'
 import { useTanks } from '@elninotech/mfd-modules'
 import Tank from '~/components/boxes/Tanks/Tank'
 import { observer } from 'mobx-react-lite'
-import Grid from '~/components/ui/Grid'
 import { useComponentSize, useWindowSize } from '~/utils/hooks'
 
 const TanksOverview = ({ mode = 'compact' }: BoxProps) => {
@@ -38,44 +34,43 @@ const TanksOverview = ({ mode = 'compact' }: BoxProps) => {
       >
         <div>
           <div>Tanks</div>
-            <div>
-              {tanks &&
-                tanks.map((tank, index) => {
-                  return tank ? <Tank key={index} tankInstanceId={tank} /> : null
-                })}
-            </div>
+          <div>
+            {tanks &&
+              tanks.map((tank, index) => {
+                return tank ? <Tank key={index} tankInstanceId={tank} /> : null
+              })}
+          </div>
         </div>
       </Box>
     )
   }
 
-  if (orientation == "vertical") {
-  return (
-      <Box title={'Tanks'} icon={<TanksIcon className={'w-6 text-black dark:text-white'} />}>
-              <div ref={gridRef}>
-                {tanks &&
-                tanks.map((tank, index) => {
-                  return tank ? <Tank key={index} tankInstanceId={tank} mode='full' orientation={orientation}/> : <></>
-                })}
-              </div>
-      </Box>
-    )
-  } else {
+  if (orientation == 'vertical') {
     return (
       <Box title={'Tanks'} icon={<TanksIcon className={'w-6 text-black dark:text-white'} />}>
-        <div className='grid grid-cols-4 h-full'>
-              {tanks &&
-                tanks.map((tank, index) => {
-                  return tank ? <Tank key={index} tankInstanceId={tank} mode='full' orientation={orientation}/> : <></>
-                })}
+        <div ref={gridRef}>
+          {tanks &&
+            tanks.map((tank, index) => {
+              return tank ? <Tank key={index} tankInstanceId={tank} mode='full' orientation={orientation} /> : <></>
+            })}
         </div>
       </Box>
     )
   }
-  
+
+  return (
+    <Box title={'Tanks'} icon={<TanksIcon className={'w-6 text-black dark:text-white'} />}>
+      <div className='grid grid-cols-4 h-full'>
+        {tanks &&
+          tanks.map((tank, index) => {
+            return tank ? <Tank key={index} tankInstanceId={tank} mode='full' orientation={orientation} /> : <></>
+          })}
+      </div>
+    </Box>
+  )
 }
 
-const ComponentWithErrorBoundary = withErrorBoundary(TanksOverview, {
+const ComponentWithErrorBoundary = withErrorBoundary(observer(TanksOverview), {
   FallbackComponent: ErrorFallback,
   onError(error, info) {
     console.error(error, info)
