8bc46cc52f625bdfb45f851fa3aad15ef0b8a2eb
andreielnino <andrei@elnino.tech>
2023-02-07 16:20:50 +0100

    Fix preferences modal and add translations

--- src/components/theme/ThemeMode/ThemeMode.tsx
+++ src/components/theme/ThemeMode/ThemeMode.tsx
@@ -1,34 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import { useTheme } from '@elninotech/mfd-modules'
 import { Modal } from '~/components/modals/Modal'
 import PreferencesIcon from '~/public/icons/preferences.svg'
 import { ToggleSwitch, RadioButton } from '~/components/buttons/base'
+import { useTranslation } from 'next-i18next'
+import { useTheme, useAppStore } from '@elninotech/mfd-modules'
 
 const ThemeMode = () => {
+  const { locked, toggleLocked } = useAppStore()
   const { themeStore } = useTheme()
   const [modal, setModal] = useState(false)
-  const [lock, setLock] = useState(false)
+  const { t } = useTranslation()
 
-  const toggleMode = () => {
-    themeStore.setDarkMode(!themeStore.darkMode)
-  }
-
-  const toggleLock = () => {
-    setLock(!lock)
-  }
-
-  // TODO: Move logic to utils 
   const setAutoMode = () => {
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
 
-    if (mediaQuery.matches) {
+    if (mediaQuery.matches && !themeStore.darkMode) {
       themeStore.setDarkMode(true)
-    } else {
+    } else if(!mediaQuery.matches && themeStore.darkMode) {
       themeStore.setDarkMode(false)
     }
   }
-  // TODO: Add translations
+
   return (
     <div className='dark:text-white cursor-pointer'>
       <div className='flex justify-center items-center w-full'>
@@ -45,22 +38,22 @@ const ThemeMode = () => {
             <div className='flex flex-col'>
               <div className='text-lg mb-4 dark:text-white md:text-xl'>
                 <label className='flex justify-between items-center pb-4'>
-                  <span className='mr-2'>Lock to prevent changes</span>
-                  <ToggleSwitch onChange={toggleLock} selected={lock} />
+                  <span className='mr-2'>{t('locker.lockMessage')}</span>
+                  <ToggleSwitch onChange={toggleLocked} selected={locked} />
                 </label>
                 <div className='border border-gray-300'></div>
-                <label className='text-base text-victron-gray pb-4'>Mode</label>
+                <label className='text-base text-victron-gray pb-4'>{t('common.mode')}</label>
                 <label className='flex justify-between items-center pb-8'>
-                  <span className='mr-2'>Light</span>
-                  <RadioButton onChange={toggleMode} selected={!themeStore.darkMode} disabled={lock} />
+                  <span className='mr-2'>{t('common.light')}</span>
+                  <RadioButton onChange={() => themeStore.setDarkMode(false)} selected={!themeStore.darkMode} disabled={locked} />
                 </label>
                 <label className='flex justify-between items-center pb-8'>
-                  <span className='mr-2'>Dark</span>
-                  <RadioButton onChange={toggleMode} selected={themeStore.darkMode} disabled={lock} />
+                  <span className='mr-2'>{t('common.dark')}</span>
+                  <RadioButton onChange={() => themeStore.setDarkMode(true)} selected={themeStore.darkMode} disabled={locked} />
                 </label>
                 <label className='flex justify-between items-center pb-8'>
-                  <span className='mr-2'>Auto</span>
-                  <ToggleSwitch onChange={setAutoMode} disabled={lock} />
+                  <span className='mr-2'>{t('common.auto')}</span>
+                  <ToggleSwitch onChange={setAutoMode} disabled={locked} />
                 </label>
                 <div className='border border-gray-300'></div>
               </div>
@@ -70,7 +63,7 @@ const ThemeMode = () => {
             onClick={() => setModal(false)}
             className='w-full border-2 border-victron-blue bg-victron-blue/30 rounded-md pt-2 pb-2'
           >
-            Remote Console
+            {t('header.remoteConsole')}
           </button>
         </Modal.Frame>
       </div>
