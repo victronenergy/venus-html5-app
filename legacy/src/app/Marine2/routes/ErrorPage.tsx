702b9ffe2dc2431f597311f508fde66333980445
Sergey Nechaev <sergey@elnino.tech>
2023-02-09 21:18:23 +0100

    Fix ErrorPage component

--- legacy/src/app/Marine2/routes/ErrorPage.tsx
+++ legacy/src/app/Marine2/routes/ErrorPage.tsx
@@ -1,18 +1,16 @@
 import { useRouteError } from "react-router-dom"
 
 export default function ErrorPage() {
-  const error = useRouteError()
+  const error = useRouteError() as Error
   console.error(error)
 
   return (
     <div id="error-page">
       <h1>Something went wrong</h1>
-      <div>
-        <a href={"/app"}>Back to the main page</a>
-      </div>
-      <p>
-        <i>{(error as any).statusText || (error as any).message || "Unknown error"}</i>
-      </p>
+      <pre>{error.message || JSON.stringify(error)}</pre>
+      <button onClick={() => (window.location.href = "/app")} className={"rounded p-2"}>
+        Click here to reload the app
+      </button>
     </div>
   )
 }
