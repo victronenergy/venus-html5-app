f94dd440fbbb17e070ac64b99da8b75757a1922e
Sergey Nechaev <sergey@elnino.tech>
2023-02-14 18:49:17 +0100

    Add Connecting component

---
+++ legacy/src/app/Marine2/components/ui/Connecting/Connecting.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { translate } from "react-i18nify"
+import Spinner from "../Spinner"
+
+const Connecting = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center bg-victron-darkGray border-4 border-victron-blue rounded-md">
+      <div className="flex flex-col text-center">
+        <div className="pl-4">
+          <Spinner />
+        </div>
+        <div className="relative mt-28">
+          <div className="text-2xl text-victron-blue">{translate("header.connecting")}...</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Connecting
---
+++ legacy/src/app/Marine2/components/ui/Connecting/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./Connecting"
+export * from "./Connecting"
