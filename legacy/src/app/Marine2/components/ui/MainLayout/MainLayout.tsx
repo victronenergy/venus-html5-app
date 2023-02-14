945ba533b926adda2b04998360fa59f503b38fe4
Sergey Nechaev <sergey@elnino.tech>
2023-02-14 18:05:52 +0100

    Add titles for app views and boxes

--- legacy/src/app/Marine2/components/ui/MainLayout/MainLayout.tsx
+++ legacy/src/app/Marine2/components/ui/MainLayout/MainLayout.tsx
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useMemo } from "react"
 import Footer from "../Footer"
 import Header from "../Header"
+import { observer } from "mobx-react"
+import { useAppViewsStore } from "../../../modules/AppViews"
 
-const MainLayout = ({ children }: Props) => {
+const MainLayout = ({ children, title }: Props) => {
+  const appViewsStore = useAppViewsStore()
+
+  const getTitle = useMemo(() => {
+    return title || appViewsStore.getViewTitle()
+  }, [title, appViewsStore])
+
   return (
     <div className={"dark:bg-black dark:text-white text-black bg-white flex flex-col w-full h-full p-4"}>
       <div className={"flex flex-row w-full h-full grow-0 basis-0 min-h-fit"}>
-        <Header title={"System overview"} />
+        <Header title={getTitle} />
       </div>
       <div className={"flex flex-col grow w-full h-full"}>{children}</div>
       <div className={"flex flex-row w-full h-full grow-0 basis-0 min-h-fit"}>
@@ -18,6 +26,7 @@ const MainLayout = ({ children }: Props) => {
 
 interface Props {
   children?: JSX.Element
+  title?: string
 }
 
-export default MainLayout
+export default observer(MainLayout)
