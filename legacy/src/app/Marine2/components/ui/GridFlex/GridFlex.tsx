8f9283d7c4414319a0891fbbc96167b4b86a3d54
Sergey Nechaev <sergey@elnino.tech>
2023-02-15 18:56:30 +0100

    Fix GridFlex to use correct width and height container values

--- legacy/src/app/Marine2/components/ui/GridFlex/GridFlex.tsx
+++ legacy/src/app/Marine2/components/ui/GridFlex/GridFlex.tsx
@@ -8,7 +8,11 @@ const GridFlex = ({ children, className, childClassName, flow = "row", forceOneD
   const gridRef = useRef<HTMLDivElement>(null)
   const gridSize = useComponentSize(gridRef)
 
+  const [width, setWidth] = useState(gridSize.width)
+  const [height, setHeight] = useState(gridSize.height)
+
   const [gridFlow, setGridFlow] = useState<"row" | "col">(flow)
+
   const [forceOneDimension, setForceOneDimension] = useState(false)
 
   const elementsInRow = useMemo(() => {
@@ -27,36 +31,59 @@ const GridFlex = ({ children, className, childClassName, flow = "row", forceOneD
     setGridFlow(isOneDimension ? (ratio > 1 ? "row" : "col") : flow)
   }, [gridSize, forceOneDimensionRatio])
 
+  useEffect(() => {
+    setWidth(gridSize.width)
+    setHeight(gridSize.height)
+  }, [gridSize])
+
   return (
-    <div
-      ref={gridRef}
-      className={classnames(
-        `w-full h-full min-h-0 flex${forceOneDimension ? "" : " flex-wrap"} flex-${gridFlow === "col" ? "col" : "row"}`,
-        className
-      )}
-    >
-      {Array.isArray(children) ? (
-        children.map((child, i) => (
-          <div
-            className={classnames(
-              `basis-${
-                !forceOneDimension &&
-                childrenCount > elementsInRow &&
-                childrenCount % elementsInRow &&
-                i === childrenCount - 1
-                  ? "full"
-                  : `1/${elementsInRow}`
-              }`,
-              childClassName
-            )}
-            key={child.key || i}
-          >
-            {child}
-          </div>
-        ))
-      ) : (
-        <div className={classnames("basis-full", childClassName)}>{children}</div>
-      )}
+    <div ref={gridRef} className={classnames("w-full h-full", className)}>
+      <div
+        className={classnames("flex", {
+          "flex-col": gridFlow === "col",
+          "flex-row": gridFlow === "row",
+          "flex-wrap": !forceOneDimension,
+        })}
+        style={{ width, height }}
+      >
+        {Array.isArray(children) ? (
+          children.map((child, i) => (
+            <div
+              className={classnames(childClassName)}
+              /* 
+                We have to use native styles here, because Tailwind can't do JIT css styles compilation for dynamic values:
+                https://stackoverflow.com/questions/69687530/dynamically-build-classnames-in-tailwindcss
+              */
+              style={{
+                width:
+                  gridFlow === "row" || !forceOneDimension
+                    ? `${100 / (forceOneDimension ? childrenCount : elementsInRow)}%`
+                    : undefined,
+                height:
+                  gridFlow === "col" && forceOneDimension
+                    ? `${100 / (forceOneDimension ? childrenCount : elementsInRow)}%`
+                    : undefined,
+                flexBasis: `${
+                  100 /
+                  (!forceOneDimension &&
+                  childrenCount > elementsInRow &&
+                  childrenCount % elementsInRow &&
+                  i === childrenCount - 1
+                    ? 1
+                    : forceOneDimension
+                    ? childrenCount
+                    : elementsInRow)
+                }%`,
+              }}
+              key={child.key || i}
+            >
+              {child}
+            </div>
+          ))
+        ) : (
+          <div className={classnames("basis-full", childClassName)}>{children}</div>
+        )}
+      </div>
     </div>
   )
 }
