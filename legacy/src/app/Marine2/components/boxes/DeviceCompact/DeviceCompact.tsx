95d7e981457daabbc57c71205711d2dc526dbac1
Dominik Mysliwiec <dominik@elnino.tech>
2023-02-18 01:50:35 +0100

    add template for standard compact device feature

---
+++ legacy/src/app/Marine2/components/boxes/DeviceCompact/DeviceCompact.tsx
@@ -0,0 +1,35 @@
+const DeviceCompact = ({ icon, title, subTitle, value, unit }: Props) => {
+  return (
+    <div className={"flex flex-row items-center justify-between w-full mb-2"}>
+      <div className={"flex flex-row items-center justify-start"}>
+        <div className={"w-11"}>{icon}</div>
+        <div className={"col-span-7 flex flex-col"}>
+          <div className={"text-base md:text-lg lg:text-xl leading-none md:leading-none lg:leading-none pb-1"}>
+            {title}
+          </div>
+          <div
+            className={
+              "dark:text-victron-gray-dark text-xs md:text-sm lg:text-sm leading-none md:leading-none lg:leading-none "
+            }
+          >
+            {!!subTitle && subTitle}
+          </div>
+        </div>
+      </div>
+      <div className={"col-span-2 justify-self-end text-3xl flex flex-row pr-2"}>
+        <div>{value}</div>
+        <div className={"text-victron-gray/70 pl-1"}>{unit}</div>
+      </div>
+    </div>
+  )
+}
+
+interface Props {
+  icon: JSX.Element
+  title: string
+  subTitle: string | false
+  value: number | string | false
+  unit: string
+}
+
+export default DeviceCompact
---
+++ legacy/src/app/Marine2/components/boxes/DeviceCompact/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./DeviceCompact"
+export * from "./DeviceCompact"
