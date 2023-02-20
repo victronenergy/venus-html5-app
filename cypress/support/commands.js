a7621b0285f869a9b4f7090ebb046eb1daeda57e
Sergey Nechaev <sergey@elnino.tech>
2023-02-20 16:19:19 +0100

    Add threshold to isWithinViewport command to handle fragile tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,10 +22,14 @@ import "@testing-library/cypress/add-commands"
 Cypress.Commands.add(`isWithinViewport`, { prevSubject: true }, (subject, width, height) => {
   const bounding = subject[0].getBoundingClientRect()
 
+  // in headless browser some tests are very fragile because of rounding errors
+  // so we use a threshold to make them more stable
+  const threshold = 3
+
   expect(bounding.top).to.be.at.least(0)
   expect(bounding.left).to.be.at.least(0)
-  expect(bounding.right).to.be.lessThan(width + 2)
-  expect(bounding.bottom).to.be.lessThan(height + 2)
+  expect(bounding.right).to.be.lessThan(width + threshold)
+  expect(bounding.bottom).to.be.lessThan(height + threshold)
 
   return subject
 })
