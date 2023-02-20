08a98907cc82a49db71719ca3b62d294ec4e0b02
Sergey Nechaev <sergey@elnino.tech>
2023-02-20 15:08:54 +0100

    Add version info component

--- src/app/Marine2/components/ui/Footer/Footer.tsx
+++ src/app/Marine2/components/ui/Footer/Footer.tsx
@@ -1,15 +1,11 @@
 import React from "react"
-import LogoIcon from "../../../images/logo.svg"
 import Settings from "../Settings"
+import VersionInfo from "../VersionInfo"
 
 const Footer = () => {
   return (
     <div className="flex w-full flex-row justify-between pt-6 pb-4">
-      <div>
-        {/* todo: fix types for svg */}
-        {/* @ts-ignore */}
-        <LogoIcon className={"w-32 text-black dark:text-white"} alt={"Victron Energy"} />
-      </div>
+      <VersionInfo />
       <Settings />
     </div>
   )
