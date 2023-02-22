b5d70b8dd8c5caf05f9555912d4b67453ff9ead3
andreielnino <andrei@elnino.tech>
2023-02-22 22:59:56 +0100

    Fix dynamic classnames not getting compiled

--- src/app/Marine2/components/ui/ProgressBar/ProgressBar.tsx
+++ src/app/Marine2/components/ui/ProgressBar/ProgressBar.tsx
@@ -3,31 +3,32 @@ import classnames from "classnames"
 
 const ProgressBar = ({ percentage, type, orientation = "horizontal" }: Props) => {
   const color = colorFormatter(type)
+  const bgColor = colorFormatter(type, true)
 
   if (orientation === "horizontal") {
     return (
       <div className="w-full flex p-5">
-        <div className={classnames(`w-1/4 h-2 rounded-l-2xl mr-2 ${color}/30`)}>
+        <div className={classnames(`w-1/4 h-2 rounded-l-2xl mr-2 ${bgColor}`)}>
           <div
-            className={classnames(`h-full rounded-l-2xl ${color}/100`)}
+            className={classnames(`h-full rounded-l-2xl ${color}`)}
             style={{ width: percentage >= 25 ? "100%" : `${percentage * 4}%` }}
           />
         </div>
-        <div className={classnames(`w-1/4 h-2 mr-2 ${color}/30`)}>
+        <div className={classnames(`w-1/4 h-2 mr-2 ${bgColor}`)}>
           <div
-            className={classnames(`h-full ${color}/100`)}
+            className={classnames(`h-full ${color}`)}
             style={{ width: percentage >= 50 ? "100%" : percentage <= 25 ? "0%" : `${(percentage - 25) * 4}%` }}
           />
         </div>
-        <div className={classnames(`w-1/4 h-2 mr-2 ${color}/30`)}>
+        <div className={classnames(`w-1/4 h-2 mr-2 ${bgColor}`)}>
           <div
-            className={classnames(`h-full ${color}/100`)}
+            className={classnames(`h-full ${color}`)}
             style={{ width: percentage >= 75 ? "100%" : percentage <= 50 ? "0%" : `${(percentage - 50) * 4}%` }}
           />
         </div>
-        <div className={classnames(`w-1/4 h-2 rounded-r-2xl mr-2 ${color}/30`)}>
+        <div className={classnames(`w-1/4 h-2 rounded-r-2xl mr-2 ${bgColor}`)}>
           <div
-            className={classnames(`h-full ${color}/100`)}
+            className={classnames(`h-full ${color}`)}
             style={{ width: percentage <= 75 ? "0%" : `${(percentage - 75) * 4}%` }}
           />
         </div>
@@ -37,27 +38,27 @@ const ProgressBar = ({ percentage, type, orientation = "horizontal" }: Props) =>
 
   return (
     <div className="h-full w-full flex flex-col-reverse items-center">
-      <div className={classnames(`h-1/4 w-2 m-1 ${color}/30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 ${bgColor} rotate-180`)}>
         <div
-          className={classnames(`w-full rounded-t-2xl ${color}/100`)}
+          className={classnames(`w-full rounded-t-2xl ${color}`)}
           style={{ height: percentage >= 25 ? "100%" : `${percentage * 4}%` }}
         />
       </div>
-      <div className={classnames(`h-1/4 w-2 m-1 ${color}/30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 ${bgColor} rotate-180`)}>
         <div
-          className={classnames(`w-full ${color}/100`)}
+          className={classnames(`w-full ${color}`)}
           style={{ height: percentage >= 50 ? "100%" : percentage <= 25 ? "0%" : `${(percentage - 25) * 4}%` }}
         />
       </div>
-      <div className={classnames(`h-1/4 w-2 m-1 ${color}/30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 ${bgColor} rotate-180`)}>
         <div
-          className={classnames(`w-full ${color}/100`)}
+          className={classnames(`w-full ${color}`)}
           style={{ height: percentage >= 75 ? "100%" : percentage <= 50 ? "0%" : `${(percentage - 50) * 4}%` }}
         />
       </div>
-      <div className={classnames(`h-1/4 w-2 m-1 rounded-b-2xl ${color}/30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 rounded-b-2xl ${bgColor} rotate-180`)}>
         <div
-          className={classnames(`w-full ${color}/100`)}
+          className={classnames(`w-full ${color}`)}
           style={{ height: percentage <= 75 ? "0%" : `${(percentage - 75) * 4}%` }}
         />
       </div>
@@ -65,7 +66,7 @@ const ProgressBar = ({ percentage, type, orientation = "horizontal" }: Props) =>
   )
 }
 
-const colorFormatter = (type: number) => {
+const colorFormatter = (type: number, opacity = false) => {
   switch (type) {
     case 0:
     case 4:
@@ -73,17 +74,17 @@ const colorFormatter = (type: number) => {
     case 7:
     case 8:
     case 9:
-      return "bg-victron-lime"
+      return opacity ? "bg-victron-lime/30" : "bg-victron-lime/100"
     case 1:
-      return "bg-victron-cyan"
+      return opacity ? "bg-victron-cyan/30" : "bg-victron-cyan/100"
     case 2:
     case 11:
-      return "bg-victron-slate"
+      return opacity ? "bg-victron-slate/30" : "bg-victron-slate/100"
     case 3:
     case 10:
-      return "bg-victron-green"
+      return opacity ? "bg-victron-green/30" : "bg-victron-green/100"
     case 5:
-      return "bg-victron-purple"
+      return opacity ? "bg-victron-purple/30" : "bg-victron-purple/100"
   }
 }
 
