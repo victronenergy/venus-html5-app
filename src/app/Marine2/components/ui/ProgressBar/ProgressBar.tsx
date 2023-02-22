3a5a2963f62ee804e52a339034121137a34745fc
andreielnino <andrei@elnino.tech>
2023-02-22 16:58:24 +0100

    Fix broken bg-opacity for ProgressBar component

--- src/app/Marine2/components/ui/ProgressBar/ProgressBar.tsx
+++ src/app/Marine2/components/ui/ProgressBar/ProgressBar.tsx
@@ -7,27 +7,27 @@ const ProgressBar = ({ percentage, type, orientation = "horizontal" }: Props) =>
   if (orientation === "horizontal") {
     return (
       <div className="w-full flex p-5">
-        <div className={classnames(`w-1/4 h-2 rounded-l-2xl mr-2 ${color} bg-opacity-30`)}>
+        <div className={classnames(`w-1/4 h-2 rounded-l-2xl mr-2 ${color}/30`)}>
           <div
-            className={classnames(`h-full rounded-l-2xl ${color} bg-opacity-100`)}
+            className={classnames(`h-full rounded-l-2xl ${color}/100`)}
             style={{ width: percentage >= 25 ? "100%" : `${percentage * 4}%` }}
           />
         </div>
-        <div className={classnames(`w-1/4 h-2 mr-2 ${color} bg-opacity-30`)}>
+        <div className={classnames(`w-1/4 h-2 mr-2 ${color}/30`)}>
           <div
-            className={classnames(`h-full ${color} bg-opacity-100`)}
+            className={classnames(`h-full ${color}/100`)}
             style={{ width: percentage >= 50 ? "100%" : percentage <= 25 ? "0%" : `${(percentage - 25) * 4}%` }}
           />
         </div>
-        <div className={classnames(`w-1/4 color/30 h-2 mr-2 ${color} bg-opacity-30`)}>
+        <div className={classnames(`w-1/4 h-2 mr-2 ${color}/30`)}>
           <div
-            className={classnames(`h-full ${color} bg-opacity-100`)}
+            className={classnames(`h-full ${color}/100`)}
             style={{ width: percentage >= 75 ? "100%" : percentage <= 50 ? "0%" : `${(percentage - 50) * 4}%` }}
           />
         </div>
-        <div className={classnames(`w-1/4 color/30 h-2 rounded-r-2xl mr-2 ${color} bg-opacity-30`)}>
+        <div className={classnames(`w-1/4 h-2 rounded-r-2xl mr-2 ${color}/30`)}>
           <div
-            className={classnames(`h-full rounded-r-2xl ${color} bg-opacity-100`)}
+            className={classnames(`h-full ${color}/100`)}
             style={{ width: percentage <= 75 ? "0%" : `${(percentage - 75) * 4}%` }}
           />
         </div>
@@ -37,27 +37,27 @@ const ProgressBar = ({ percentage, type, orientation = "horizontal" }: Props) =>
 
   return (
     <div className="h-full w-full flex flex-col-reverse items-center">
-      <div className={classnames(`h-1/4 w-2 m-1 ${color} bg-opacity-30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 ${color}/30 rotate-180`)}>
         <div
-          className={classnames(`w-full rounded-t-2xl ${color} bg-opacity-100`)}
+          className={classnames(`w-full rounded-t-2xl ${color}/100`)}
           style={{ height: percentage >= 25 ? "100%" : `${percentage * 4}%` }}
         />
       </div>
-      <div className={classnames(`h-1/4 w-2 m-1 ${color} bg-opacity-30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 ${color}/30 rotate-180`)}>
         <div
-          className={classnames(`w-full ${color} bg-opacity-100`)}
+          className={classnames(`w-full ${color}/100`)}
           style={{ height: percentage >= 50 ? "100%" : percentage <= 25 ? "0%" : `${(percentage - 25) * 4}%` }}
         />
       </div>
-      <div className={classnames(`h-1/4 w-2 m-1 ${color} bg-opacity-30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 ${color}/30 rotate-180`)}>
         <div
-          className={classnames(`w-full ${color} bg-opacity-100`)}
+          className={classnames(`w-full ${color}/100`)}
           style={{ height: percentage >= 75 ? "100%" : percentage <= 50 ? "0%" : `${(percentage - 50) * 4}%` }}
         />
       </div>
-      <div className={classnames(`h-1/4 w-2 m-1 rounded-b-2xl ${color} bg-opacity-30 rotate-180`)}>
+      <div className={classnames(`h-1/4 w-2 m-1 rounded-b-2xl ${color}/30 rotate-180`)}>
         <div
-          className={classnames(`w-full ${color} bg-opacity-100`)}
+          className={classnames(`w-full ${color}/100`)}
           style={{ height: percentage <= 75 ? "0%" : `${(percentage - 75) * 4}%` }}
         />
       </div>
