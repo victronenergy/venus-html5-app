3446206e342741c3fe9b7003e1cbda7d5a14c1fb
Sergey Nechaev <sergey@elnino.tech>
2023-02-24 01:52:15 +0100

    Add visibility logic to BatteriesOverview

--- src/app/Marine2/components/boxes/BatteriesOverview/BatteriesOverview.tsx
+++ src/app/Marine2/components/boxes/BatteriesOverview/BatteriesOverview.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite"
 import { useBattery } from "@elninotech/mfd-modules"
 import { Battery as BatteryType } from "@elninotech/mfd-modules"
 import Box from "../../ui/Box"
-import { BATTERY } from "../../../utils/constants"
+import { BATTERY, WIDGET_TYPES } from "../../../utils/constants"
 import Grid from "../../ui/Grid"
 import Battery from "../Battery/Battery"
 import BatteriesIcon from "../../../images/icons/batteries.svg"
@@ -12,6 +12,7 @@ import { withErrorBoundary } from "react-error-boundary"
 import { AppViews } from "../../../modules/AppViews"
 import { translate } from "react-i18nify"
 import { appErrorBoundaryProps } from "../../ui/Error/appErrorBoundary"
+import { useVisibilityNotifier } from "../../../modules"
 
 interface Props {
   mode?: "compact" | "full"
@@ -19,6 +20,8 @@ interface Props {
 
 const BatteriesOverview = ({ mode = "full" }: Props) => {
   const { batteries } = useBattery()
+
+  useVisibilityNotifier({ widgetName: WIDGET_TYPES.BATTERY, visible: !!(batteries && batteries.length) })
 
   const sortedBatteries = sortBatteries(batteries ?? [])
   const overviewBatteries = getOverviewBatteries(sortedBatteries, 2)
