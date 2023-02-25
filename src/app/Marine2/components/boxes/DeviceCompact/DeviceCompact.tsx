0d9c52fc70b2537772c916d9883b3e47227af8d9
Dominik Mysliwiec <dominik@elnino.tech>
2023-02-25 14:31:20 +0100

    make compact device more responsive to screen size changes

--- src/app/Marine2/components/boxes/DeviceCompact/DeviceCompact.tsx
+++ src/app/Marine2/components/boxes/DeviceCompact/DeviceCompact.tsx
@@ -1,16 +1,26 @@
 const DeviceCompact = ({ icon, title, subTitle, value, unit }: Props) => {
   return (
-    <div className={"flex flex-row items-center justify-between w-full mb-2"}>
-      <div className={"flex flex-row items-center justify-start"}>
-        <div className={"w-11"}>{icon}</div>
-        <div className={"flex flex-col"}>
-          <div className={"text-base leading-none md:leading-none lg:leading-none pb-1"}>{title}</div>
-          <div className={"dark:text-victron-gray-dark text-sm leading-none md:leading-none lg:leading-none "}>
+    <div className={"flex flex-row items-center justify-between w-full mb-2 md:mb-3 lg:mb-4 min-w-0"}>
+      <div className={"shrink min-w-0 flex flex-row items-center justify-start"}>
+        <div className={"shrink-0 w-11"}>{icon}</div>
+        <div className={"shrink min-w-0 flex flex-col mr-2"}>
+          <div
+            className={
+              "text-xs sm:text-sm md:text-base lg:text-base leading-none sm:leading-none md:leading-none lg:leading-none pb-1 truncate"
+            }
+          >
+            {title}
+          </div>
+          <div
+            className={
+              "dark:text-victron-gray-dark text-xs md:text-sm lg:text-sm leading-none md:leading-none lg:leading-none truncate"
+            }
+          >
             {!!subTitle && subTitle}
           </div>
         </div>
       </div>
-      <div className={"justify-self-end text-xl flex flex-row pr-2"}>
+      <div className={"justify-self-end text-sm sm:text-base md:text-lg lg:text-xl flex flex-row pr-2 w-max"}>
         <div>{value}</div>
         <div className={"text-victron-gray/70 pl-1"}>{unit}</div>
       </div>
