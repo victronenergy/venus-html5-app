a059518e949aea2797c39993736ed07cbf12e9e1
andreielnino <andrei@elnino.tech>
2023-02-26 19:40:40 +0100

    Apply media queries for box size using boxRef

--- src/app/Marine2/components/ui/Box/Box.tsx
+++ src/app/Marine2/components/ui/Box/Box.tsx
@@ -1,18 +1,28 @@
-import React from "react"
+import React, { useEffect } from "react"
 import classNames from "classnames"
 import ArrowRightIcon from "../../../images/icons/arrow-right.svg"
 import { AppViews, useAppViewsStore } from "../../../modules/AppViews"
+import { useComponentSize } from "app/Marine2/utils/hooks"
 
-const Box = ({ children, icon, title, className, linkedView }: BoxProps) => {
+const Box = ({ children, icon, title, className, linkedView, getBoxSizeCallback }: BoxProps) => {
   const appViewsStore = useAppViewsStore()
   const handleClick = () => {
     if (linkedView) {
       appViewsStore.setView(linkedView)
     }
   }
+  const boxRef = React.useRef<HTMLDivElement>(null)
+  const componentSize = useComponentSize(boxRef)
+
+  useEffect(() => {
+    if (getBoxSizeCallback) {
+      getBoxSizeCallback(componentSize)
+    }
+  }, [componentSize])
 
   return (
     <div
+      ref={boxRef}
       className={classNames(
         "w-full h-full min-h-0 p-4 flex flex-col bg-victron-lightGray dark:bg-victron-darkGray rounded-md",
         className
@@ -51,6 +61,7 @@ export interface BoxProps {
   linkedView?: AppViews
   className?: string
   headerActions?: JSX.Element
+  getBoxSizeCallback?: (size: { width: number; height: number }) => void
 }
 
 export default Box
