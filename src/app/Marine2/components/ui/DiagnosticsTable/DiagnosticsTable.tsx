6f5ce11e07088a733296ab6e17abb3d50cdc7883
Sergey Nechaev <sergey@elnino.tech>
2023-03-06 20:35:49 +0100

    Fix styles for Diagnostics table

--- src/app/Marine2/components/ui/DiagnosticsTable/DiagnosticsTable.tsx
+++ src/app/Marine2/components/ui/DiagnosticsTable/DiagnosticsTable.tsx
@@ -3,13 +3,16 @@ import React from "react"
 const DiagnosticsTable = ({ title, diagnostics }: Props) => {
   return (
     <div>
-      <h2 className={"text-sm md-m:text-base"}>{title}</h2>
+      <h2 className={"text-sm md-m:text-base mb-2"}>{title}</h2>
       <table className={"table-fixed border-collapse w-full text-xs md-m:text-sm"}>
         <tbody>
           {diagnostics.map(({ property, value }) => (
-            <tr key={property} className={"border-b border-t border-victron-gray dark:border-victron-gray-dark"}>
-              <td className={"w-1/3"}>{property}</td>
-              <td>{value}</td>
+            <tr
+              key={property}
+              className={"border-b border-t border-victron-gray dark:border-victron-gray-dark align-top"}
+            >
+              <td className={"w-1/3 py-2"}>{property}</td>
+              <td className={"py-2"}>{value}</td>
             </tr>
           ))}
         </tbody>
