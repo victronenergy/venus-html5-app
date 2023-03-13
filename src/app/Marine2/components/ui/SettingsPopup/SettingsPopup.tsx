5c8d58a79fe5d338da375867cb5eac0acba5173d
Sergey Nechaev <sergey@elnino.tech>
2023-03-13 13:25:37 +0100

    Add styles and class names for SettingsPopup component

--- src/app/Marine2/components/ui/SettingsPopup/SettingsPopup.tsx
+++ src/app/Marine2/components/ui/SettingsPopup/SettingsPopup.tsx
@@ -21,7 +21,7 @@ const Frame = ({ children, title, className }: Props) => {
         onClose={() => {
           setIsModalOpen(false)
         }}
-        className="w-full max-w-sm"
+        className="w-full max-w-sm border-victron-gray-200 border-2"
       >
         <Modal.Body>{children}</Modal.Body>
       </Modal.Frame>
@@ -29,12 +29,18 @@ const Frame = ({ children, title, className }: Props) => {
   )
 }
 
-const Header: React.FC<{ children: React.ReactNode }> = ({ children }) => <div className="text-sm">{children}</div>
-const Body: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div className="flex flex-col text-sm mb-2 dark:text-white md:mb-4 md-m:text-base">{children}</div>
+const Header: React.FC<{ children: React.ReactNode; className?: string }> = ({ children, className }) => (
+  <div className={classnames("text-md text-center", className)}>{children}</div>
+)
+const Body: React.FC<{ children: React.ReactNode; className?: string }> = ({ children, className }) => (
+  <div className={classnames("flex flex-col text-sm mb-2 dark:text-white md:mb-4 md-m:text-base p-4", className)}>
+    {children}
+  </div>
 )
 
-const Footer: React.FC<{ children: React.ReactNode }> = ({ children }) => <div>{children}</div>
+const Footer: React.FC<{ children: React.ReactNode; className?: string }> = ({ children, className }) => (
+  <div className={classnames("w-full flex flex-row justify-between", className)}>{children}</div>
+)
 
 const SettingsPopup = { Frame, Header, Body, Footer }
 export default SettingsPopup
