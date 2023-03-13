dcdd895b7aaf2c333b146a72a3a61d127de91515
Sergey Nechaev <sergey@elnino.tech>
2023-03-13 13:22:53 +0100

    Add transparent variant for Button

--- src/app/Marine2/components/ui/Button/Button.tsx
+++ src/app/Marine2/components/ui/Button/Button.tsx
@@ -7,18 +7,23 @@ interface Props {
   className?: string
   disabled?: boolean
   size?: "lg" | "md"
+  variant?: "primary" | "transparent"
 }
 
-const Button = ({ children, onClick, className, disabled, size = "lg", ...props }: Props) => {
+const Button = ({ children, onClick, className, disabled, size = "lg", variant = "primary", ...props }: Props) => {
   return (
     <button
       className={classnames(
-        "rounded-md px-4 py-1.5 border-2 whitespace-nowrap",
+        "px-4 py-1.5 whitespace-nowrap",
         {
+          "rounded-md border-2": variant === "primary",
           "bg-victron-blue/30 dark:bg-victron-blue-dark/30 border-victron-blue text-black dark:text-white cursor-pointer":
-            !disabled,
+            !disabled && variant === "primary",
           "bg-victron-gray/30 dark:bg-victron-gray-dark/30 border-victron-gray text-victron-gray dark:text-victron-gray-dark":
-            disabled,
+            disabled && variant === "primary",
+          "bg-transparent text-black dark:text-white cursor-pointer cursor-pointer":
+            !disabled && variant === "transparent",
+          "bg-transparent text-victron-gray dark:text-victron-gray-dark": disabled && variant === "transparent",
           "text-base": size === "lg",
           "text-sm": size === "md",
         },
