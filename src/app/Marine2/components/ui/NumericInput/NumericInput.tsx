d923f1bbcb9659ce553b4e0ca8f763f7f9de7b33
Sergey Nechaev <sergey@elnino.tech>
2023-03-13 17:08:36 +0100

    Add NumericInput component

---
+++ src/app/Marine2/components/ui/NumericInput/NumericInput.tsx
@@ -0,0 +1,94 @@
+import { ReactNode, useState } from "react"
+import classnames from "classnames"
+
+interface Props {
+  initialValue: number
+  unit?: string
+  className?: string
+  onChange?: (value: number) => void
+  max?: number
+  min?: number
+  step?: number
+  range?: number[]
+}
+
+const NumericInputButton = ({ children, onClick }: { children: ReactNode; onClick: () => void }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={
+        "bg-victron-blue/70 dark:bg-victron-blue-dark/70 border-victron-blue text-black dark:text-white cursor-pointer py-4 px-10 rounded-md text-lg"
+      }
+    >
+      {children}
+    </button>
+  )
+}
+
+const NumericInputRange = ({
+  onChange,
+  range,
+  className,
+}: {
+  onChange: (value: number) => void
+  range: number[]
+  className?: string
+}) => {
+  if (!range?.length) return null
+
+  return (
+    <div
+      className={classnames(
+        "flex justify-between items-center w-full rounded-md text-md border-victron-blue border-2 bg-victron-blue/30 dark:bg-victron-blue-dark/30 text-black dark:text-white",
+        className
+      )}
+    >
+      {range.map((val) => (
+        <div key={val} className={"py-2 px-3"} onClick={() => onChange(val)}>
+          {val}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const NumericInput = ({ initialValue = 0, unit, onChange, className, min, max, step = 1, range }: Props) => {
+  const [value, setValue] = useState(initialValue)
+
+  const increase = () => {
+    const newValue = max ? Math.max(value + step, max) : value + step
+    setValue(newValue)
+    onChange && onChange(newValue)
+  }
+
+  const decrease = () => {
+    const newValue = min ? Math.min(value - step, min) : value - step
+    setValue(newValue)
+    onChange && onChange(newValue)
+  }
+
+  const setExactValue = (val: number) => {
+    setValue(val)
+    onChange && onChange(val)
+  }
+
+  return (
+    <div className={classnames(className)}>
+      <div className={"flex justify-between items-center mt-4"}>
+        <NumericInputButton onClick={decrease}>−</NumericInputButton>
+        <div className={"text-2xl text-victron-darkGray dark:text-white"}>
+          {value}
+          <span className={"pl-0.5 text-victron-gray dark:text-victron-gray-500"}>{unit}</span>
+        </div>
+        <NumericInputButton onClick={increase}>+</NumericInputButton>
+      </div>
+      {range?.length && (
+        <div>
+          <NumericInputRange onChange={setExactValue} range={range} className={"mt-10 mb-2"} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default NumericInput
