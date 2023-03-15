e37376d5bfcc88020bf24cf4117e2057cc14ed07
Sergey Nechaev <sergey@elnino.tech>
2023-03-15 14:52:03 +0100

    Add first and last child tests for Grid component

--- src/app/Marine2/components/ui/Grid/Grid.test.js
+++ src/app/Marine2/components/ui/Grid/Grid.test.js
@@ -35,8 +35,64 @@ describe("Grid element", () => {
       expect(wrapper.find("Box").length).toBe(3)
     })
 
-    it.only("should use col flow", () => {
+    it("should use col flow", () => {
       expect(wrapper.find("Box").first().parents(".flex.flex-wrap").prop("className")).toContain("flex-col")
+    })
+  })
+
+  describe("first child full height size", () => {
+    it("should use 100% width for first child in row flow", () => {
+      const wrapper = mount(
+        <Grid>
+          <Box title={"Box1"}>Box1</Box>
+          <Box title={"Box2"}>Box2</Box>
+          <Box title={"Box3"}>Box3</Box>
+        </Grid>
+      )
+
+      expect(wrapper.find("Box").first().parent().prop("style")).toHaveProperty("width", "100%")
+      expect(wrapper.find("Box").first().parent().prop("style")).toHaveProperty("height", "50%")
+    })
+
+    it("should use 100% height for first child in col flow", () => {
+      const wrapper = mount(
+        <Grid flow={"col"}>
+          <Box title={"Box1"}>Box1</Box>
+          <Box title={"Box2"}>Box2</Box>
+          <Box title={"Box3"}>Box3</Box>
+        </Grid>
+      )
+
+      expect(wrapper.find("Box").first().parent().prop("style")).toHaveProperty("height", "100%")
+      expect(wrapper.find("Box").first().parent().prop("style")).toHaveProperty("width", "50%")
+    })
+  })
+
+  describe("last child full height size", () => {
+    it("should use 100% width for last child in row flow", () => {
+      const wrapper = mount(
+        <Grid forceFirstOrLastChild={"last"}>
+          <Box title={"Box1"}>Box1</Box>
+          <Box title={"Box2"}>Box2</Box>
+          <Box title={"Box3"}>Box3</Box>
+        </Grid>
+      )
+
+      expect(wrapper.find("Box").last().parent().prop("style")).toHaveProperty("width", "100%")
+      expect(wrapper.find("Box").last().parent().prop("style")).toHaveProperty("height", "50%")
+    })
+
+    it("should use 100% height for last child in col flow", () => {
+      const wrapper = mount(
+        <Grid flow={"col"} forceFirstOrLastChild={"last"}>
+          <Box title={"Box1"}>Box1</Box>
+          <Box title={"Box2"}>Box2</Box>
+          <Box title={"Box3"}>Box3</Box>
+        </Grid>
+      )
+
+      expect(wrapper.find("Box").last().parent().prop("style")).toHaveProperty("height", "100%")
+      expect(wrapper.find("Box").last().parent().prop("style")).toHaveProperty("width", "50%")
     })
   })
 })
