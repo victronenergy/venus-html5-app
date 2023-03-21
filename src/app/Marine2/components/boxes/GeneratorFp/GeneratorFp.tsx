9766e038329f5324b9c62f81056a87d1421c0959
NikMys <d.jaktplan@gmail.com>
2023-03-21 13:10:23 +0100

    use new valuebars and activestyles in generatorfp

--- src/app/Marine2/components/boxes/GeneratorFp/GeneratorFp.tsx
+++ src/app/Marine2/components/boxes/GeneratorFp/GeneratorFp.tsx
@@ -5,12 +5,52 @@ import { GENSET_STATE } from "../../../utils/constants"
 import { translate } from "react-i18nify"
 import { formatPower } from "../../../utils/format"
 import DeviceCompact from "../DeviceCompact"
-import range from "lodash-es/range"
 import Box from "../../ui/Box"
 import Button from "../../ui/Button"
 import classnames from "classnames"
-import { useComponentSize, useWindowSize } from "../../../utils/hooks"
-import { useEffect, useRef, useState } from "react"
+import { useState } from "react"
+import ValueBar from "../../ui/ValueBar"
+import { applyStyles, BreakpointStylesType } from "../../../utils/media"
+import { withErrorBoundary } from "react-error-boundary"
+import { appErrorBoundaryProps } from "../../ui/Error/appErrorBoundary"
+
+const styles: BreakpointStylesType = {
+  default: {
+    flow: "flex-row",
+    value: "text-base w-1/2 shrink",
+    valueSubtitle: "w-1/2 shrink-0 grow text-xs",
+    subValue: "text-sm",
+    valueBars: "text-sm",
+  },
+  "sm-s": {
+    flow: "flex-col",
+    value: "text-base w-full",
+    valueSubtitle: "w-fit mt-3 text-xs",
+    subValue: "text-sm",
+    valueBars: "text-sm",
+  },
+  "sm-m": {
+    flow: "flex-col",
+    value: "text-lg w-full",
+    valueSubtitle: "w-fit text-xs mt-5",
+    subValue: "text-sm",
+    valueBars: "text-base",
+  },
+  "md-s": {
+    flow: "flex-row",
+    value: "text-xl w-1/2 shrink",
+    valueSubtitle: "w-1/2 shrink-0 grow text-xs",
+    subValue: "text-lg",
+    valueBars: "text-lg",
+  },
+  "md-m": {
+    flow: "flex-col",
+    value: "text-3xl w-full",
+    valueSubtitle: "w-fit text-base mt-5",
+    subValue: "text-lg",
+    valueBars: "text-lg",
+  },
+}
 
 const GeneratorFp = ({ mode = "compact", generatorFp }: Props) => {
   const gensetStateFormatter = (value: number) => {
@@ -29,9 +69,9 @@ const GeneratorFp = ({ mode = "compact", generatorFp }: Props) => {
     }
   }
 
-  const { productName, phases, statusCode, gensetAutoStart, autoStart, updateAutoMode, updateManualMode } = generatorFp
+  const { productName, phases, statusCode, gensetAutoStart } = generatorFp //TODO: once design is ready use autoStart, updateAutoMode, updateManualMode
   const gensetValues = useGensetValues()
-  const { voltage, current, power, frequency, coolant, winding, exhaust } = gensetValues
+  const { voltage, current, power, coolant, winding, exhaust } = gensetValues
   // When a topic is invalid, it returns undefined -> no value means topic is not supported
   const title = productName || "Genset"
   const subTitle = (!!statusCode || statusCode === 0) && gensetStateFormatter(Number(statusCode))
@@ -42,19 +82,17 @@ const GeneratorFp = ({ mode = "compact", generatorFp }: Props) => {
   const powerFormatted = formatPower(powerSum)
   const unit = powerSum > 1000 ? "kW" : "W"
 
-  const parentRef = useRef<HTMLDivElement>(null)
-  const componentSize = useComponentSize(parentRef)
-  const windowSize = useWindowSize()
-  const [isFullHeight, setFullHeight] = useState<boolean>(false)
+  const [boxSize, setBoxSize] = useState<{ width: number; height: number }>({ width: 0, height: 0 })
+  const activeStyles = applyStyles(boxSize, styles)
 
-  useEffect(() => {
-    if (!windowSize || !componentSize) return
-    if (windowSize.height !== undefined && componentSize.height < windowSize.height / 2) {
-      setFullHeight(false)
-    } else {
-      setFullHeight(true)
-    }
-  }, [windowSize, componentSize])
+  const phasesOverview = []
+  for (let phase = 0; phase < phases; phase++) {
+    phasesOverview.push([
+      { value: voltage[phase], unit: "V" },
+      { value: current[phase], unit: "A" },
+      { value: power[phase], unit: "W" },
+    ])
+  }
 
   if (mode === "compact") {
     return (
@@ -84,45 +122,34 @@ const GeneratorFp = ({ mode = "compact", generatorFp }: Props) => {
         ></GeneratorIcon>
       }
       title={title}
+      getBoxSizeCallback={setBoxSize}
     >
-      <div ref={parentRef} className="w-full h-full flex flex-col justify-between">
-        <div
-          className={classnames("flex w-full", {
-            "flex-row": !isFullHeight,
-            "flex-col": isFullHeight,
-          })}
-        >
+      <div className="w-full h-full flex flex-col justify-between">
+        <div className={classnames("flex w-full", activeStyles?.flow)}>
+          <div className={classnames("mt-1 flex flex-row pr-2", activeStyles?.value)}>{subTitle}</div>
           <div
-            className={classnames("mt-1 flex flex-row pr-2", {
-              "text-3xl w-full": isFullHeight,
-              "text-base sm:text-lg md:text-xl lg:text-xl w-1/2 shrink": !isFullHeight,
-            })}
+            className={classnames(
+              "flex flex-row mr-4 justify-start text-victron-gray min-w-0",
+              activeStyles?.valueSubtitle
+            )}
           >
-            {subTitle}
-          </div>
-          <div
-            className={classnames("flex flex-row justify-start text-victron-gray min-w-0", {
-              "w-full": isFullHeight,
-              "w-1/2 shrink-0 grow": !isFullHeight,
-            })}
-          >
-            <div className="flex flex-none flex-col mr-3 w-1/4 truncate">
+            <div className="flex flex-none flex-col mr-3 w-1/3 truncate">
               <span>Coolant</span>
-              <div className="text-white">
+              <div className={classnames("text-white", activeStyles?.subValue)}>
                 {coolant}
                 <span>°</span>
               </div>
             </div>
-            <div className="flex flex-none flex-col mr-3 w-1/4 truncate">
+            <div className="flex flex-none flex-col mr-3 w-1/3 truncate">
               <span>Winding</span>
-              <div className="text-white">
+              <div className={classnames("text-white", activeStyles?.subValue)}>
                 {winding}
                 <span>°</span>
               </div>
             </div>
-            <div className="flex flex-none flex-col w-1/4 truncate">
+            <div className="flex flex-none flex-col w-1/3 truncate">
               <span>Exhaust</span>
-              <div className="text-white">
+              <div className={classnames("text-white", activeStyles?.subValue)}>
                 {exhaust}
                 <span>°</span>
               </div>
@@ -130,35 +157,8 @@ const GeneratorFp = ({ mode = "compact", generatorFp }: Props) => {
           </div>
         </div>
         <div className="w-full h-full min-h-0 shrink flex flex-col justify-end mt-2">
-          <div className={"shrink overflow-hidden"}>
-            {!!gensetValues &&
-              range(phases).map((_, i) => (
-                <>
-                  <div
-                    className={classnames("flex flex-row justify between text-victron-gray text-xs min-h-0", {
-                      " md:text-base lg:text-xl": isFullHeight,
-                    })}
-                  >
-                    <p className="w-8 sm:w-8 md:w-10 lg:w-16 shrink-0">{`L${i + 1}`}</p>
-                    <div className="grow shrink flex justify-between w-full min-w-0 whitespace-nowrap">
-                      <p className={"w-1/5 overflow-hidden"}>
-                        {(voltage[i] ?? 0).toFixed(1)} <span className="text-victron-gray-400">V</span>
-                      </p>
-                      <p className={"w-1/5 overflow-hidden"}>
-                        {(current[i] ?? 0).toFixed(1)} <span className="text-victron-gray-400">A</span>
-                      </p>
-                      <p className={"w-1/5 overflow-hidden"}>
-                        {formatPower(power[i] ?? 0)}{" "}
-                        <span className="text-victron-gray-400">{(power[i] ?? 0) > 1000 ? "kW" : "W"}</span>
-                      </p>
-                      <p className={"w-1/5 overflow-hidden"}>
-                        {(frequency[i] ?? 0).toFixed(1)} <span className="text-victron-gray-400">Hz</span>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="my-1 border-[1px] border-victron-darkGray-2" />
-                </>
-              ))}
+          <div className={classnames("shrink overflow-hidden", activeStyles?.valueBars)}>
+            {!!gensetValues && phasesOverview.map((v, i) => <ValueBar key={i} prefix={"L" + (i + 1)} values={v} />)}
           </div>
           <Button size="md" className="flex-none mt-3" disabled={isAutoStartDisabled}>
             {translate("common.autoStartStop")}
@@ -174,4 +174,4 @@ interface Props {
   generatorFp: GeneratorFpProvider
 }
 
-export default observer(GeneratorFp)
+export default withErrorBoundary(observer(GeneratorFp), appErrorBoundaryProps)
