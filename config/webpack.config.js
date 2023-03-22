9fccddae3c84a504857b8e6e1ba5eccb55d11462
Alex Imbrea <alex@elnino.tech>
2023-03-22 19:00:37 +0100

    Add assets path

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -176,6 +176,7 @@ module.exports = function (webpackEnv) {
       chunkFilename: isEnvProduction
         ? "static/js/[name].[contenthash:8].chunk.js"
         : isEnvDevelopment && "static/js/[name].chunk.js",
+      assetModuleFilename: 'static/media/[name].[hash][ext]',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
