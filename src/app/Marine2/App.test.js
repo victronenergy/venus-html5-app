193b8a82fe791f5d1caf4ac020c02eb3974502bc
Sergey Nechaev <sergey@elnino.tech>
2023-03-23 22:42:24 +0100

    Fix tests for App

--- src/app/Marine2/App.test.js
+++ src/app/Marine2/App.test.js
@@ -1,13 +1,12 @@
-import { mount } from "enzyme"
+import { mount, shallow } from "enzyme"
 import React from "react"
 import App from "./App"
 
 describe("App element", () => {
   describe("error boundary", () => {
-    const wrapper = mount(<App />)
+    const wrapper = shallow(<App />)
 
     it("should use error boundary", () => {
-      console.log("-> wrapper.debug()", wrapper.debug())
       const errorBoundary = wrapper.find("ErrorBoundary")
       expect(errorBoundary.exists()).toBe(true)
       expect(errorBoundary.find("Memo(App)").exists()).toBe(true)
