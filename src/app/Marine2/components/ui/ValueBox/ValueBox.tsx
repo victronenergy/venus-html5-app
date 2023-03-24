ec71833709a6e907a6eef9d6643b16f7b58656bf
NikMys <d.jaktplan@gmail.com>
2023-03-24 13:24:58 +0100

    add possibility of string value in valuebox

--- src/app/Marine2/components/ui/ValueBox/ValueBox.tsx
+++ src/app/Marine2/components/ui/ValueBox/ValueBox.tsx
@@ -37,7 +37,7 @@ const ValueBox = ({ title, icon, value, unit, bottomValues, children, buttons, i
   const activeStyles = applyStyles(boxSize, styles)
   const isMultiPhase = bottomValues.length > 1
 
-  if (unit === "W" && value && value > 1000) {
+  if (unit === "W" && value && typeof value === "number" && value > 1000) {
     unit = "kW"
     value = value ? value / 1000 : value
   }
@@ -47,8 +47,10 @@ const ValueBox = ({ title, icon, value, unit, bottomValues, children, buttons, i
       <div className="w-full h-full flex flex-col justify-between">
         <div className={"w-full h-full shrink overflow-hidden"}>
           <div className={classNames("text-victron-darkGray dark:text-white", activeStyles?.value)}>
-            {inputLimitValue ?? formatValue(value)}
-            <span className={"pl-0.5 text-victron-gray dark:text-victron-gray-500"}>{unit}</span>
+            {(typeof value === "number" && formatValue(value)) || value}
+            {typeof value === "number" && (
+              <span className={"pl-0.5 text-victron-gray dark:text-victron-gray-500"}>{unit}</span>
+            )}
           </div>
           <div className={classNames("text-victron-gray dark:text-victron-gray-500", activeStyles.valueSubtitle)}>
             {children}
@@ -70,9 +72,8 @@ const ValueBox = ({ title, icon, value, unit, bottomValues, children, buttons, i
 interface Props {
   icon?: JSX.Element
   title: string
-  value?: number
-  inputLimitValue?: JSX.Element
-  unit: string
+  value?: number | string
+  unit?: string
   bottomValues: ValueWithUnit[][]
   children?: JSX.Element | JSX.Element[] | string
   buttons?: JSX.Element | JSX.Element[]
