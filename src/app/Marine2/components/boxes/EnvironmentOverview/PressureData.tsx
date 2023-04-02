3d15459c7f1901d811b949e907a80500097f10a4
ccioloss <rarencoandrei@gmail.com>
2023-04-02 23:42:02 +0200

    Add pressure data component

---
+++ src/app/Marine2/components/boxes/EnvironmentOverview/PressureData.tsx
@@ -0,0 +1,49 @@
+import { observer } from "mobx-react-lite"
+import { usePressure } from "@elninotech/mfd-modules"
+import { useEffect } from "react"
+import ValueOverview from "../../ui/ValueOverview"
+import ThermometerIcon from "../../../images/icons/thermometer.svg"
+import ValueBox from "../../ui/ValueBox"
+import { translate } from "react-i18nify"
+
+interface Props {
+  dataId: number
+  mode?: "compact" | "full"
+  boxSize: { width: number; height: number }
+}
+
+const PressureData = ({ dataId, mode, boxSize }: Props) => {
+  const { pressure, customName } = usePressure(dataId)
+
+  if (pressure === undefined || customName === undefined) {
+    return null
+  }
+
+  if (mode === "compact") {
+    return (
+      <ValueOverview
+        /* @ts-ignore */
+        Icon={ThermometerIcon}
+        title={customName}
+        value={pressure}
+        boxSize={boxSize}
+        unit={"hPa"}
+        valueType={"environment"}
+      />
+    )
+  }
+
+  return (
+    <ValueBox
+      title={translate("boxes.pressure") + " " + customName}
+      /* todo: fix types for svg */
+      /* @ts-ignore */
+      icon={<ThermometerIcon className={"w-5"} />}
+      value={pressure}
+      bottomValues={[]}
+      unit={"hPa"}
+    />
+  )
+}
+
+export default observer(PressureData)
