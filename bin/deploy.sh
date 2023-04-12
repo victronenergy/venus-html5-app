e96e28652365df825eb3269812b0469726a0c5c8
Jarco van Roest <jarco@elnino.tech>
2023-04-12 16:38:29 +0200

    Move mkpath under cli flag to not fill in ssh key twice
    No support for the native rsync --mkpath yet. Does not have to run
    every time so not necessary to fill ssh into device twice.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 BUILD=false
+MKPATH=false
 POSITIONAL=()
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -18,6 +19,10 @@ while [[ $# -gt 0 ]]; do
         ;;
         -b|--build)
             BUILD=true
+            shift # past argument
+        ;;
+        -m|--mkpath)
+            MKPATH=true
             shift # past argument
         ;;
         *)  # unknown option
@@ -38,6 +43,9 @@ fi
 
 echo "Uploading dist/* to ${USERNAME}@${HOST}:/data/www/app/"
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${USERNAME}@${HOST} "mkdir -p /data/www/app/"
+if $MKPATH; then
+  echo "mkdir -p /data/www/app/"
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${USERNAME}@${HOST} "mkdir -p /data/www/app/"
+fi
 
 rsync --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" -r dist/* ${USERNAME}@${HOST}:/data/www/app/
