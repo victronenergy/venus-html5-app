ae3b221261fac3b8e13e2a11b1dea1ee19df16ae
andrei <andrei@elnino.tech>
2023-07-18 16:10:04 +0200

    Cleanup

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,8 +12,6 @@ import { precacheAndRoute } from "workbox-precaching"
 import { registerRoute } from "workbox-routing"
 import { StaleWhileRevalidate } from "workbox-strategies"
 
-console.log("Hello from service-worker.js")
-
 clientsClaim()
 
 /**
