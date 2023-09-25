25c0dca0927cf6cd9c47e21ae111a02e973802ce
Frank Waalkens <f.waalkens@tfe.nl>
2023-09-25 11:57:55 +0200

    Prettier and removed unnecessary div.

--- src/app/Marine2/components/ui/VersionInfo/VersionInfo.tsx
+++ src/app/Marine2/components/ui/VersionInfo/VersionInfo.tsx
@@ -35,46 +35,43 @@ const VersionInfo = () => {
       {/* todo: fix types for svg */}
       {/* @ts-ignore */}
       <LogoOnlyIcon className="w-10 text-black dark:text-white sm:hidden block" alt="Victron Energy" />
-
-
-        <Modal.Frame
-          open={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          className="w-full max-w-sm mb-16 ml-6 bottom-0 -left-4"
-        >
-          <Modal.Body variant="popUp">
-            <div className="text-sm dark:text-white md-m:text-base lg-s:text-sm lg-l:text-base">
-              {/* todo: fix types for svg */}
-              {/* @ts-ignore */}
-              <LogoIcon className="w-24 text-black dark:text-white" alt="Victron Energy" />
-              <div className="flex flex-col mt-1">
-                <div className="py-2">
-                  <Translate
-                    value="versionInfo.version"
-                    version={`${packageInfo.version} ${process.env.REACT_APP_WHITELABEL}`}
-                  />
-                </div>
-                <div className="flex flex-row justify-between">
-                  <div className="min-w-sm">{translate("versionInfo.buildDate")}</div>
-                  <div>{BUILD_TIMESTAMP}</div>
-                </div>
-                <div className="flex flex-row justify-between">
-                  <div>{translate("versionInfo.venusOs")}</div>
-                  <div>{humanReadableFirmwareVersion}</div>
-                </div>
-                <div className="flex flex-row justify-between">
-                  <div>{translate("versionInfo.identifier")}</div>
-                  <div>{portalId}</div>
-                </div>
-              </div>
-              <div className="mt-2">
-                <Button onClick={openDiagnostics} className="w-full mt-1" size="md">
-                  {translate("diagnostics.diagnostics")}
-                </Button>
-              </div>
+      <Modal.Frame
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        className="w-full max-w-sm mb-16 ml-6 bottom-0 -left-4"
+      >
+        <Modal.Body variant="popUp">
+          <div className="text-sm dark:text-white md-m:text-base lg-s:text-sm lg-l:text-base">
+            {/* todo: fix types for svg */}
+            {/* @ts-ignore */}
+            <LogoIcon className="w-24 text-black dark:text-white" alt="Victron Energy" />
+            <div className="mt-1 py-2">
+              <Translate
+                value="versionInfo.version"
+                version={`${packageInfo.version} ${process.env.REACT_APP_WHITELABEL}`}
+              />
             </div>
-          </Modal.Body>
-        </Modal.Frame>
+            <div className="flex flex-row justify-between">
+              <div className="min-w-sm">{translate("versionInfo.buildDate")}</div>
+              <div>{BUILD_TIMESTAMP}</div>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div>{translate("versionInfo.venusOs")}</div>
+              <div>{humanReadableFirmwareVersion}</div>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div>{translate("versionInfo.identifier")}</div>
+              <div>{portalId}</div>
+            </div>
+
+            <div className="mt-2">
+              <Button onClick={openDiagnostics} className="w-full mt-1" size="md">
+                {translate("diagnostics.diagnostics")}
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal.Frame>
     </div>
   )
 }
