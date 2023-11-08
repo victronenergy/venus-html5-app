770e0e571561b0ff323d66c8b37b79784b3b2b3e
Frank Waalkens <f.waalkens@tfe.nl>
2023-11-08 10:09:13 +0100

    Upgraded tailwind and removed line-clamp dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -167,5 +167,5 @@ module.exports = {
     placeholderOpacity: false,
     ringOpacity: false,
   },
-  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/line-clamp")],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 }
