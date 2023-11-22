1969d1e2616280909660bee5ebdab8fe702cc0ae
Frank Waalkens <f.waalkens@tfe.nl>
2023-11-22 10:09:20 +0100

    SVG prop error fix.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -45,12 +45,8 @@ declare module "*.webp" {
 }
 
 declare module "*.svg" {
-  import * as React from "react"
-
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>
-
-  const src: string
-  export default src
+    const content: any;
+    export default content;
 }
 
 declare module "*.module.css" {
