42e1ed2ac4612dc4dcd45fe73fc2f577e82cf9f0
fwaalkens <f.waalkens@tfe.nl>
2024-02-23 12:59:07 +0100

    Refactored compact and vertical style for tanks.

--- src/app/Marine2/components/boxes/Tanks/Tank/Tank.tsx
+++ src/app/Marine2/components/boxes/Tanks/Tank/Tank.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useMemo, useState } from "react"
-import classNames from "classnames"
 import { useTank } from "@victronenergy/mfd-modules"
 import { observer } from "mobx-react-lite"
 import ProgressBar from "../../../ui/ProgressBar"
 import classnames from "classnames"
 import { ISize } from "@m2Types/generic/size"
-import { applyStyles, StylesType } from "../../../../utils/media"
+import { applyStyles } from "../../../../utils/media"
 import { ComponentMode } from "@m2Types/generic/component-mode"
 import { compactStyles, horizontalStyles, verticalStyles } from "./Styles"
 import { formatLevelFor } from "../../../../utils/formatters/devices/tanks/format-level-for"
@@ -19,23 +18,17 @@ interface Props {
   tankInstanceId: number
   componentMode?: ComponentMode
   orientation?: ScreenOrientation
-  parentSize?: ISize
+  parentSize: ISize
 }
 
 const Tank = ({ tankInstanceId, componentMode, orientation = "vertical", parentSize }: Props) => {
-  let { capacity, fluidType, level, remaining, customName, unit } = useTank(tankInstanceId)
+  const { capacity, fluidType, level, remaining, customName, unit } = useTank(tankInstanceId)
   const fluidTypeNum = +fluidType
   const [isComponentVisible, setIsComponentVisible] = useState(false)
 
-  let horizontalActiveStyles,
-    verticalActiveStyles,
-    compactActiveStyles: StylesType = {}
-
-  if (parentSize) {
-    compactActiveStyles = applyStyles(parentSize, compactStyles)
-    verticalActiveStyles = applyStyles(parentSize, verticalStyles)
-    horizontalActiveStyles = applyStyles(parentSize, horizontalStyles)
-  }
+  const compactActiveStyles = applyStyles(parentSize, compactStyles)
+  const verticalActiveStyles = applyStyles(parentSize, verticalStyles)
+  const horizontalActiveStyles = applyStyles(parentSize, horizontalStyles)
 
   const tankTitle = useMemo(() => customName || FLUID_TRANSLATIONS[fluidTypeNum], [fluidTypeNum, customName])
 
@@ -43,39 +36,28 @@ const Tank = ({ tankInstanceId, componentMode, orientation = "vertical", parentS
     setIsComponentVisible(level !== undefined || capacity !== undefined)
   }, [level, capacity])
 
-  if (!isComponentVisible) return <></>
+  if (!isComponentVisible) return null
 
   // Tanks that are missing level readings and only have capacity
-  const isAuxillaryTank = !!capacity && level === undefined
-  const tempWidth = parentSize ? parentSize.width : {}
-
-  const iconContainerClasses = classNames("flex items-center gap-1 truncate", {
-    "w-[10rem]": tempWidth > 572 || tempWidth < 400,
-    "w-[5rem]": tempWidth <= 572 && tempWidth >= 400,
-  })
-
-  const progressBarContainerClasses = classNames({
-    "w-[calc(100%-14rem)]": tempWidth > 572,
-    "w-[calc(100%-9rem)]": tempWidth <= 572,
-    hidden: tempWidth < 400,
-  })
+  const isAuxiliaryTank = !!capacity && level === undefined
+  const percentage = isAuxiliaryTank ? 0 : formatLevelFor(level)
 
   if (componentMode === "compact") {
     return (
-      <div className="flex justify-between items-center gap-2">
-        <div className={iconContainerClasses}>
+      <div className="flex justify-between items-center gap-2 mb-4">
+        <div className={classnames("flex items-center gap-1 truncate", compactActiveStyles?.iconContainer)}>
           <FluidIcon fluid={fluidTypeNum} className={compactActiveStyles?.icon} />
           <div className={classnames("truncate", compactActiveStyles?.tankName)}>{tankTitle} </div>
         </div>
-        <div className={progressBarContainerClasses}>
-          <ProgressBar percentage={isAuxillaryTank ? 0 : formatLevelFor(level)} type={fluidTypeNum} />
+        <div className={compactActiveStyles?.progressBar}>
+          <ProgressBar percentage={percentage} type={fluidTypeNum} />
         </div>
-        <div className="w-[4rem]">
+        <div className={compactActiveStyles?.percentage}>
           <ValueWithPercentage
             fluid={fluidTypeNum}
             level={level}
             className={compactActiveStyles?.level}
-            isAuxillaryTank={isAuxillaryTank}
+            isAuxillaryTank={isAuxiliaryTank}
           />
         </div>
       </div>
@@ -83,83 +65,32 @@ const Tank = ({ tankInstanceId, componentMode, orientation = "vertical", parentS
   }
 
   if (orientation === "vertical") {
-    const iconContainerClasses = classNames("flex items-center gap-2 truncate", {
-      "w-[16.5rem]": tempWidth > 572 || tempWidth < 400,
-      "w-[5rem]": tempWidth <= 572 && tempWidth >= 400,
-    })
-
-    const progressBarContainerClasses = classNames({
-      "w-[calc(100%-14rem)]": tempWidth > 572,
-      "w-[calc(100%-9rem)]": tempWidth <= 572,
-      hidden: tempWidth < 400,
-    })
-
     return (
-      <>
-
-        <div className="flex justify-between items-center gap-2">
-          <div className={iconContainerClasses}>
-            <FluidIcon fluid={fluidTypeNum} className={compactActiveStyles?.icon} />
-            <div className="flex flex-col truncate">
-              <div className={classnames("truncate", verticalActiveStyles?.tankName)}>{tankTitle}</div>
-              <Capacity
-                remaining={remaining}
-                unit={unit}
-                capacity={capacity}
-                className={verticalActiveStyles?.capacity}
-              />
-            </div>
-          </div>
-          <div className={progressBarContainerClasses}>
-            <ProgressBar percentage={isAuxillaryTank ? 0 : formatLevelFor(level)} type={fluidTypeNum} />
-          </div>
-          <div className="w-[4rem]">
-            <ValueWithPercentage
-              fluid={fluidTypeNum}
-              level={level}
-              className={compactActiveStyles?.level}
-              isAuxillaryTank={isAuxillaryTank}
+      <div className="flex justify-between items-center gap-2 mb-4">
+        <div className={classnames("flex items-center gap-1 truncate", verticalActiveStyles?.iconContainer)}>
+          <FluidIcon fluid={fluidTypeNum} className={compactActiveStyles?.icon} />
+          <div className="flex flex-col truncate">
+            <div className={classnames("truncate", verticalActiveStyles?.tankName)}>{tankTitle}</div>
+            <Capacity
+              remaining={remaining}
+              unit={unit}
+              capacity={capacity}
+              className={verticalActiveStyles?.capacity}
             />
           </div>
         </div>
-
-      {/*  <div className="flex items-center">
-          <div className={classnames("flex items-center min-w-[15%] truncate")}>
-            <div className="mr-2 lg:mr-3">
-              <FluidIcon fluid={fluidTypeNum} className="w-[24px] md:w-[32px]" />
-            </div>
-            <div className="flex flex-col p-2 w-full truncate">
-              <div className={classnames("truncate", verticalActiveStyles?.tankName)}>{tankTitle}</div>
-              <Capacity
-                remaining={remaining}
-                unit={unit}
-                capacity={capacity}
-                className={verticalActiveStyles?.capacity}
-              />
-            </div>
-          </div>
-
-          <div
-            className={classnames("flex items-center justify-center")}
-            style={
-              parentSize?.width
-                ? {
-                    width: parentSize.width > 1000 ? parentSize.width * 0.7 : parentSize.width * 0.5,
-                    display: parentSize.width < 400 ? "none" : "block",
-                  }
-                : {}
-            }
-          >
-            <ProgressBar percentage={isAuxillaryTank ? 0 : formatLevelFor(level)} type={fluidTypeNum} size="large" />
-          </div>
+        <div className={verticalActiveStyles?.progressBar}>
+          <ProgressBar percentage={percentage} type={fluidTypeNum} />
+        </div>
+        <div className={verticalActiveStyles?.percentage}>
           <ValueWithPercentage
             fluid={fluidTypeNum}
             level={level}
             className={verticalActiveStyles?.level}
-            isAuxillaryTank={isAuxillaryTank}
+            isAuxillaryTank={isAuxiliaryTank}
           />
-        </div>*/}
-      </>
+        </div>
+      </div>
     )
   }
 
@@ -167,11 +98,7 @@ const Tank = ({ tankInstanceId, componentMode, orientation = "vertical", parentS
     <div className="px-4">
       <div className="h-full w-full flex flex-row">
         <div className="min-w-[10%] h-auto">
-          <ProgressBar
-            percentage={isAuxillaryTank ? 0 : formatLevelFor(level)}
-            type={fluidTypeNum}
-            orientation="vertical"
-          />
+          <ProgressBar percentage={percentage} type={fluidTypeNum} orientation="vertical" />
         </div>
         <div className="flex flex-col justify-between min-w-[90%]">
           <div className="pt-2 px-2">
@@ -187,7 +114,7 @@ const Tank = ({ tankInstanceId, componentMode, orientation = "vertical", parentS
               fluid={fluidTypeNum}
               level={level}
               className={horizontalActiveStyles?.level}
-              isAuxillaryTank={isAuxillaryTank}
+              isAuxillaryTank={isAuxiliaryTank}
               orientation={orientation}
             />
             <Capacity
