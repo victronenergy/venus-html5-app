913a2a883073582979144732948d8d86c79d7988
fwaalkens <f.waalkens@tfe.nl>
2024-02-26 13:25:58 +0100

    Simplified vertical tank styles.

--- src/app/Marine2/components/boxes/Tanks/Tank/Styles.ts
+++ src/app/Marine2/components/boxes/Tanks/Tank/Styles.ts
@@ -5,7 +5,7 @@ export const compactStyles: BreakpointStylesType = {
   default: {
     tankName: "text-sm ",
     level: "text-base min-w-[38px]",
-    icon: "shrink-0 w-[18px]",
+    icon: "w-[18px]",
     iconContainer: "w-[10rem]",
     progressBar: "w-[calc(100%-14rem)]",
     percentage: "w-[4rem]",
@@ -13,7 +13,7 @@ export const compactStyles: BreakpointStylesType = {
   "sm-s": {
     tankName: "text-sm ",
     level: "text-base min-w-[38px]",
-    icon: "shrink-0 w-[24px]",
+    icon: "w-[24px]",
     iconContainer: "w-[5rem]",
     progressBar: "hidden",
     percentage: "w-[4rem]",
@@ -21,7 +21,7 @@ export const compactStyles: BreakpointStylesType = {
   "sm-m": {
     tankName: "text-base min-w-[2rem]",
     level: "text-base min-w-[38px]",
-    icon: "shrink-0 w-[32px]",
+    icon: "w-[32px]",
     iconContainer: "w-[10rem]",
     progressBar: "hidden",
     percentage: "w-[4rem]",
@@ -29,7 +29,7 @@ export const compactStyles: BreakpointStylesType = {
   "md-s": {
     tankName: "text-base",
     level: "text-lg min-w-[3rem]",
-    icon: "shrink-0 w-[32px]",
+    icon: "w-[32px]",
     iconContainer: "w-[5rem]",
     progressBar: "w-[calc(100%-9rem)]",
     percentage: "w-[4rem]",
@@ -37,45 +37,10 @@ export const compactStyles: BreakpointStylesType = {
   "md-l": {
     tankName: "text-lg",
     level: "text-lg min-w-[3rem]",
-    icon: "shrink-0 w-[32px]",
+    icon: "w-[32px]",
     iconContainer: "w-[10rem]",
     progressBar: "w-[calc(100%-14rem)]",
     percentage: "w-[4rem]",
-  },
-}
-
-export const verticalStyles: BreakpointStylesType = {
-  default: {
-    tankName: "text-base",
-    level: "text-lg min-w-[3rem]",
-    capacity: "text-xs",
-    iconContainer: "w-[17rem]",
-    progressBar: "w-[calc(100%-22rem)]",
-    percentage: "w-[5rem]",
-  },
-  "sm-s": {
-    tankName: "text-base",
-    level: "text-lg min-w-[3rem]",
-    capacity: "text-xs",
-    iconContainer: "w-[10rem]",
-    progressBar: "hidden",
-    percentage: "w-[5rem]",
-  },
-  "md-s": {
-    tankName: "text-lg",
-    level: "text-xl min-w-[3.8rem]",
-    capacity: "text-xs",
-    iconContainer: "w-[12rem]",
-    progressBar: "hidden",
-    percentage: "w-[5rem]",
-  },
-  "lg-s": {
-    tankName: "text-lg",
-    level: "text-xl min-w-[3.8rem]",
-    capacity: "text-xs",
-    iconContainer: "w-[12rem]",
-    progressBar: "w-[calc(100%-17rem)]",
-    percentage: "w-[5rem]",
   },
 }
 
