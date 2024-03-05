d966a57ed152cdbf0534a1b79d2ba8aa1851b8c2
fwaalkens <f.waalkens@tfe.nl>
2024-03-05 16:06:15 +0100

    Refactored progress circle.

--- src/app/Marine2/components/ui/ProgressCircle/ProgressCircle.tsx
+++ src/app/Marine2/components/ui/ProgressCircle/ProgressCircle.tsx
@@ -1,8 +1,10 @@
-import classNames from "classnames"
-import { applyStyles } from "../../../utils/media"
-import { colorFor } from "../../../utils/formatters/generic"
+import { FC } from "react"
 import { ISize } from "@m2Types/generic/size"
+import { colorFor } from "../../../utils/formatters/generic"
+import { applyStyles } from "../../../utils/media"
+import { InnerCircle } from "./InnerCircle/InnerCircle"
 import { Styles } from "./Styles"
+import classNames from "classnames"
 
 interface Props {
   percentage: number
@@ -10,54 +12,49 @@ interface Props {
   boxSize: ISize
 }
 
-const ProgressCircle = ({ percentage, children, boxSize }: Props) => {
-  const activeStyles = applyStyles(boxSize, Styles)
-  const hasPercentage = percentage !== null
+export const ProgressCircle: FC<Props> = ({ percentage, children, boxSize }) => {
+  const radius = 110
+  const pathLength = 2 * Math.PI * radius
+
+  const { strokeWidth } = applyStyles(boxSize, Styles)
+
+  if (percentage === null) {
+    const classes = classNames("fill-none stroke-victron-gray-400 dark:stroke-victron-gray-dark", strokeWidth)
+    const strokeDashArray = `${pathLength} ${pathLength}`
+
+    return (
+      <div className="relative flex justify-center items-center w-full h-full max-w-[15rem]">
+        <svg className="absolute w-full h-full -rotate-90" viewBox="0 0 238 238">
+          <circle r={radius} cx="50%" cy="50%" className={classNames("opacity-30", classes)} />
+          <InnerCircle radius={radius} classes={classes} strokeDashArray={strokeDashArray} />
+        </svg>
+        {children}
+      </div>
+    )
+  }
+
   const roundedPercentage = Math.round(percentage)
-  const color = hasPercentage ? colorFor(roundedPercentage) : "victron-gray-400"
-  const strokeClasses = classNames("fill-none stroke-16", {
+  const color = colorFor(roundedPercentage)
+  const classes = classNames("fill-none", strokeWidth, {
     "stroke-victron-green dark:stroke-victron-green-dark": color === "victron-green",
     "stroke-victron-yellow dark:stroke-victron-yellow-dark": color === "victron-yellow",
     "stroke-victron-red dark:stroke-victron-red-dark": color === "victron-red",
     "stroke-victron-blue dark:stroke-victron-blue-dark": color === "victron-blue",
-    "stroke-victron-gray-400 dark:stroke-victron-gray-dark": color === "victron-gray-400",
   })
-
-  // Due to the pathLength attribute not working on some device, we need to calculate some values.
-  const circleRadius = 111
-  const circlePathLength = 2 * Math.PI * circleRadius
-  const circleStrokeLength = hasPercentage
-    ? (Math.floor(roundedPercentage / 5) * 5 * circlePathLength) / 100
-    : circlePathLength
+  const strokeDashArray = `${(Math.floor(roundedPercentage / 5) * 5 * pathLength) / 100} ${pathLength}`
 
   return (
-    <div className="relative flex justify-center items-center w-full h-full">
+    <div className="relative flex justify-center items-center w-full h-full max-w-[15rem]">
       <svg className="absolute w-full h-full -rotate-90" viewBox="0 0 238 238">
-        <circle r={circleRadius} cx="50%" cy="50%" className={classNames("opacity-30", strokeClasses)} />
-        {roundedPercentage !== 0 && (
-          <circle
-            r={circleRadius}
-            cx="50%"
-            cy="50%"
-            strokeDasharray={`${circleStrokeLength} ${circlePathLength}`}
-            strokeDashoffset={0}
-            strokeLinecap="round"
-            className={strokeClasses}
-            style={{ transition: "stroke-dasharray .5s ease" }}
-          />
-        )}
+        <circle r={radius} cx="50%" cy="50%" className={classNames("opacity-30", classes)} />
+        <InnerCircle
+          radius={radius}
+          classes={classes}
+          percentage={roundedPercentage}
+          strokeDashArray={strokeDashArray}
+        />
       </svg>
-      <div className="flex flex-col items-center">
-        {hasPercentage && (
-          <div className={classNames(activeStyles.percentage)}>
-            {roundedPercentage}
-            <span className="text-victron-gray dark:text-victron-gray-dark">%</span>
-          </div>
-        )}
-        {children}
-      </div>
+      {children}
     </div>
   )
 }
-
-export default ProgressCircle
