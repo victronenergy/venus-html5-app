f53cfd563eda19dc91b4fa92bd23959a552ffeb6
Martin Man <mman@martinman.net>
2024-09-27 13:43:48 +0200

    move to unofficial @cfaester/enzyme-adapter-react-18

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
 import { configure } from "enzyme"
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
+import Adapter from "@cfaester/enzyme-adapter-react-18"
 
 configure({ adapter: new Adapter() })
