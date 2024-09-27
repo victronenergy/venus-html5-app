683707539ecbfe3495e7f2c2eac4dccb2c57ebaf
Martin Man <mman@martinman.net>
2024-09-27 11:16:15 +0200

    feat(console): display QR code to access remote console via browser (#408)
    * Display QR code to access remote console via browser when GUI-v2 is set to default
    * Upate translations

--- src/app/Marine2/components/ui/RemoteConsole/RemoteConsole.tsx
+++ src/app/Marine2/components/ui/RemoteConsole/RemoteConsole.tsx
@@ -3,6 +3,8 @@ import { observer } from "mobx-react-lite"
 import classnames from "classnames"
 import { translate, Translate } from "react-i18nify"
 import useSize from "@react-hook/size"
+import { useApp } from "@victronenergy/mfd-modules"
+import { QRCode } from "react-qrcode-logo"
 
 const RemoteConsole = ({ host, width, height }: Props) => {
   const iframeRef = useRef<HTMLIFrameElement>(null)
@@ -12,10 +14,27 @@ const RemoteConsole = ({ host, width, height }: Props) => {
   const loading = !iframeLoaded
   const protocol = (typeof window !== "undefined" && window.location.protocol) || "http:"
   const url = protocol + "//" + host
+  const app = useApp()
 
   useEffect(() => {
     iframeRef.current?.focus()
   }, [iframeLoaded])
+
+  console.log(`Venus Running GUI version: ${app.guiVersion}`)
+
+  const consoleUrl = `${window.location.protocol}//venus.local/`
+
+  if (app.guiVersion !== 1) {
+    return (
+      <div className="flex flex-col items-center w-2/3 md:w-1/3 space-y-4">
+        <QRCode value={consoleUrl} />
+        <label className="text-xs text-victron-gray-400 sm-l:text-sm dark:text-victron-gray-500">{consoleUrl}</label>
+        <label className="text-xs text-victron-gray-400 sm-l:text-sm dark:text-victron-gray-500 text-center">
+          <Translate value={"error.remoteConsole.qrCodeMessage"} />
+        </label>
+      </div>
+    )
+  }
 
   return (
     <>
