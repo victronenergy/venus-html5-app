51c0d293d9593abb62e32c110d3304622940b16a
Martin Man <mman@martinman.net>
2024-10-03 17:53:37 +0200

    merge with master

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,4 +6,4 @@ import "@testing-library/jest-dom"
 import { configure } from "enzyme"
 import Adapter from "@cfaester/enzyme-adapter-react-18"
 
-configure({ adapter: new Adapter() })
+// configure({ adapter: new Adapter() })
