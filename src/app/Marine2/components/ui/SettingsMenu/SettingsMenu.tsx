8bd9529ab53344fb32ad3d741e4e6e2e8c905f50
Martin Man <mman@martinman.net>
2025-03-05 16:06:38 +0100

    fix: make Night mode a toggle switch

--- src/app/Marine2/components/ui/SettingsMenu/SettingsMenu.tsx
+++ src/app/Marine2/components/ui/SettingsMenu/SettingsMenu.tsx
@@ -120,15 +120,19 @@ const SettingsMenu = () => {
               </label>
               <label className="flex justify-between items-center pb-4 sm-m:pb-6 sm-l:pb-8">
                 <span className="mr-1 text-sm sm-m:mr-2 sm-l:text-base text-black dark:text-white">
-                  {translate("common.night")}
-                </span>
-                <RadioButton onChange={() => themeStore.setNightMode(true)} selected={themeStore.nightMode} />
-              </label>
-              <label className="flex justify-between items-center pb-4 sm-m:pb-6 sm-l:pb-8">
-                <span className="mr-1 text-sm sm-m:mr-2 sm-l:text-base text-black dark:text-white">
                   {translate("common.auto")}
                 </span>
                 <ToggleSwitch id="ToggleAutoMode" onChange={setAutoMode} />
+              </label>
+              <label className="flex justify-between items-center pb-4 sm-m:pb-6 sm-l:pb-8">
+                <span className="mr-1 text-sm sm-m:mr-2 sm-l:text-base text-black dark:text-white">
+                  {translate("common.night")}
+                </span>
+                <ToggleSwitch
+                  id="Toggle2"
+                  onChange={() => themeStore.setNightMode(!themeStore.nightMode)}
+                  selected={themeStore.nightMode}
+                />
               </label>
               <Button onClick={openRemoteConsole} className="w-full" size="md">
                 {translate("header.remoteConsole")}
