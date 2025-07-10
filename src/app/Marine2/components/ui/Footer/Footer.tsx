2058046b69e0cd6432f954891f771f6cca4b1052
Martin Man <mman@martinman.net>
2025-07-10 18:40:38 +0200

    feat: Use smarswitch off/on icon as in GUI-v2

--- src/app/Marine2/components/ui/Footer/Footer.tsx
+++ src/app/Marine2/components/ui/Footer/Footer.tsx
@@ -5,12 +5,16 @@ import PageSelector, { PageSelectorProps } from "../PageSelector"
 import BackIcon from "../../../images/icons/back.svg"
 import { AppViews, useAppViewsStore } from "../../../modules/AppViews"
 import SwitchingPaneOffIcon from "../../../images/icons/smartswitch_off.svg"
+import SwitchingPaneOnIcon from "../../../images/icons/smartswitch_on.svg"
 
 const Footer = ({ pageSelectorProps }: Props) => {
   const appViewsStore = useAppViewsStore()
   const [isShowingBackButton, setIsShowingBackButton] = useState(appViewsStore.currentView !== AppViews.ROOT)
   const [isShowingSwitchingPaneButton, setIsShowingSwitchingPaneButton] = useState(
     appViewsStore.currentView === AppViews.ROOT
+  )
+  const [isShowingSwitchingPane, setIsShowingSwitchingPane] = useState(
+    appViewsStore.currentView === AppViews.SWITCHING_PANE
   )
 
   const handleBackClick = () => {
@@ -24,6 +28,7 @@ const Footer = ({ pageSelectorProps }: Props) => {
   useEffect(() => {
     setIsShowingBackButton(appViewsStore.currentView !== AppViews.ROOT)
     setIsShowingSwitchingPaneButton(appViewsStore.currentView === AppViews.ROOT)
+    setIsShowingSwitchingPane(appViewsStore.currentView === AppViews.SWITCHING_PANE)
   }, [appViewsStore.currentView])
 
   return (
@@ -36,20 +41,29 @@ const Footer = ({ pageSelectorProps }: Props) => {
           </div>
         )}
         {isShowingBackButton && (
-          <div onClick={handleBackClick} className={"w-px-44 h-px-44 justify-center p-1 cursor-pointer"}>
-            <BackIcon onClick={handleBackClick} className={"text-content-victronBlue"} alt={"Back"} />
+          <>
+            {!isShowingSwitchingPane && (
+              <div onClick={handleBackClick} className={"w-px-44 h-px-44 justify-center p-1 cursor-pointer"}>
+                <BackIcon onClick={handleBackClick} className={"text-content-victronBlue"} alt={"Back"} />
+              </div>
+            )}
+            {isShowingSwitchingPane && (
+              <div onClick={handleBackClick} className={"w-px-44 h-px-44 justify-center p-2 cursor-pointer"}>
+                <SwitchingPaneOnIcon onClick={handleBackClick} className={"text-content-victronBlue"} alt={"Back"} />
+              </div>
+            )}
+          </>
+        )}
+        {isShowingSwitchingPaneButton && (
+          <div onClick={openSwitchingPane} className={"w-px-44 h-px-44 justify-center p-2 cursor-pointer"}>
+            <SwitchingPaneOffIcon
+              onClick={openSwitchingPane}
+              className={"text-content-victronBlue"}
+              alt={"Switching Pane"}
+            />
           </div>
         )}
       </div>
-      {isShowingSwitchingPaneButton && (
-        <div onClick={openSwitchingPane} className={"w-px-44 h-px-44 justify-center p-2 cursor-pointer"}>
-          <SwitchingPaneOffIcon
-            onClick={openSwitchingPane}
-            className={"text-content-victronBlue"}
-            alt={"Switching Pane"}
-          />
-        </div>
-      )}
       <SettingsMenu />
     </div>
   )
