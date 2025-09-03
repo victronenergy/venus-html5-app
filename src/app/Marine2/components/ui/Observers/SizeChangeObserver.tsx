b89fe7e59ffbd0753a9f0b1c9d5fb5123237da7e
Martin Man <mman@martinman.net>
2025-09-03 16:49:04 +0200

    chore: Add comments

--- src/app/Marine2/components/ui/Observers/SizeChangeObserver.tsx
+++ src/app/Marine2/components/ui/Observers/SizeChangeObserver.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useEffect, useRef, useState } from "react"
+import { CSSProperties, useLayoutEffect, useRef, useState } from "react"
 import { observer } from "mobx-react"
 import useSize from "@react-hook/size"
 import { ScreenOrientation } from "@m2Types/generic/screen-orientation"
@@ -12,13 +12,16 @@ interface Props {
   style?: CSSProperties
 }
 
+/// Invoke `onSizeChange` when `container` changes its size depending on `orientation`
+/// NOTE: Deprecated because using this component causes UI flicker when cascading
+/// re-render is invoked as part of size change
 const SizeChangeObserver = ({ children, onSizeChange, orientation, className = "", style }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null)
 
   const [width, height] = useSize(containerRef)
   const [initialSize, setInitialSize] = useState<ISize>({ width: 0, height: 0 })
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (initialSize.width === 0 && initialSize.height === 0 && width !== 0 && height !== 0) {
       setInitialSize({ width, height })
     } else if (
