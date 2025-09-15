7ed1223cd0da64d0bf83324ecc45fe6960744e2d
Martin Man <mman@martinman.net>
2025-09-15 22:36:36 +0200

    fix: Document observers, useLayoutEffect insteada of useEffect to partially avoid flickering on resize

--- src/app/Marine2/components/ui/Observers/ScrollSizeObserver.tsx
+++ src/app/Marine2/components/ui/Observers/ScrollSizeObserver.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useLayoutEffect, useRef } from "react"
 import { observer } from "mobx-react"
 import classnames from "classnames"
 import useSize from "@react-hook/size"
@@ -10,12 +10,17 @@ interface Props {
   orientation: ScreenOrientation
 }
 
+/// Invoke `onSizeChange` when `container` does not fit inside `parent`
+/// in specified `orientation` and scrolling is needed.
+/// This is useful when `children` embedded inside `container` overflow and need paging.
+/// NOTE: Deprecated because using this component causes UI flicker when cascading
+/// re-render is invoked as part of scroll size change
 const ScrollSizeObserver = ({ children, onSizeChange, orientation }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null)
   const parentRef = useRef<HTMLDivElement>(null)
   const [scrollWidth, scrollHeight] = useSize(containerRef)
   const [componentWidth, componentHeight] = useSize(parentRef)
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (
       (orientation === "vertical" && scrollHeight > componentHeight) ||
       (orientation === "horizontal" && scrollWidth > componentWidth)
