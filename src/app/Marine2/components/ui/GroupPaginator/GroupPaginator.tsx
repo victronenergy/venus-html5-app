b1c9159abe32620981f1923ed05a2bf1d6bbb61b
Martin Man <mman@martinman.net>
2025-10-02 13:32:37 +0200

    feat: Introduce GroupPaginator
    GroupPaginator takes groups of children and lays them all out vertically
    to compute precise paging information for each group. It then concatenates
    all pages making sure that each group starts on a fresh page.
    
    GroupPaginator has currently been tested only for children
    laid out vertically within each group.
    
    Horizonal paging of groups with children laid out horizontally
    should just work and be the same logic as regular Paginator, but is
    currently not needed anywhere in the app.

---
+++ src/app/Marine2/components/ui/GroupPaginator/GroupPaginator.tsx
@@ -0,0 +1,220 @@
+import React, { useRef, useState, useCallback, useMemo, useLayoutEffect } from "react"
+import classnames from "classnames"
+import { PageSelectorProps, SelectorLocation } from "../PageSelector"
+import { observer } from "mobx-react"
+import PageFlipper from "../PageFlipper"
+import useSize from "@react-hook/size"
+import { ScreenOrientation } from "@m2Types/generic/screen-orientation"
+import OffscreenPageSplitter, { Children, Pages } from "../OffscreenPageSplitter"
+import { boxBreakpoints } from "../../../utils/media"
+
+/// Split `children` contained in `childrenGroups` laid out in given `orientation` into pages
+/// and allow flipping through them using `PageSelector` positioned in `selectorLocation`.
+/// Pages always contain only `children` from one group, and new page is open for children
+/// from next group.
+const GroupPaginator = <T extends React.JSX.Element>({
+  children,
+  childrenGroups,
+  orientation = "horizontal",
+  selectorLocation = "bottom-center",
+}: Props<T>) => {
+  // Layout children horizontally or vertically with min-[wh]-fit to measure thir size
+  // to compute pages
+  const groupsOfChildrenToMeasure = useMemo(() => {
+    return childrenGroups.map((group) => {
+      return group.map((child, i) => (
+        <div
+          className={classnames("", {
+            "h-full min-w-fit": orientation === "horizontal",
+            "w-full min-h-fit": orientation === "vertical",
+          })}
+          key={i}
+        >
+          {child}
+        </div>
+      )) as Children<T>
+    })
+  }, [childrenGroups, orientation])
+
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [width, height] = useSize(wrapperRef)
+  const [availableSpace, setAvailableSpace] = useState(0)
+  const [columnsPerPage, setColumnsPerPage] = useState(1)
+
+  const [pagingResults, setPagingResults] = useState<Pages<T>[]>([])
+  const [pageCount, setPageCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState(0)
+
+  const [pagesElement, setPagesElement] = useState<React.JSX.Element>()
+
+  const setStartingPage = useCallback(
+    (startingPage: number) => {
+      if (!!currentPage && startingPage !== pageCount && currentPage >= startingPage) {
+        setCurrentPage(startingPage - 1)
+      }
+    },
+    [currentPage, pageCount],
+  )
+
+  const onPagesForGroupCalculated = useCallback(
+    (index: number, pages: Pages<T>, _orientation: ScreenOrientation, _selectorSize: number) => {
+      setPagingResults((prev) => {
+        return [...prev.slice(0, index), pages, ...prev.slice(index + 1)]
+      })
+    },
+    [],
+  )
+
+  useLayoutEffect(() => {
+    // each column must have at least ld-s width
+    if (width / 3 >= boxBreakpoints["lg-s"].width) {
+      setColumnsPerPage(3)
+    } else if (width / 2 >= boxBreakpoints["lg-s"].width) {
+      setColumnsPerPage(2)
+    } else {
+      setColumnsPerPage(1)
+    }
+  }, [width, height])
+
+  useLayoutEffect(() => {
+    const columnCount = pagingResults.reduce((total, group) => {
+      return total + group.length
+    }, 0)
+
+    const pageCount = Math.ceil(columnCount / columnsPerPage)
+    const fullWidthPct = (100.0 / columnsPerPage) * columnCount
+
+    if (orientation === "vertical") {
+      var columnIndex = 0
+      const pagesEl = (
+        <div
+          style={{
+            width: `${fullWidthPct}%`,
+          }}
+          className="h-full flex"
+        >
+          {pagingResults.flatMap((group, groupIndex) => {
+            const groupColumnCount = group.length
+            return group.map((column, groupColumnIndex) => {
+              const result = (
+                <div
+                  key={`columnEl${groupIndex}${groupColumnIndex}`}
+                  style={{
+                    width: `calc(100% / ${columnsPerPage})`,
+                  }}
+                >
+                  {children({
+                    columnIndex: columnIndex,
+                    columnCount: columnCount,
+                    columnsPerPage: columnsPerPage,
+                    columnChildren: column.children,
+                    groupIndex: groupIndex,
+                    groupColumnIndex: groupColumnIndex,
+                    groupColumnCount: groupColumnCount,
+                    isFirstColumnOnPage: isFirstColumnOnPage(columnIndex, columnCount, columnsPerPage),
+                  })}
+                </div>
+              )
+              columnIndex += 1
+              return result
+            })
+          })}
+        </div>
+      )
+
+      setPagesElement(pagesEl)
+    } else {
+      // TODO: add support for horizontal children paging and test it
+      //   const pagesEl = <div className="h-full w-fit w-min-full">{childrenArray}</div>
+      //   setPagesElement(pagesEl)
+    }
+    setPageCount(pageCount)
+    setStartingPage(pageCount)
+  }, [children, childrenGroups, columnsPerPage, orientation, pagingResults, setStartingPage])
+
+  useLayoutEffect(() => {
+    if (orientation === "horizontal") {
+      setPagesElement(undefined)
+      setAvailableSpace(width)
+    }
+  }, [width, orientation])
+
+  useLayoutEffect(() => {
+    if (orientation === "vertical") {
+      setPagesElement(undefined)
+      setAvailableSpace(height)
+    }
+  }, [height, orientation])
+
+  return (
+    <>
+      <div>
+        {groupsOfChildrenToMeasure.map((group, index) => (
+          <div key={`splitter_${index}`}>
+            <OffscreenPageSplitter
+              orientation={orientation}
+              availableSpace={availableSpace - 40} // TODO: subtract Box title height properly
+              selectorLocation={selectorLocation}
+              isSelectorAlwaysDisplayed={groupsOfChildrenToMeasure.length > 1}
+              identifier={index}
+              onPagesCalculated={onPagesForGroupCalculated}
+            >
+              {groupsOfChildrenToMeasure[index]}
+            </OffscreenPageSplitter>
+          </div>
+        ))}
+      </div>
+      <div ref={wrapperRef} className="w-full h-full">
+        {!!pagesElement && pageCount && (
+          <PageFlipper
+            pages={pageCount}
+            currentPageSetter={setCurrentPage}
+            startingPage={currentPage}
+            selectorLocation={selectorLocation}
+          >
+            {pagesElement}
+          </PageFlipper>
+        )}
+      </div>
+    </>
+  )
+}
+
+function isFirstColumnOnPage(columnIndex: number, totalColumns: number, columnsPerPage: number) {
+  if (totalColumns <= columnsPerPage) {
+    return columnIndex === 0
+  }
+
+  if (columnIndex % columnsPerPage === 0 && columnIndex < totalColumns - columnsPerPage) {
+    return true
+  }
+
+  const lastPageStart = totalColumns - columnsPerPage
+  return columnIndex === lastPageStart
+}
+
+interface PaginationState<T extends React.JSX.Element = React.JSX.Element> {
+  columnIndex: number
+  columnCount: number
+  columnsPerPage: number
+  columnChildren: Children<T>
+  groupIndex: number
+  groupColumnIndex: number
+  groupColumnCount: number
+  isFirstColumnOnPage: boolean
+}
+
+type PaginationRenderer<T extends React.JSX.Element = React.JSX.Element> = (
+  state: PaginationState<T>,
+) => React.JSX.Element
+
+interface Props<T extends React.JSX.Element = React.JSX.Element> {
+  children: PaginationRenderer<T>
+  childrenGroups: Children<T>[]
+  orientation?: ScreenOrientation
+  pageNumber?: number
+  selectorLocation?: SelectorLocation
+  pageSelectorPropsSetter?: (arg0: PageSelectorProps) => void
+}
+
+export default observer(GroupPaginator)
