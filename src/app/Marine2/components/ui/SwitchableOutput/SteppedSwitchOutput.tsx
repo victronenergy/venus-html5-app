6ef9c8fc47e71118ba436610bf41e692183f84d6
Martin Man <mman@martinman.net>
2025-10-09 17:45:20 +0200

    feat: SteppedSwitch

--- src/app/Marine2/components/ui/SwitchableOutput/SteppedSwitchOutput.tsx
+++ src/app/Marine2/components/ui/SwitchableOutput/SteppedSwitchOutput.tsx
@@ -13,26 +13,68 @@ interface SteppedSwitchOutputProps {
 const SteppedSwitchOutput = observer((props: SteppedSwitchOutputProps) => {
   const switchableOutput = useSwitchableOutput(props.deviceId, props.outputId)
 
-  const variant = "off"
+  const variant = switchableOutput.state === 1 ? "on" : "off"
+  const buttons = Array.from({ length: switchableOutput.dimmingMax || 1 }, (_, i) => i + 1)
+  const selected = switchableOutput.dimming || 1
+
+  const handleClickOnOff = () => {
+    switchableOutput.updateState(switchableOutput.state === 1 ? 0 : 1)
+  }
+
+  const handleClickButton = (index: number) => {
+    if (switchableOutput.state === 0) {
+      return
+    }
+    switchableOutput.updateDimming(index)
+  }
 
   return (
     <div className={classnames("mt-4", props.className)}>
       <div>{switchableOutput.customName || switchableOutput.name}</div>
-      <button
-        className={classnames(
-          "h-px-44 px-4 py-1.5 whitespace-nowrap",
-          "border-2 border-content-victronBlue cursor-pointer",
-          "text-sm min-h-[2.375rem]",
-          "rounded-md",
-          {
-            "bg-surface-victronBlue text-content-primary": variant === "off",
-            // "bg-content-victronBlue text-content-onVictronBlue": variant === "on",
-          },
-          props.className,
-        )}
-      >
-        SteppedSwitchOutput
-      </button>
+      {/* Border */}
+      <div className="h-px-44 rounded-md bg-surface-victronBlue border-2 border-content-victronBlue">
+        {/* Container */}
+        <div className="h-full rounded-sm flex overflow-hidden">
+          {/* On/Off Background */}
+          <div
+            className={classnames("h-full flex items-center", {
+              // "bg-surface-victronBlue": true, // variant === "off",
+              // "bg-content-victronBlue": variant === "on",
+            })}
+          >
+            {/* On/Off Button */}
+            <button
+              className={classnames(
+                "h-full px-2 whitespace-nowrap cursor-pointer text-sm min-h-[2.375rem] min-w-[3rem]",
+                {
+                  "text-content-primary": true, // variant === "off",
+                  // "text-content-onVictronBlue": variant === "on",
+                },
+              )}
+              onClick={handleClickOnOff}
+            >
+              {variant === "on" ? "On" : "Off"}
+            </button>
+            {/* Separator */}
+            <div className="w-px-2 h-[80%] rounded-sm bg-content-lightGray"></div>
+          </div>
+          <div className="flex-1 flex h-full w-full">
+            {buttons.map((index) => (
+              <button
+                key={index}
+                className={classnames("flex-1", {
+                  "text-content-onVictronBlue": index === selected,
+                  "bg-content-victronBlue": index === selected && variant === "on",
+                  "bg-content-victronBlue50": index === selected && variant === "off",
+                })}
+                onClick={() => handleClickButton(index)}
+              >
+                {`${index}`}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 })
