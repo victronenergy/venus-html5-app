9ebae9e9d87a0c1843b860ccfcb00752f5e219d7
Martin Man <mman@martinman.net>
2025-10-14 12:21:25 +0200

    fix: Make temperature rounding default, but optional

--- src/app/Marine2/utils/formatters/temperature/temperature-value-for.ts
+++ src/app/Marine2/utils/formatters/temperature/temperature-value-for.ts
@@ -1,11 +1,11 @@
 import { TemperatureUnit } from "@victronenergy/mfd-modules"
 import { celsiusToFahrenheit } from "./celcius-to-fahrenheit"
 
-export const temperatureValueFor = (temperature: number, unit: TemperatureUnit) => {
+export const temperatureValueFor = (temperature: number, unit: TemperatureUnit, rounded: boolean = true) => {
   if (unit === "fahrenheit") {
     const fahrenheitValue = celsiusToFahrenheit(temperature)
-    return Math.round(fahrenheitValue * 10) / 10
+    return rounded ? Math.round(fahrenheitValue * 10) / 10 : fahrenheitValue
   }
 
-  return Math.round(temperature * 10) / 10
+  return rounded ? Math.round(temperature * 10) / 10 : temperature
 }
