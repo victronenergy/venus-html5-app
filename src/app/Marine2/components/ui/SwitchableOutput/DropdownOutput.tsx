3cce4d6723c266af00df522d59e736c25bb0bb44
Martin Man <mman@martinman.net>
2025-10-16 15:02:29 +0200

    feat: Dropdown

--- src/app/Marine2/components/ui/SwitchableOutput/DropdownOutput.tsx
+++ src/app/Marine2/components/ui/SwitchableOutput/DropdownOutput.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import {
   getSwitchableOutputNameForDisplay,
   SwitchableOutputId,
@@ -7,6 +7,7 @@ import {
 } from "@victronenergy/mfd-modules"
 import classnames from "classnames"
 import { observer } from "mobx-react"
+import ArrowRightIcon from "../../../images/icons/arrow-right.svg"
 
 interface DropdownOutputProps {
   key: string
@@ -16,26 +17,79 @@ interface DropdownOutputProps {
   className?: string
 }
 
-// TODO: this needs work
-
 const DropdownOutput = observer((props: DropdownOutputProps) => {
   const switchableOutput = useSwitchableOutput(props.deviceId, props.outputId)
   const outputName = getSwitchableOutputNameForDisplay(switchableOutput, props.parentDeviceName)
 
-  const parsedLabels = switchableOutput.labels ? JSON.parse(switchableOutput.labels) : "[]"
-  const labels = Array.isArray(parsedLabels) ? parsedLabels : []
-  const selectedLabelIndex = switchableOutput.dimming || 0
-  const label = labels.length > selectedLabelIndex ? labels[selectedLabelIndex] : ""
+  const options = Array.isArray(switchableOutput.labels) ? switchableOutput.labels : []
+  // TODO: this should be a number but as of today we sometimes receive a string
+  const selectedOptionIndex = Number(switchableOutput.dimming)
+  const selectedOption =
+    options.length > selectedOptionIndex ? options[selectedOptionIndex] : options.length > 0 ? options[0] : ""
+
+  const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!isOpen) return
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => document.removeEventListener("mousedown", handleClickOutside)
+  }, [isOpen])
+
+  const handleSelect = (event: any, index: number) => {
+    event.stopPropagation()
+    setIsOpen(false)
+    switchableOutput.updateDimming(index)
+  }
 
   return (
     <div className={classnames("mt-4", props.className)}>
       <div>{outputName}</div>
-      {/* Border */}
-      <div className="h-px-44 rounded-md bg-surface-victronBlue border-2 border-content-victronBlue">
-        {/* Container */}
+      {/* Container */}
+      <div
+        className="relative h-px-44 rounded-md bg-surface-victronBlue"
+        ref={dropdownRef}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {/* Selected Option */}
         <div className="h-full rounded-sm flex overflow-hidden items-center">
-          <div className="px-2 whitespace-nowrap cursor-pointer text-sm">{label}</div>
+          <div className="px-4 flex-1 whitespace-nowrap cursor-pointer text-sm select-none">{selectedOption}</div>
+          <ArrowRightIcon className="h-full py-3 px-4 text-content-victronBlue cursor-pointer outline-none rotate-90" />
         </div>
+        {/* Dropdown */}
+        {isOpen && (
+          <>
+            <div className="absolute w-full rounded-md bg-surface-secondary overflow-hidden">
+              <div className="rounded-md w-full py-px-1 max-h-[calc(5*44px*var(--uix))] bg-surface-victronBlue overflow-y-auto">
+                {options.map((option, index) => (
+                  <div
+                    key={index}
+                    onClick={(event: any) => handleSelect(event, index)}
+                    className={classnames(
+                      "px-4 h-px-44 flex items-center cursor-pointer text-sm select-none hover:bg-content-victronBlue hover:text-content-onVictronBlue text-content-primary",
+                      {
+                        "bg-content-victronBlue50 ": index === selectedOptionIndex,
+                      },
+                    )}
+                  >
+                    {option}
+                  </div>
+                ))}
+              </div>
+              {/* Dropdown Border */}
+              <div className="absolute inset-0 rounded-md border-2 border-content-victronBlue pointer-events-none" />
+            </div>
+          </>
+        )}
+        {/* Selected Option Border */}
+        <div className="absolute inset-0 h-px-44 rounded-md border-2 border-content-victronBlue pointer-events-none" />
       </div>
     </div>
   )
