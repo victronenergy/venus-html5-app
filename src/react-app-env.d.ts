f5bad446f1f8c17898fbdfc0db6b18b283eac57e
Martin Man <mman@martinman.net>
2026-06-01 19:28:28 +0200

    chore: bump typescript ^5.9.3 → ^6.0.3

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -49,6 +49,12 @@ declare module "*.svg" {
   export default content
 }
 
+declare module "*.css" {}
+
+declare module "*.scss" {}
+
+declare module "*.sass" {}
+
 declare module "*.module.css" {
   const classes: { readonly [key: string]: string }
   export default classes
