25547e28cd2efaf0fc402e61fb146deaee81f13c
Martin Man <mman@martinman.net>
2026-06-02 19:22:50 +0200

    chore: upgrade Jest 26 → 30, remove jest-circus and jest-resolve

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,13 @@ const config = {
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testMatch: ["<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}", "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"],
   testEnvironment: "jsdom",
-  testRunner: "<rootDir>/node_modules/jest-circus/runner.js",
   transform: {
     "^.+\\.svg$": "<rootDir>/config/jest/fileTransform.js",
     "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/config/jest/babelTransform.js",
     "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
     "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js",
   },
-  transformIgnorePatterns: ["node_modules/(?!@victronenergy/mfd-modules|lodash-es|axios)"],
+  transformIgnorePatterns: ["node_modules/(?!@victronenergy/mfd-modules|@react-hook|lodash-es|axios)"],
   modulePaths: ["./src"],
   moduleNameMapper: {
     "^react-native$": "react-native-web",
