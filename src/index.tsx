09735fb4110dbdd0a4553e68007ab1ec82f3d086
Martin Man <mman@martinman.net>
2026-06-02 22:48:04 +0200

    chore: remove unused web-vitals dependency and reportWebVitals boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -79,8 +79,3 @@ const root = createRoot(container!)
 root.render(<React.StrictMode>{getApp()}</React.StrictMode>)
 
 registerServiceWorker()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
