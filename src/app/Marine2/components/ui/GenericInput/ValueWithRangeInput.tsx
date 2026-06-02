350e1984cecfbeda1b66ac8e380d4808de766e9d
Martin Man <mman@martinman.net>
2026-06-02 17:44:09 +0200

    feat: Add Value with Range Generic Input

--- src/app/Marine2/components/ui/GenericInput/ValueWithRangeInput.tsx
+++ src/app/Marine2/components/ui/GenericInput/ValueWithRangeInput.tsx
@@ -8,6 +8,7 @@ import {
 } from "@victronenergy/mfd-modules"
 import classnames from "classnames"
 import { observer } from "mobx-react"
+import { getValueOrDefault, useValueFormatter } from "../SwitchableOutput/helpers"
 
 interface ValueWithRangeInputProps {
   key: string
@@ -22,11 +23,28 @@ const ValueWithRangeInput = observer((props: ValueWithRangeInputProps) => {
   const genericInput = useGenericInput(props.tree, props.deviceId, props.inputId)
   const inputName = getSwitchingPaneItemNameForDisplay(genericInput, props.parentDeviceName)
 
+  const unit = getValueOrDefault(genericInput.unit, "")
+  const decimals = getValueOrDefault(genericInput.decimals, 0)
+  const formatValueAndUnit = useValueFormatter({ decimals })
+
+  const value = genericInput.value
+  const min = getValueOrDefault(genericInput.rangeMin, 0)
+  const max = getValueOrDefault(genericInput.rangeMax, 100)
+  const ratio =
+    value !== undefined && value !== null && max !== min ? Math.round(((value - min) / (max - min)) * 100) : 0
+  const formattedValue = value !== undefined && value !== null ? formatValueAndUnit(value, unit, false) : "--"
+  const formattedUnit =
+    value !== undefined && value !== null ? formatValueAndUnit(value, unit).slice(formattedValue.length) : ""
+
   return (
     <div className={classnames("mt-4", props.className)}>
       <div>{inputName}</div>
       <div className="h-px-44 rounded-md bg-surface-tertiary flex items-center px-4">
-        <span className="text-sm text-content-primary">--</span>
+        <div className="flex-1 h-px-8 rounded-full bg-surface-victronBlue overflow-hidden mr-4">
+          <div className="h-full bg-content-victronBlue rounded-full" style={{ width: `${ratio}%` }} />
+        </div>
+        <span className="text-base text-content-primary">{formattedValue}</span>
+        <span className="text-base text-content-tertiary">{formattedUnit}</span>
       </div>
     </div>
   )
