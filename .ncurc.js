35d6956cae77bb1f229d2fdff9fccf2382d2f60e
Martin Man <mman@martinman.net>
2026-06-02 23:10:43 +0200

    chore: switch .ncurc from JSON to JS to allow inline comments explaining rejections

---
+++ .ncurc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reject: [
+    // v8+ requires ES2018+, our target is Chrome 51 (ES2015)
+    "@sentry/react",
+    // deprecated in v8+, merged into @sentry/react
+    "@sentry/tracing",
+    // pinned to React 18, upgrade together with React 19 migration
+    "react-test-renderer",
+  ],
+}
