4b76ddbdee46abe6bbe29de3fe7925f83cc7cbda
Martin Man <mman@martinman.net>
2026-09-15 15:54:32 +0200

    fix: use camelCase keys for statusWidget and tankWidget translations

--- src/app/Marine2/utils/constants/devices/tanks.ts
+++ src/app/Marine2/utils/constants/devices/tanks.ts
@@ -1,16 +1,16 @@
 import { translate } from "react-i18nify"
 
 export const FLUID_TRANSLATIONS = [
-  translate("tankWidget.Fuel"),
-  translate("tankWidget.Fresh water"),
-  translate("tankWidget.Waste water"),
-  translate("tankWidget.Live well"),
-  translate("tankWidget.Oil"),
-  translate("tankWidget.Black water"),
-  translate("tankWidget.Gasoline"),
-  translate("tankWidget.Diesel"),
-  translate("tankWidget.LPG"),
-  translate("tankWidget.LNG"),
-  translate("tankWidget.Hydraulic oil"),
-  translate("tankWidget.Raw water"),
+  translate("tankWidget.fuel"),
+  translate("tankWidget.freshWater"),
+  translate("tankWidget.wasteWater"),
+  translate("tankWidget.liveWell"),
+  translate("tankWidget.oil"),
+  translate("tankWidget.blackWater"),
+  translate("tankWidget.gasoline"),
+  translate("tankWidget.diesel"),
+  translate("tankWidget.lpg"),
+  translate("tankWidget.lng"),
+  translate("tankWidget.hydraulicOil"),
+  translate("tankWidget.rawWater"),
 ]
